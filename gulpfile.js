d66623680e738024d6a14bcf3e01e20ec01bde91
Andrzej Maciurzynski <andrzej.maciurzynski@brainly.com>
2015-06-19 14:50:58 +0200

    buttons

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var livereload = require('gulp-livereload');
 var rename = require('gulp-rename');
+var svgSprite = require('gulp-svg-sprite');
 
 gulp.task('sass:docs', function () {
     gulp.src('./docs/sass/**/*.scss')
@@ -16,14 +17,32 @@ gulp.task('sass:docs', function () {
 });
 
 gulp.task('sass:framework-build', function () {
-    gulp.src('./framework/sass/main.scss')
+    gulp.src('./framework/src/sass/main.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename('main.min.css'))
-        .pipe(gulp.dest('./framework/css/'))
+        .pipe(gulp.dest('./framework/dist/css/'))
+});
+
+gulp.task('svg:sprite-generate', function(){
+    gulp.src('./framework/src/icons/*.svg')
+        .pipe(svgSprite({
+            inline: true,
+            mode: {
+                symbol: {
+                    sprite: '../sprite.svg'
+                }
+            },
+            shape: {
+                id: {
+                    generator: 'svg-icons-%s'
+                }
+            }
+        }))
+        .pipe(gulp.dest('./framework/dist'));
 });
 
 gulp.task('watch', function () {
     livereload.listen();
     gulp.watch('./docs/sass/**/*.scss', ['sass:docs']);
-    gulp.watch('./framework/sass/**/*.scss', ['sass:docs']);
+    gulp.watch('./framework/src/sass/**/*.scss', ['sass:docs']);
 });
