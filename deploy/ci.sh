f2da2bc843e716f9106562db7a1d077f1c5a4f39
Mr_Mig <alexey.migutsky@gmail.com>
2015-08-25 11:19:29 +0200

    Add basic steps to ci script

--- deploy/ci.sh
+++ deploy/ci.sh
@@ -9,5 +9,23 @@ docker push $IMAGE_ARTIFACT
 
 # build stuff
 
+docker run -t --rm \
+  -v ${pwd}/src:/style-guide/src \
+  -v ${pwd}/gulpfile.js:/style-guide/gulpfile.js \
+  -v ${pwd}/dist:/style-guide/dist \
+  -v ${pwd}/docs:/style-guide/docs \
+  brainly/style-guide gulp sass:build
+
 # deploy to s3
 # s3_website cfg apply --headless --autocreate-cloudfront-dist
+
+docker run -t --rm \
+  -v ${pwd}/src:/style-guide/src \
+  -v ${pwd}/gulpfile.js:/style-guide/gulpfile.js \
+  -v ${pwd}/dist:/style-guide/dist \
+  -v ${pwd}/docs:/style-guide/docs \
+  -e AWS_ACCESS_KEY=$AWS_ACCESS_KEY \
+  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+  brainly/style-guide s3_website push
+
+
