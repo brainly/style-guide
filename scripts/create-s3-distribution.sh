5b736813170f2b06ba8b2b1e568e08b93edb1a40
Mr_Mig <alexey.migutsky@gmail.com>
2015-08-27 15:58:12 +0200

    Added CORS settings file for s3 bucket

--- scripts/create-s3-distribution.sh
+++ scripts/create-s3-distribution.sh
@@ -12,3 +12,6 @@ docker run -t --rm \
   -e AWS_ACCESS_KEY=$AWS_ACCESS_KEY \
   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
   brainly/style-guide s3_website cfg apply --headless --autocreate-cloudfront-dist
+
+echo
+echo "You have to MANUALLY add cors.xml in PERMISSIONS section for your s3 bucket"
