5c41943522dc06244d04cef0dfe92ec5961029e0
Andrzej Maciurzynski <andrzej.maciurzynski@brainly.com>
2015-09-17 16:45:11 +0200

    Update ide-run-scss-lint-docker.sh script

--- scripts/ide-scss-lint-exec.sh
+++ scripts/ide-scss-lint-exec.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+
+# This is an scss-lint exec script for JetBrains-family IDEs
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source ${SCRIPT_DIR}/run-scss-lint.sh $1 -f JSON
