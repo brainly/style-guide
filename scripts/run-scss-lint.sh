60dcfb33468d73bb0161895ed69d4969170e36e9
Andrzej Maciurzynski <andrzej.maciurzynski@brainly.com>
2015-09-22 12:27:59 +0200

    Fix hook

--- scripts/run-scss-lint.sh
+++ scripts/run-scss-lint.sh
@@ -4,7 +4,7 @@
 
 PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)
 
-docker run -ti --rm \
+docker run --rm \
   -v $PROJECT_DIR/src:/style-guide/src \
   -v $PROJECT_DIR/gulpfile.js:/style-guide/gulpfile.js \
   brainly/style-guide gulp scss-lint
