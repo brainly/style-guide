8007ddd6d0793200b9daad91604f3dd5cbb57f30
Andrzej Maciurzynski <andrzej.maciurzynski@brainly.com>
2015-10-06 14:13:56 +0200

    Update deploy and build scripts

--- deploy/ci.sh
+++ deploy/ci.sh
@@ -3,6 +3,8 @@ set -e
 
 source ./scripts/cibuild.sh
 
+echo "Push site to S3"
+
 docker run -t --rm \
   -v $PROJECT_DIR/dist:/style-guide/dist \
   -v $PROJECT_DIR/s3_website.yml:/style-guide/s3_website.yml \
