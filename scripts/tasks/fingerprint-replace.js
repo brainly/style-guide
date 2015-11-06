f0cad31bc4beae740b7d0fd377fb7b1d9d0a8b08
Mr_Mig <alexey.migutsky@gmail.com>
2015-11-06 16:28:13 +0100

    Added separator margins for small devices

--- scripts/tasks/fingerprint-replace.js
+++ scripts/tasks/fingerprint-replace.js
@@ -3,16 +3,24 @@ module.exports = function (gulp, plugins, consts) {
         var manifest = require(plugins.path.join(consts.VERSIONED_DIST, 'rev-manifest'));
         var cssPath = plugins.path.join(consts.VERSIONED_DIST, 'style-guide.css');
         var docsHtmlPath = plugins.path.join(consts.VERSIONED_DIST, 'docs', '*.html');
+        var componentsHtmlPath = plugins.path.join(consts.VERSIONED_DIST, 'docs', 'components','**', '*.html');
 
         gulp.src(cssPath)
             .pipe(plugins.fingerprint(manifest, {prefix: '../'}))
             .pipe(gulp.dest(consts.VERSIONED_DIST));
 
-        return gulp.src(docsHtmlPath)
+        gulp.src(docsHtmlPath)
             .pipe(plugins.fingerprint(manifest, {
                 prefix: '../../',
                 regex: /(?:url\(["']?(.*?)['"]?\)|src=["'](.*?)['"]|src=([^\s\>]+)(?:\>|\s)|data=["'](.*?)['"]|href=["'](.*?)['"]|href=([^\s\>]+)(?:\>|\s))/g
             }))
             .pipe(gulp.dest(plugins.path.join(consts.VERSIONED_DIST, 'docs')));
+
+        return gulp.src(componentsHtmlPath)
+            .pipe(plugins.fingerprint(manifest, {
+                prefix: '../../../../',
+                regex: /(?:url\(["']?(.*?)['"]?\)|src=["'](.*?)['"]|src=([^\s\>]+)(?:\>|\s)|data=["'](.*?)['"]|href=["'](.*?)['"]|href=([^\s\>]+)(?:\>|\s))/g
+            }))
+            .pipe(gulp.dest(plugins.path.join(consts.VERSIONED_DIST, 'docs', 'components')));
     }
 };
