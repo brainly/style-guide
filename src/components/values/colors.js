ddf4df73206408d67282fe25d63faf5c23b6ca62
Mr_Mig <alexey.migutsky@gmail.com>
2015-11-07 16:03:26 +0100

    Added basic scales

---
+++ src/components/values/colors.js
@@ -0,0 +1,32 @@
+export default {
+  primary: {
+    black: '#000',
+    mint: '#6ed6a0',
+    mintMidtone: '#a7e6c6',
+    mintLight: '#e1f6eb',
+    blue: '#5bb8ff',
+    blueMidtone: '#abe3ff',
+    blueLight: '#ddf3ff'
+  },
+
+  secondary: {
+    lavender: '6e85ff',
+    lavenderMidtone: '#bdc8fc',
+    lavenderLight: '#e8ecff',
+    peach: '#ff8073',
+    peachMidtone: '#fdc7c1',
+    peachLight: '#ffe5e2',
+    mustard: '#ffbe32',
+    mustardMidtone: '#ffdb8d',
+    mustardLight: '#fff1d5'
+  },
+
+  gray: {
+    white: '#ffffff',
+    gray: '#bbbdbf',
+    grayMidtone: '#e1e3e5',
+    grayLight: '#f0f2f5',
+    grayAlt: '#969799',
+    grayAltLight: '#e9ecee'
+  }
+};
---
+++ src/components/values/widthScale.js
@@ -0,0 +1,5 @@
+export default {
+  narrow: 'narrow',
+  wide: 'wide',
+  full: 'full'
+}
