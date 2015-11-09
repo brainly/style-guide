c54ce543417d49cff0bbde25b6f02515819ab98b
Mr_Mig <alexey.migutsky@gmail.com>
2015-11-09 20:29:37 +0100

    Decoupled mint-button-primary and mint-button-secondary

--- src/docs/components/docs/index.js
+++ src/docs/components/docs/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import './main.scss';
-import MintButton from 'components/mint-button';
-import { availableSizes as btnSizes } from 'components/mint-button';
+import MintButton from 'components/mint-button-primary';
+import MintButtonSecondary from 'components/mint-button-secondary';
+import { availableTypes as btnTypes } from 'components/mint-button-primary';
 import DocsEntry from '../docs-entry';
 import DocsBlock from '../docs-block';
+
+function renderAll(modifers, attr, Elt) {
+  return Object.keys(modifers).map((mod) => {
+    let attrs = {
+      [attr]: mod
+    };
+
+    return <Elt {...attrs }>Example</Elt>;
+  });
+
+}
 
 export default (props) => {
   return <DocsEntry title="Buttons">
     <DocsBlock title="Primary Buttons">
+
       <MintButton>Primary</MintButton>
+      { renderAll(btnTypes, 'type', MintButton) }
+
     </DocsBlock>
     <DocsBlock title="Secondary Buttons">
-      <MintButton size={ btnSizes.small }>Secondary</MintButton>
+
+      <MintButtonSecondary>Secondary</MintButtonSecondary>
+
     </DocsBlock>
   </DocsEntry>
 }
