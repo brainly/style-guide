9d5d4ff28e3bdf1f20fb708c4889e72ceb007c49
Mr_Mig <alexey.migutsky@gmail.com>
2015-11-11 22:41:01 +0100

    Added custom teaspoon jasmine matcher

--- tools/tests.webpack.js
+++ tools/tests.webpack.js
@@ -1,2 +1,9 @@
+var matchers = require('helpers/teaspoon-jasmine-matchers');
+console.log(matchers);
+
 var context = require.context('../src', true, /\.spec$/);
 context.keys().forEach(context);
+
+beforeEach(() => {
+  jasmine.addMatchers(matchers);
+});
