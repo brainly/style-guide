d46527455e8f28311e14f22c92574fee3ba12045
Andrzej Maciurzynski <andrzej.maciurzynski@brainly.com>
2015-11-17 12:45:36 +0100

    Add new logos

--- scripts/tasks/fingerprint.js
+++ scripts/tasks/fingerprint.js
@@ -2,10 +2,11 @@ module.exports = function (gulp, plugins, consts) {
     return function () {
         var fonts = plugins.path.join(consts.SRC, 'fonts', '*');
         var images = plugins.path.join(consts.SRC, 'images', '*');
+        var logos = plugins.path.join(consts.SRC, 'images', 'logos', '*');
 
         // by default, gulp would pick `assets/css` as the base,
         // so we need to set it explicitly:
-        return gulp.src([fonts, images], {base: './src'})
+        return gulp.src([fonts, images, logos], {base: './src'})
             .pipe(plugins.rev())
             .pipe(gulp.dest(consts.DIST))  // write rev'd assets to build dir
             .pipe(plugins.rev.manifest())
