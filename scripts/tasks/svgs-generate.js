d139886b0fe80987080d77055b7d3058d5642ba1
Konrad Dzwinel <kdzwinel@gmail.com>
2016-04-23 01:51:19 +0200

    Fix indentation

--- scripts/tasks/svgs-generate.js
+++ scripts/tasks/svgs-generate.js
@@ -1,19 +1,19 @@
 function svgSymbolCleanUp(shape, sprite, callback) {
-    var symbol = shape.dom.documentElement;
-    var childNodes = shape.dom.documentElement.childNodes;
-    symbol.setAttribute('style', 'overflow: visible');
+  var symbol = shape.dom.documentElement;
+  var childNodes = shape.dom.documentElement.childNodes;
+  symbol.setAttribute('style', 'overflow: visible');
 
-    for(var i = 0; i < childNodes.length; i++) {
-        if(childNodes[i].nodeType === 1) {
-            childNodes[i].removeAttribute('class');
-            childNodes[i].removeAttribute('fill');
-        }
+  for (var i = 0; i < childNodes.length; i++) {
+    if (childNodes[i].nodeType === 1) {
+      childNodes[i].removeAttribute('class');
+      childNodes[i].removeAttribute('fill');
     }
-    callback(null);
+  }
+  callback(null);
 }
 
 function svgAddPolyfill(svgPolyfill, svg) {
-    return svgPolyfill.replace('#SVG#', svg.replace(/(\r\n|\n|\r)/gm,""));
+  return svgPolyfill.replace('#SVG#', svg.replace(/(\r\n|\n|\r)/gm, ""));
 }
 
 function svgMoveDefs(svgString) {
@@ -21,7 +21,7 @@ function svgMoveDefs(svgString) {
   var svg = new DOMParser().parseFromString(svgString);
   var defs = svg.documentElement.getElementsByTagName('defs');
 
-  for(var i = 0; i < defs.length; i++) {
+  for (var i = 0; i < defs.length; i++) {
     var def = defs[i];
     def.parentNode.removeChild(def);
     svg.documentElement.insertBefore(def, svg.documentElement.firstChild);
@@ -31,81 +31,81 @@ function svgMoveDefs(svgString) {
 }
 
 module.exports = function (gulp, plugins, consts) {
-    return function () {
-        var fs = require('fs');
-        var svgPolyfill = fs.readFileSync(plugins.path.join(consts.SRC, 'svg-polyfill.js'), "utf8");
-        var subjectIconsPath = plugins.path.join(consts.SRC, 'images', 'subjects', '*.svg');
-        var rankIconsPath = plugins.path.join(consts.SRC, 'images', 'ranks', '*.svg');
-        var iconsPath = plugins.path.join(consts.SRC, 'images', 'icons', '*.svg');
-        var destPath = plugins.path.join(consts.SRC, 'images');
+  return function () {
+    var fs = require('fs');
+    var svgPolyfill = fs.readFileSync(plugins.path.join(consts.SRC, 'svg-polyfill.js'), "utf8");
+    var subjectIconsPath = plugins.path.join(consts.SRC, 'images', 'subjects', '*.svg');
+    var rankIconsPath = plugins.path.join(consts.SRC, 'images', 'ranks', '*.svg');
+    var iconsPath = plugins.path.join(consts.SRC, 'images', 'icons', '*.svg');
+    var destPath = plugins.path.join(consts.SRC, 'images');
 
-        var subjectIconsConfig = {
-            mode: {
-                symbol: {
-                    sprite: '../subjects-icons.js'
-                }
-            },
-            shape: {
-                id: {
-                    generator: "icon-subject-%s"
-                },
-                transform: [{
-                    custom: svgSymbolCleanUp
-                }, 'svgo']
-            },
-            svg: {
-                transform: [svgAddPolyfill.bind(null, svgPolyfill)]
-            }
-        };
+    var subjectIconsConfig = {
+      mode: {
+        symbol: {
+          sprite: '../subjects-icons.js'
+        }
+      },
+      shape: {
+        id: {
+          generator: "icon-subject-%s"
+        },
+        transform: [{
+          custom: svgSymbolCleanUp
+        }, 'svgo']
+      },
+      svg: {
+        transform: [svgAddPolyfill.bind(null, svgPolyfill)]
+      }
+    };
 
-        var rankIconsConfig = {
-            mode: {
-                symbol: {
-                    sprite: '../rank-icons.js'
-                }
-            },
-            shape: {
-                id: {
-                    generator: "icon-rank-%s"
-                },
-                transform: [{
-                    custom: svgSymbolCleanUp
-                }, 'svgo']
-            },
-            svg: {
-                transform: [svgMoveDefs, svgAddPolyfill.bind(null, svgPolyfill)]
-            }
-        };
+    var rankIconsConfig = {
+      mode: {
+        symbol: {
+          sprite: '../rank-icons.js'
+        }
+      },
+      shape: {
+        id: {
+          generator: "icon-rank-%s"
+        },
+        transform: [{
+          custom: svgSymbolCleanUp
+        }, 'svgo']
+      },
+      svg: {
+        transform: [svgMoveDefs, svgAddPolyfill.bind(null, svgPolyfill)]
+      }
+    };
 
-        var iconsConfig = {
-            mode: {
-                symbol: {
-                    sprite: '../icons.js'
-                }
-            },
-            shape: {
-                id: {
-                    generator: "icon-%s"
-                },
-                transform: [{
-                    custom: svgSymbolCleanUp
-                }, 'svgo']
-            },
-            svg: {
-                transform: [svgAddPolyfill.bind(null, svgPolyfill)]
-            }
-        };
+    var iconsConfig = {
+      mode: {
+        symbol: {
+          sprite: '../icons.js'
+        }
+      },
+      shape: {
+        id: {
+          generator: "icon-%s"
+        },
+        transform: [{
+          custom: svgSymbolCleanUp
+        }, 'svgo']
+      },
+      svg: {
+        transform: [svgAddPolyfill.bind(null, svgPolyfill)]
+      }
+    };
 
-        //gulp.src(subjectIconsPath)
-        //    .pipe(plugins.svgSprite(subjectIconsConfig))
-        //    .pipe(gulp.dest(destPath));
+    gulp.src(subjectIconsPath)
+      .pipe(plugins.svgSprite(subjectIconsConfig))
+      .pipe(gulp.dest(destPath));
 
-        return gulp.src(rankIconsPath)
-            .pipe(plugins.svgSprite(rankIconsConfig))
-            .pipe(gulp.dest(destPath));
+    gulp.src(rankIconsPath)
+      .pipe(plugins.svgSprite(rankIconsConfig))
+      .pipe(gulp.dest(destPath));
 
-        //return gulp.src(iconsPath)
-        //    .pipe(plugins.svgSprite(iconsConfig))
-        //    .pipe(gulp.dest(destPath));
-    }
+    return gulp.src(iconsPath)
+      .pipe(plugins.svgSprite(iconsConfig))
+      .pipe(gulp.dest(destPath));
+  }
 };
--- src/svg-polyfill.js
+++ src/svg-polyfill.js
@@ -1,5 +1,5 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
     var svgWrapper = document.createElement('div');
     svgWrapper.innerHTML = '#SVG#';
     svgWrapper.style.height = '0';
