8e35bc572672e25970e22dadf281cfa21cd1af35
Konrad Dzwinel <kdzwinel@gmail.com>
2016-04-23 01:50:35 +0200

    Add ranks icons

--- scripts/tasks/svgs-generate.js
+++ scripts/tasks/svgs-generate.js
@@ -16,11 +16,26 @@ function svgAddPolyfill(svgPolyfill, svg) {
     return svgPolyfill.replace('#SVG#', svg.replace(/(\r\n|\n|\r)/gm,""));
 }
 
+function svgMoveDefs(svgString) {
+  var DOMParser = require('xmldom').DOMParser;
+  var svg = new DOMParser().parseFromString(svgString);
+  var defs = svg.documentElement.getElementsByTagName('defs');
+
+  for(var i = 0; i < defs.length; i++) {
+    var def = defs[i];
+    def.parentNode.removeChild(def);
+    svg.documentElement.insertBefore(def, svg.documentElement.firstChild);
+  }
+
+  return svg.toString();
+}
+
 module.exports = function (gulp, plugins, consts) {
     return function () {
         var fs = require('fs');
         var svgPolyfill = fs.readFileSync(plugins.path.join(consts.SRC, 'svg-polyfill.js'), "utf8");
         var subjectIconsPath = plugins.path.join(consts.SRC, 'images', 'subjects', '*.svg');
+        var rankIconsPath = plugins.path.join(consts.SRC, 'images', 'ranks', '*.svg');
         var iconsPath = plugins.path.join(consts.SRC, 'images', 'icons', '*.svg');
         var destPath = plugins.path.join(consts.SRC, 'images');
 
@@ -36,10 +51,29 @@ module.exports = function (gulp, plugins, consts) {
                 },
                 transform: [{
                     custom: svgSymbolCleanUp
-                }]
+                }, 'svgo']
             },
             svg: {
                 transform: [svgAddPolyfill.bind(null, svgPolyfill)]
+            }
+        };
+
+        var rankIconsConfig = {
+            mode: {
+                symbol: {
+                    sprite: '../rank-icons.js'
+                }
+            },
+            shape: {
+                id: {
+                    generator: "icon-rank-%s"
+                },
+                transform: [{
+                    custom: svgSymbolCleanUp
+                }, 'svgo']
+            },
+            svg: {
+                transform: [svgMoveDefs, svgAddPolyfill.bind(null, svgPolyfill)]
             }
         };
 
@@ -55,19 +89,23 @@ module.exports = function (gulp, plugins, consts) {
                 },
                 transform: [{
                     custom: svgSymbolCleanUp
-                }]
+                }, 'svgo']
             },
             svg: {
                 transform: [svgAddPolyfill.bind(null, svgPolyfill)]
             }
         };
 
-        gulp.src(subjectIconsPath)
-            .pipe(plugins.svgSprite(subjectIconsConfig))
+        //gulp.src(subjectIconsPath)
+        //    .pipe(plugins.svgSprite(subjectIconsConfig))
+        //    .pipe(gulp.dest(destPath));
+
+        return gulp.src(rankIconsPath)
+            .pipe(plugins.svgSprite(rankIconsConfig))
             .pipe(gulp.dest(destPath));
 
-        return gulp.src(iconsPath)
-            .pipe(plugins.svgSprite(iconsConfig))
-            .pipe(gulp.dest(destPath));
+        //return gulp.src(iconsPath)
+        //    .pipe(plugins.svgSprite(iconsConfig))
+        //    .pipe(gulp.dest(destPath));
     }
 };
--- src/svg-polyfill.js
+++ src/svg-polyfill.js
@@ -1,8 +1,9 @@
 (function() {
   document.addEventListener('DOMContentLoaded', function() {
     var svgWrapper = document.createElement('div');
-    svgWrapper.style.display = 'none';
     svgWrapper.innerHTML = '#SVG#';
+    svgWrapper.style.height = '0';
+    svgWrapper.style.overflow = 'hidden';
     document.body.appendChild(svgWrapper);
   })
 })();
