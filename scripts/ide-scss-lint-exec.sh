d07f43635b95c9f23c60026a30c8d932cea58e05
Andrzej Maciurzynski <andrzej.maciurzynski@brainly.com>
2016-04-26 09:27:33 +0200

    Update tools configs

--- scripts/ide-scss-lint-exec.sh
+++ scripts/ide-scss-lint-exec.sh
@@ -8,4 +8,4 @@ docker run --rm \
   -v $PROJECT_DIR/src:/style-guide/src \
   -v $PROJECT_DIR/scripts:/style-guide/scripts \
   -v $PROJECT_DIR/gulpfile.js:/style-guide/gulpfile.js \
-  brainly/style-guide scss-lint $1 --format JSON
+  brainly/style-guide scss-lint -c node_modules/frontend-tools-configs/.scss-lint.yml $1 --format JSON
