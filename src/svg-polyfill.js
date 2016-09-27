ed5d5ab394a166e5c6e04fdfed36b9043b965649
Konrad Dzwinel <kdzwinel@gmail.com>
2016-09-27 10:58:56 +0200

    Revert "Replace existing external SVG polyfill with svg4everybody"

---
+++ src/svg-polyfill.js
@@ -0,0 +1,8 @@
+(function() {
+  document.addEventListener('DOMContentLoaded', function() {
+    var svgWrapper = document.createElement('div');
+    svgWrapper.style.display = 'none';
+    svgWrapper.innerHTML = '#SVG#';
+    document.body.appendChild(svgWrapper);
+  })
+})();
