d86cb71054ad66c9fb128434631b4fcaebdfd352
Konrad Dzwinel <kdzwinel@gmail.com>
2016-09-29 12:17:45 +0200

    Put icons on top and load them with async. Enable SVGO to optimize svg images

--- src/svg-polyfill.js
+++ src/svg-polyfill.js
@@ -1,8 +1,6 @@
 (function() {
-  document.addEventListener('DOMContentLoaded', function() {
-    var svgWrapper = document.createElement('div');
-    svgWrapper.style.display = 'none';
-    svgWrapper.innerHTML = '#SVG#';
-    document.body.appendChild(svgWrapper);
-  })
+  var svgWrapper = document.createElement('div');
+  svgWrapper.style.display = 'none';
+  svgWrapper.innerHTML = '#SVG#';
+  document.body.insertBefore(svgWrapper, document.body.firstChild);
 })();
