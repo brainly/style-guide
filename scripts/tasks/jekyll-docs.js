3aa6bf92bf9a8f4831c36f31e7ac3013ac4e0955
Andrzej Maciurzynski <andrzej.maciurzynski@brainly.com>
2016-10-24 08:41:20 +0200

    Remove symlink option

--- scripts/tasks/jekyll-docs.js
+++ scripts/tasks/jekyll-docs.js
@@ -3,8 +3,6 @@ module.exports = function (gulp, plugins, consts) {
         var spawn = require('child_process').spawn;
         var docsOutputPath = plugins.path.join(consts.VERSIONED_DIST, 'docs');
 
-
-
         var jekyll = spawn('jekyll', ['build', '--config', 'src/docs/_config.yml', '--source', 'src/docs', '-d', docsOutputPath], {stdio: 'inherit'});
         jekyll.on('exit', function (code) {
             done(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
