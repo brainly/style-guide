e2966547ff005820d73756f9dd5327450ee14864
Patrycja Radaczyńska <trycjapa@gmail.com>
2016-12-08 14:10:06 +0100

    Topbar improvement - changes for search component. (#875)

--- scripts/tasks/watch-docs-templates.js
+++ scripts/tasks/watch-docs-templates.js
@@ -1,9 +1,10 @@
 module.exports = function (gulp, plugins, consts) {
     return function (done) {
         var docsSources = plugins.path.join(consts.DOCS, '**', '*.{html,yml}');
+        var componentsTemplatesSources = plugins.path.join(consts.COMPONENTS, '**', '*.html');
 
         plugins.livereload.listen();
-        return gulp.watch([docsSources], function () {
+        return gulp.watch([docsSources, componentsTemplatesSources], function () {
             plugins.runSequence('jekyll:docs', 'docs:copy-components', 'fingerprint-replace', 'index-fingerprint-replace');
         });
     }
