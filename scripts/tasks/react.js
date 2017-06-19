6c1cf59fa43378a00869cd67898f6490aad46a8e
Mateusz Wyka <matikrk@gmail.com>
2017-06-19 13:33:05 +0200

    React components in SG fast version with webpack full working with cleaning files

--- scripts/tasks/react.js
+++ scripts/tasks/react.js
@@ -1,43 +1,76 @@
-const webpack = require('webpack-stream');
+const webpack = require('webpack');
 const ReactDOMServer = require('react-dom/server');
 const React = require('react');
 const through = require('through2');
 const path = require('path');
 const named = require('vinyl-named');
-const vm = require('vm');
 const rename = require("gulp-rename");
+const fs = require('fs');
+
+
+const nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(dir => dir !== '.bin')
+  .forEach(mod => nodeModules[mod] = 'commonjs ' + mod);
+
+
+const coreConfig = {
+  target: 'node',
+  module: {
+    loaders: [
+      {
+        test: /\.js|jsx?$/,
+        loader: 'babel-loader'
+      },
+    ],
+  },
+  externals: nodeModules
+};
 
 
 module.exports = function(gulp, plugins, consts) {
   return function() {
     const componentsHtml = plugins.path.join(consts.COMPONENTS, '/**/pages/*.jsx');
-    // const docsOutputPath = plugins.path.join(consts.VERSIONED_DIST, 'docs');
-    const docsOutputPath = plugins.path.join(consts.VERSIONED_DIST, 'docs/generatedComponents');
+    const docsOutputPath = plugins.path.join(consts.VERSIONED_DIST, 'docs');
 
     return gulp.src(componentsHtml, {base: consts.SRC})
-      .pipe(named())
-      .pipe(webpack({
-        module: {
-          loaders: [
-            {
-              test: /\.js|jsx?$/,
-              loader: 'babel-loader'
-            },
-          ],
-        },
+      .pipe(through.obj((file, enc, cb) => {
+        const pathArray = file.path.replace(consts.SRC, '').split('/');
+        const fileNameWithExtension = pathArray.pop();
+        const relativePath = pathArray.join('/');
+        const jsPath = plugins.path.join(consts.VERSIONED_DIST, 'docs', relativePath)
+
+        const config = Object.assign({}, coreConfig, {
+          entry: {
+            [fileNameWithExtension]: file.path
+          },
+          output: {
+            filename: "[name]",
+            path: jsPath,
+            libraryTarget: 'commonjs2'
+          }
+        });
+
+        webpack(config, function(err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          cb(null, file)
+        });
+
+
       }))
       .pipe(through.obj((file, enc, cb) => {
-        // console.log(arguments)
-        // console.log(file)
-        // console.dir(file)
-        const fileString = file.contents.toString(enc);
-        const script = new vm.Script('bundle=' + fileString);
-        script.runInThisContext();
-        const ReactPageClass = bundle.default; //bundle from running script
+        const path = file.path.replace(consts.SRC, consts.VERSIONED_DIST + '/docs');
+        const ReactPageClass = require(path).default;
         const htmlPage = ReactDOMServer.renderToStaticMarkup(React.createElement(ReactPageClass));
+
+        fs.unlinkSync(path);
 
         file.contents = new Buffer(htmlPage);
         cb(null, file);
+
       }))
       .pipe(rename(function(path) {
         path.extname = ".html"
