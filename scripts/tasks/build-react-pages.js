f848a60f0833843423225300c72347e11ef1c4b0
Mateusz Wyka <matikrk@gmail.com>
2017-06-19 17:03:42 +0200

    React components in SG refactor

--- scripts/tasks/build-react-pages.js
+++ scripts/tasks/build-react-pages.js
@@ -5,6 +5,7 @@ const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 const rename = require('gulp-rename');
 
+// create external modules for webpack to not include it in bundles.
 const nodeModules = {};
 fs.readdirSync('node_modules')
   .filter(dir => dir !== '.bin')
@@ -23,54 +24,58 @@ const coreConfig = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
- externals: nodeModules
+  externals: nodeModules
 };
 
 module.exports = function(gulp, plugins, consts) {
+  const createWebpackBundles = function(file, enc, cb) {
+    const pathArray = file.path.replace(consts.SRC, '').split('/');
+    const fileNameWithExtension = pathArray.pop();
+    const relativePath = pathArray.join('/');
+    const jsPath = plugins.path.join(consts.VERSIONED_DIST, 'docs', relativePath);
+
+    const config = Object.assign({}, coreConfig, {
+      entry: {
+        [fileNameWithExtension]: file.path
+      },
+      output: {
+        filename: '[name]',
+        path: jsPath,
+        libraryTarget: 'commonjs2'
+      }
+    });
+
+    webpack(config, function(err) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      cb(null, file)
+    });
+  };
+
+  const createHtmlFiles = function(file, enc, cb) {
+    const path = file.path.replace(consts.SRC, consts.VERSIONED_DIST + '/docs');
+    const ReactPageClass = require(path).default;
+    const htmlPage = ReactDOMServer.renderToStaticMarkup(React.createElement(ReactPageClass));
+
+    file.contents = new Buffer(htmlPage);
+    cb(null, file);
+  };
+
+
   return function() {
     const componentsHtml = plugins.path.join(consts.COMPONENTS, '/**/pages/*.jsx');
     const docsOutputPath = plugins.path.join(consts.VERSIONED_DIST, 'docs');
 
     return gulp.src(componentsHtml, {base: consts.SRC})
-      .pipe(through.obj((file, enc, cb) => {
-        const pathArray = file.path.replace(consts.SRC, '').split('/');
-        const fileNameWithExtension = pathArray.pop();
-        const relativePath = pathArray.join('/');
-        const jsPath = plugins.path.join(consts.VERSIONED_DIST, 'docs', relativePath);
-
-        const config = Object.assign({}, coreConfig, {
-          entry: {
-            [fileNameWithExtension]: file.path
-          },
-          output: {
-            filename: '[name]',
-            path: jsPath,
-            libraryTarget: 'commonjs2'
-          }
-        });
-
-        webpack(config, function(err) {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          cb(null, file)
-        });
-
-
-      }))
-      .pipe(through.obj((file, enc, cb) => {
-        const path = file.path.replace(consts.SRC, consts.VERSIONED_DIST + '/docs');
-        const ReactPageClass = require(path).default;
-        const htmlPage = ReactDOMServer.renderToStaticMarkup(React.createElement(ReactPageClass));
-
-        file.contents = new Buffer(htmlPage);
-        cb(null, file);
-
-      }))
-      .pipe(rename(function(path) {
-        path.extname = '.html';
-      }))
+      .pipe(through.obj(createWebpackBundles))
+      .pipe(through.obj(createHtmlFiles))
+      .pipe(rename(
+        function(path) {
+          path.extname = '.html';
+        })
+      )
       .pipe(gulp.dest(docsOutputPath));
   }
 };
