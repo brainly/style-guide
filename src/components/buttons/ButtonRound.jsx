0cf95cff503bd65e37c26c2408971374e289b962
Mateusz Wyka <matikrk@gmail.com>
2017-06-22 15:51:15 +0200

    Create button react component * rounded button label upgrade & tests, tests, tests

--- src/components/buttons/ButtonRound.jsx
+++ src/components/buttons/ButtonRound.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ButtonRound = ({label, children, href = '#', onClick = () => undefined, ...props}) =>
-  <a {...props} href={href} onClick={onClick} className="sg-button-primary-round">
-    <div className="sg-button-primary-round__icon">
-      {children}
-    </div>
-    <span className="sg-button-primary-round__label">{label}</span>
-  </a>;
+const ButtonRound = ({label, children, href = '#', onClick = () => undefined, ...props}) => {
+  let labelElem;
+
+  if (label) {
+    labelElem = <span className="sg-button-primary-round__label">{label}</span>;
+  }
+
+  return (
+    <a {...props} href={href} onClick={onClick} className="sg-button-primary-round">
+      <div className="sg-button-primary-round__icon">
+        {children}
+      </div>
+      {labelElem}
+    </a>
+  );
+};
 
 
 ButtonRound.propTypes = {
