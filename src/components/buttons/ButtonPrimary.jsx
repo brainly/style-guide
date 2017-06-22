03ecca72adcb58a21bb2637a3566b8a047a08c2d
Mateusz Wyka <matikrk@gmail.com>
2017-06-22 11:07:31 +0200

    Create button react component * ico in primary * types in primary * define contrastBlockCssClass in DocsBlock.jsx * create examples for primary

--- src/components/buttons/ButtonPrimary.jsx
+++ src/components/buttons/ButtonPrimary.jsx
@@ -2,21 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const ButtonPrimary = ({disabled, type, children, wide, ...props}) => {
+
+const types = {
+  alt: 'alt',
+  dark: 'dark',
+  inverse: 'inverse',
+  alt_inverse: 'alt-inverse',
+  dark_inverse: 'dark-inverse',
+  fb: 'fb'
+};
+
+
+const ButtonPrimary = ({disabled, icon, type, children, wide, ...props}) => {
   const btnClass = classNames('sg-button-primary', {
     'sg-button-primary--disabled': disabled,
     'sg-button-primary--full-width': wide,
     [`sg-button-primary--${type}`]: type
   });
 
-  return <button {...props} disabled={disabled} className={btnClass}>{children}</button>;
+  let ico;
+
+  if (icon) {
+    ico = <span className="sg-button-primary__icon">{icon}</span>;
+  }
+
+  return <button {...props} disabled={disabled} className={btnClass}>
+    {ico}
+    {children}
+  </button>;
 };
 
 ButtonPrimary.propTypes = {
   children: PropTypes.node.isRequired,
+  icon: PropTypes.node,
   wide: PropTypes.bool,
   disabled: PropTypes.bool,
-  type: PropTypes.oneOf(['alt', 'dark', 'alt-inverse', 'dark-inverse', 'fb'])
+  type: PropTypes.oneOf(Object.values(types))
 };
 
 export default ButtonPrimary;
+export {types};
--- src/components/buttons/pages/buttons2.jsx
+++ src/components/buttons/pages/buttons2.jsx
@@ -1,11 +1,58 @@
-//TODO delete this later, this file is only example
-
 import React from 'react';
-import ButtonPrimary from '../ButtonPrimary';
+import ButtonPrimary, {types} from '../ButtonPrimary';
+import DocsBlock, {contrastBlockCssClass} from '../../../docs/DocsBlock';
+
 
 const Buttons = () =>
   <div>
-    <ButtonPrimary type='dark'>Sample text</ButtonPrimary>
+    <DocsBlock info='Primary buttons'>
+      <ButtonPrimary>
+        Add your answer
+      </ButtonPrimary>
+      <ButtonPrimary type={types.alt}>
+        Ask your question
+      </ButtonPrimary>
+      <ButtonPrimary type={types.dark}>
+        Search
+      </ButtonPrimary>
+      <ButtonPrimary disabled={true}>
+        Disabled
+      </ButtonPrimary>
+
+      <br/>
+      <br/>
+
+      <ButtonPrimary
+        icon={
+          <svg className="sg-icon sg-icon--x16 sg-icon--adaptive">
+            <use xlinkHref="#icon-fb"></use>
+          </svg>
+        } type={types.fb}>
+        Login with Facebook
+      </ButtonPrimary>
+    </DocsBlock>
+
+    <DocsBlock info="Primary buttons wide">
+      <ButtonPrimary wide={true}>Search</ButtonPrimary>
+    </DocsBlock>
+
+    <DocsBlock info="Primary buttons inverted">
+      <div className={contrastBlockCssClass}>
+        <ButtonPrimary type={types.inverse}>
+          Add your answer
+        </ButtonPrimary>
+        <ButtonPrimary type={types.alt_inverse}>
+          Ask your question
+        </ButtonPrimary>
+        <ButtonPrimary type={types.dark_inverse}>
+          Search
+        </ButtonPrimary>
+        <ButtonPrimary disabled={true} type={types.inverse}>
+          Disabled
+        </ButtonPrimary>
+      </div>
+    </DocsBlock>
+
   </div>;
 
 export default Buttons;
