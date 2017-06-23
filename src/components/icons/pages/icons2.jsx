28a99f6342b1fbf7133c009fc215d299121ff6ef
Mateusz Wyka <matikrk@gmail.com>
2017-06-23 16:33:19 +0200

    Icon react component * starting create icons examples * upgrade DocsBlock.jsx

---
+++ src/components/icons/pages/icons2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import DocsBlock, {contrastBlockCssClass} from '../../../docs/DocsBlock';
+
+
+const icons = () => <div>
+  <DocsBlock >
+    <div className={contrastBlockCssClass}></div>
+  </DocsBlock>
+</div>;
+
+export default icons;
