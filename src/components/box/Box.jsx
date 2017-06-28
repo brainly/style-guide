17e124ee081271eb6a77b39e5205f7a7dc002f86
matikrk <matikrk@gmail.com>
2017-06-28 14:50:28 +0200

    create Box component (#1020)

---
+++ src/components/box/Box.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const colors = {
+  blue: 'blue',
+  lavender: 'lavender',
+  dark: 'dark',
+  mint: 'mint',
+  mint_secondary: 'mint-secondary',
+  navyblue_secondary: 'navyblue-secondary',
+  blue_secondary_light: 'blue-secondary-light'
+};
+
+//todo implement
+const paddings = {
+  small: 'small-padding',
+  large: 'large-padding'
+};
+
+const Box = ({color, padding, full, children, border = !color, imgSrc, noMinHeight}) => {
+  const boxClass = classNames('sg-box', {
+    [`sg-box--${color}`]: color,
+    'sg-box--no-border': !border,
+    'sg-box--full': full,
+    [`sg-box--${padding}`]: padding,
+    'sg-box--image-wrapper': imgSrc,
+    'sg-box--no-min-height': noMinHeight
+  });
+
+  let content;
+
+  if (imgSrc) {
+    content = <img className="sg-box__image" src={imgSrc}/>;
+  } else {
+    content = <div className="sg-box__hole">{children}</div>;
+  }
+
+  return <div className={boxClass}>
+    {content}
+  </div>;
+};
+
+Box.propTypes = {
+  children: PropTypes.node,
+  color: PropTypes.oneOf(Object.values(colors)),
+  border: PropTypes.bool,
+  noMinHeight: PropTypes.bool,
+  full: PropTypes.bool,
+  padding: PropTypes.oneOf(Object.values(paddings)),
+  imgSrc: PropTypes.string
+};
+
+export default Box;
+export {colors, paddings};
---
+++ src/components/box/box.spec.jsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import Box, {colors, paddings} from './Box';
+import {shallow} from 'enzyme';
+
+test('render', () => {
+  const box = shallow(
+    <Box>some text</Box>
+  );
+
+  expect(box.hasClass('sg-box')).toEqual(true);
+});
+
+
+
+test('colors', () => {
+  const color = colors.lavender;
+  const box = shallow(
+    <Box color={color}>some text</Box>
+  );
+
+  expect(box.hasClass(`sg-box--${color}`)).toEqual(true);
+});
+
+
+test('border', () => {
+  const boxComponent = <Box border={true}>some text</Box>;
+  const box = shallow(boxComponent);
+
+  expect(boxComponent.props.border).toEqual(true);
+  expect(box.hasClass('sg-box--no-border')).toEqual(false);
+});
+
+test('border', () => {
+  const boxComponent = <Box border={false}>some text</Box>;
+  const box = shallow(boxComponent);
+
+  expect(boxComponent.props.border).toEqual(false);
+  expect(box.hasClass('sg-box--no-border')).toEqual(true);
+});
+
+test('no colors => default border on ', () => {
+  const boxComponent = <Box>some text</Box>;
+  const box = shallow(boxComponent);
+
+  expect(box.hasClass('sg-box--no-border')).toEqual(false);
+});
+
+test(' colors => default border off', () => {
+  const color = colors.lavender;
+  const box = shallow(
+    <Box color={color}>some text</Box>
+  );
+
+  expect(box.hasClass('sg-box--no-border')).toEqual(true);
+});
+
+test('default padding', () => {
+  const box = shallow(
+    <Box>some text</Box>
+  );
+
+  expect(box.hasClass(`sg-box--${paddings.large}`)).toEqual(false);
+  expect(box.hasClass(`sg-box--${paddings.small}`)).toEqual(false);
+});
+
+test('small padding', () => {
+  const padding = paddings.small;
+  const box = shallow(
+    <Box padding={padding}>some text</Box>
+  );
+
+  expect(box.hasClass(`sg-box--${paddings.small}`)).toEqual(true);
+});
+
+
+test('large padding', () => {
+  const padding = paddings.large;
+  const box = shallow(
+    <Box padding={padding}>some text</Box>
+  );
+
+  expect(box.hasClass(`sg-box--${paddings.large}`)).toEqual(true);
+});
+
+test('no min height', () => {
+  const box = shallow(
+    <Box noMinHeight={true}>some text</Box>
+  );
+
+  expect(box.hasClass('sg-box--no-min-height')).toEqual(true);
+});
+
+test('full width', () => {
+  const box = shallow(
+    <Box full={true}>some text</Box>
+  );
+
+  expect(box.hasClass('sg-box--full')).toEqual(true);
+});
+
+
+test('image container', () => {
+  const imgSrc = 'https://source.unsplash.com/100x100/?man';
+
+  const box = shallow(
+    <Box imgSrc={imgSrc}/>
+  );
+
+  expect(box.hasClass('sg-box--image-wrapper')).toEqual(true);
+  expect(box.find('.sg-box__image')).toHaveLength(1);
+  expect(box.find('.sg-box__hole')).toHaveLength(0);
+});
+
+test('standard box, no image container', () => {
+  const box = shallow(<Box>some text</Box>);
+
+  expect(box.hasClass('sg-box--image-wrapper')).toEqual(false);
+  expect(box.find('.sg-box__image')).toHaveLength(0);
+  expect(box.find('.sg-box__hole')).toHaveLength(1);
+});
