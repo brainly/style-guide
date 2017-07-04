0b452840d4b73513a6cff8141c749d8a629114a4
matikrk <matikrk@gmail.com>
2017-07-04 14:44:21 +0200

    Rwd helpers react (#1066)

---
+++ src/components/helpers/RwdHelper.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const TYPE = {
+  SMALL_ONLY: 'small-only',
+  MEDIUM_ONLY: 'medium-only',
+  MEDIUM_DOWN: 'medium-down',
+  MEDIUM_UP: 'medium-up',
+  LARGE_ONLY: 'large-only'
+};
+
+const RwdHelper = ({hide, children}) => {
+  if (!children) {
+    return null;
+  }
+
+  const hideClass = `sg-hide-for-${hide}`;
+
+  if (typeof children === 'string') {
+    return <span className={hideClass}>{children}</span>;
+  }
+
+  const finalClassName = classNames(children.props.className, hideClass);
+
+  return React.cloneElement(children, {className: finalClassName});
+};
+
+RwdHelper.propTypes = {
+  // One child only !!!
+  children: PropTypes.oneOfType([PropTypes.element, PropTypes.string]).isRequired,
+  hide: PropTypes.oneOf(Object.values(TYPE)).isRequired
+};
+
+export default RwdHelper;
+export {TYPE};
---
+++ src/components/helpers/pages/rwd.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import RwdHelper, {TYPE} from '../RwdHelper';
+import DocsBlock from '../../../docs/DocsBlock';
+import Icon, {types as icoTypes, colors} from '../../icons/Icon';
+
+const RwdHelpers = () =>
+  <div>
+    <DocsBlock info='Top (middle)' additionalInfo='--top'>
+      <ul>
+        <li>
+          <RwdHelper hide={TYPE.SMALL_ONLY}>
+            <span>
+              <Icon type={icoTypes.heart} color={colors.peach} size={14}/>
+            </span>
+          </RwdHelper>
+          - hidden for small screens
+        </li>
+
+        <li>
+          <RwdHelper hide={TYPE.MEDIUM_ONLY}>
+            <span>
+              <Icon type={icoTypes.heart} color={colors.peach} size={14}/>
+            </span>
+          </RwdHelper>
+          - hidden for medium screens
+        </li>
+
+        <li>
+          <RwdHelper hide={TYPE.MEDIUM_DOWN}>
+            <span>
+              <Icon type={icoTypes.heart} color={colors.peach} size={14}/>
+            </span>
+          </RwdHelper>
+          - hidden for small and medium screens
+        </li>
+
+        <li>
+          <RwdHelper hide={TYPE.MEDIUM_UP}>
+            <span>
+              <Icon type={icoTypes.heart} color={colors.peach} size={14}/>
+            </span>
+          </RwdHelper>
+          - hidden for medium and large screens
+        </li>
+
+        <li>
+          <RwdHelper hide={TYPE.LARGE_ONLY}>
+            <span>
+              <Icon type={icoTypes.heart} color={colors.peach} size={14}/>
+            </span>
+          </RwdHelper>
+          - hidden for large screens
+        </li>
+      </ul>
+    </DocsBlock>
+
+  </div>;
+
+export default RwdHelpers;
