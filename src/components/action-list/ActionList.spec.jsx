df09914e3b41691f3bd0f6a594c0c2dba2f42d33
Mateusz Zimowski <mat.zimowski@gmail.com>
2017-07-07 14:44:32 +0200

    Add actionList react component (#1070)
    * Add actionList react component
    
    * Update DIRECTION
    
    * Add ActionListHole component
    
    * Add ActionListHole component
    
    * Add --narrow modifier to docs-block
    
    * Avatar refactor
    * ActionListHole as standalone element
    
    * 85.11.0

---
+++ src/components/action-list/ActionList.spec.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import ActionList, {DIRECTION} from './ActionList';
+import ActionListHole from './ActionListHole';
+import {shallow} from 'enzyme';
+import ButtonSecondary, {types as buttonSecondaryTypes} from '../buttons/ButtonSecondary';
+import ButtonPrimary, {types as buttonPrimaryTypes} from '../buttons/ButtonPrimary';
+
+describe('ActionList', () => {
+  test('render', () => {
+    const actionList = shallow(
+      <ActionList>
+        <ActionListHole>
+          <ButtonSecondary type={buttonSecondaryTypes.alt} small={true}>
+            accept
+          </ButtonSecondary>
+        </ActionListHole>
+        <ActionListHole>
+          <ButtonPrimary type={buttonPrimaryTypes.dark_inverse}>
+            Later
+          </ButtonPrimary>
+        </ActionListHole>
+      </ActionList>
+    );
+
+    expect(actionList.hasClass('sg-actions-list')).toEqual(true);
+  });
+
+
+  test('to-right', () => {
+    const actionList = shallow(
+      <ActionList direction={DIRECTION.TO_RIGHT}></ActionList>
+    );
+
+    expect(actionList.hasClass('sg-actions-list--to-right')).toEqual(true);
+  });
+
+  test('to-top', () => {
+    const actionList = shallow(
+      <ActionList direction={DIRECTION.TO_TOP}></ActionList>
+    );
+
+    expect(actionList.hasClass('sg-actions-list--to-top')).toEqual(true);
+  });
+
+  test('centered', () => {
+    const actionList = shallow(
+      <ActionList direction={DIRECTION.CENTERED}></ActionList>
+    );
+
+    expect(actionList.hasClass('sg-actions-list--centered')).toEqual(true);
+  });
+
+  test('space-between', () => {
+    const actionList = shallow(
+      <ActionList direction={DIRECTION.SPACE_BETWEEN}></ActionList>
+    );
+
+    expect(actionList.hasClass('sg-actions-list--space-between')).toEqual(true);
+  });
+
+  test('no-wrap', () => {
+    const actionList = shallow(
+      <ActionList noWrap={true}></ActionList>
+    );
+
+    expect(actionList.hasClass('sg-actions-list--no-wrap')).toEqual(true);
+  });
+});
+
+describe('ActionListHole', () => {
+  test('render', () => {
+    const actionListHole = shallow(
+      <ActionListHole>
+        <ButtonSecondary type={buttonSecondaryTypes.alt} small={true}>
+          accept
+        </ButtonSecondary>
+      </ActionListHole>
+    );
+
+    expect(actionListHole.hasClass('sg-actions-list__hole')).toEqual(true);
+  });
+});
+
