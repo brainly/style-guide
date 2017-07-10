54d136f116f289f3e0b2cb457faf3cd4c015ab75
Konrad Dzwinel <kdzwinel@gmail.com>
2017-07-10 21:09:27 +0200

    Avatar fixes
    Do not allow changing icon or color of icon in the avatar.
    Fix icon sizes and color of default avatar.

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Icon, {TYPE as ICON_TYPE, COLOR as ICON_COLOR} from '../icons/Icon';
 
+const DEFAULT_ICON = ICON_TYPE.PROFILE;
+const DEFAULT_COLOR = ICON_COLOR.GRAY;
+const BORDER_SIZE = 2;
 const SIZE = {
   XSMALL: {className: 'xsmall', iconSize: 16},
   SMALL: {className: 'small', iconSize: 24},
@@ -13,39 +16,19 @@ const SIZE = {
   XXXLARGE: {className: 'xxxlarge', iconSize: 120}
 };
 
-const AvatarContent = ({imgSrc, iconType, iconColor, iconSize}) => {
-  if (imgSrc) {
-    return <img className="sg-avatar__image" src={imgSrc}/>;
-  }
-
-  if (iconType) {
-    return <div className="sg-avatar__image sg-avatar__image--icon">
-      <Icon type={iconType} color={iconColor} size={iconSize}/>
-    </div>;
-  }
-
-  console.error('Define imgSrc or iconType in Avatar component');
-  return null;
-};
-
-AvatarContent.propTypes = {
-  imgSrc: PropTypes.string,
-  iconSize: PropTypes.oneOf(Object.values(SIZE).map(size => size.iconSize)),
-  iconType: PropTypes.oneOf(Object.values(ICON_TYPE)),
-  iconColor: PropTypes.oneOf(Object.values(ICON_COLOR))
-};
-
-const Avatar = ({size = SIZE.NORMAL, border, spaced, ...contentProps}) => {
+const Avatar = ({size = SIZE.NORMAL, border = false, spaced, imgSrc}) => {
   const avatarClass = classNames('sg-avatar', {
     [`sg-avatar--${size.className}`]: size !== SIZE.normal,
     'sg-avatar--with-border': border,
     'sg-avatar--spaced': spaced
   });
 
-  contentProps.iconSize = size.iconSize;
+  const defaultAvatar = <div className="sg-avatar__image sg-avatar__image--icon">
+    <Icon type={DEFAULT_ICON} color={DEFAULT_COLOR} size={border ? size.iconSize - BORDER_SIZE : size.iconSize}/>
+  </div>;
 
   return <div className={avatarClass}>
-    <AvatarContent {...contentProps}/>
+    {imgSrc ? <img className="sg-avatar__image" src={imgSrc}/> : defaultAvatar}
   </div>;
 };
 
@@ -53,10 +36,8 @@ Avatar.propTypes = {
   size: PropTypes.oneOf(Object.values(SIZE)),
   border: PropTypes.bool,
   spaced: PropTypes.bool,
-  imgSrc: PropTypes.string,
-  iconType: PropTypes.oneOf(Object.values(ICON_TYPE)),
-  iconColor: PropTypes.oneOf(Object.values(ICON_COLOR))
+  imgSrc: PropTypes.string
 };
 
 export default Avatar;
-export {SIZE, ICON_TYPE, ICON_COLOR};
+export {SIZE};
