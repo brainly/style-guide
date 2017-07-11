ebf36d29f345380b46dc5b5e9a193df0a8c0725c
Konrad Dzwinel <kdzwinel@gmail.com>
2017-07-11 12:04:37 +0200

    Avatar fixes (#1091)
    * Avatar fixes
    
    Do not allow changing icon or color of icon in the avatar.
    Fix icon sizes and color of default avatar.
    
    * Bump version.

--- src/components/avatar/avatar.spec.jsx
+++ src/components/avatar/avatar.spec.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Avatar, {SIZE, ICON_TYPE, ICON_COLOR} from './Avatar';
+import Avatar, {SIZE} from './Avatar';
 import Icon from '../icons/Icon';
 
 import {shallow, mount} from 'enzyme';
 
-test('render avatar with Icon', () => {
-  const avatar = mount(<Avatar iconType={ICON_TYPE.FRIENDS}/>);
+test('render default', () => {
+  const avatar = mount(<Avatar />);
 
   expect(avatar.find('.sg-avatar__image')).toHaveLength(1);
   expect(avatar.find('img')).toHaveLength(0);
@@ -28,36 +28,10 @@ test('render with image', () => {
   expect(avatar.find(Icon)).toHaveLength(0);
 });
 
-test('error when render without image & icon', () => {
-  const spy = jest.spyOn(console, 'error');
-
-  console.error = jest.fn();
-  mount(<Avatar/>);
-  expect(console.error.mock.calls).toHaveLength(1);
-
-  spy.mockRestore();
-});
-
-
-test('icon pass properties to Icon', () => {
-  const iconColor = ICON_COLOR.MUSTARD;
-  const iconType = ICON_TYPE.FRIENDS;
-  const avatar = mount(<Avatar iconType={iconType} iconColor={iconColor}/>);
-
-  expect(avatar.find(Icon)).toHaveLength(1);
-
-  const icoProps = avatar.find(Icon).props();
-
-  expect(icoProps.color).toEqual(iconColor);
-  expect(icoProps.type).toEqual(iconType);
-  expect(icoProps.size).not.toEqual(undefined);
-});
-
-
 test('SIZE', () => {
   const size = SIZE.XLARGE;
   const avatar = shallow(
-    <Avatar size={size} iconType={ICON_TYPE.FRIENDS}/>
+    <Avatar size={size}/>
   );
 
   expect(avatar.hasClass('sg-avatar--xlarge')).toEqual(true);
@@ -65,7 +39,7 @@ test('SIZE', () => {
 
 test('border', () => {
   const avatar = shallow(
-    <Avatar border={true} iconType={ICON_TYPE.FRIENDS}/>
+    <Avatar border={true}/>
   );
 
   expect(avatar.hasClass('sg-avatar--with-border')).toEqual(true);
@@ -73,7 +47,7 @@ test('border', () => {
 
 test('spaced', () => {
   const avatar = shallow(
-    <Avatar spaced={true} iconType={ICON_TYPE.FRIENDS}/>
+    <Avatar spaced={true}/>
   );
 
   expect(avatar.hasClass('sg-avatar--spaced')).toEqual(true);
