dfc12a3668c0955a6408325f308b7399e6f8b3da
anna-wro <wro.online@gmail.com>
2017-07-11 16:30:04 +0200

    more accurate tests

--- src/components/badges/badge.spec.jsx
+++ src/components/badges/badge.spec.jsx
@@ -1,40 +1,47 @@
 import React from 'react';
 import Badge, {COLOR, SIZE} from './Badge';
-import Text from '../text/Text';
+import Text, {SIZE as TEXT_SIZE, COLOR as TEXT_COLOR} from '../text/Text';
 import {shallow, mount} from 'enzyme';
 
 test('render', () => {
   const badge = shallow(
     <Badge>Test</Badge>
   );
+  const text = badge.find(Text);
 
+  expect(text).toHaveLength(1);
   expect(badge.hasClass('sg-badge')).toEqual(true);
 });
 
-test('size', () => {
-  const badge = shallow(
-    <Badge size={SIZE.SMALL}>Test</Badge>
-  );
-
-  expect(badge.hasClass('sg-badge--small')).toEqual(true);
-});
-
-test('default font size', () => {
+test('default size', () => {
   const badge = mount(
     <Badge>Test</Badge>
   );
   const text = badge.find(Text);
 
-  expect(text.hasClass('sg-text--xsmall')).toEqual(true);
+  expect(text.props().size).toEqual(TEXT_SIZE.XSMALL);
+  expect(badge.hasClass('sg-badge--small')).toEqual(false);
+  expect(badge.hasClass('sg-badge--large')).toEqual(false);
 });
 
-test('larger font size', () => {
-  const badge = mount(
+test('small size', () => {
+  const badge = shallow(
+    <Badge size={SIZE.SMALL}>Test</Badge>
+  );
+  const text = badge.find(Text);
+
+  expect(text.props().size).toEqual(TEXT_SIZE.XSMALL);
+  expect(badge.hasClass('sg-badge--small')).toEqual(true);
+});
+
+test('larger size', () => {
+  const badge = shallow(
     <Badge size={SIZE.LARGE}>Test</Badge>
   );
   const text = badge.find(Text);
 
-  expect(text.hasClass('sg-text--xsmall')).toEqual(false);
+  expect(text.props().size).toEqual(TEXT_SIZE.NORMAL);
+  expect(badge.hasClass('sg-badge--large')).toEqual(true);
 });
 
 test('color', () => {
@@ -44,7 +51,7 @@ test('color', () => {
   const text = badge.find(Text);
 
   expect(badge.hasClass('sg-badge--mint-secondary')).toEqual(true);
-  expect(text.hasClass('sg-text--light')).toEqual(true);
+  expect(text.props().color).toEqual(TEXT_COLOR.LIGHT);
 });
 
 test('animation', () => {
@@ -68,6 +75,16 @@ test('error when no child', () => {
 
   console.error = jest.fn();
   shallow(<Badge/>);
+  expect(console.error.mock.calls).toHaveLength(1);
+
+  spy.mockRestore();
+});
+
+test('error when passing a react element as a child', () => {
+  const spy = jest.spyOn(console, 'error');
+
+  console.error = jest.fn();
+  shallow(<Badge><div>asd</div></Badge>);
   expect(console.error.mock.calls).toHaveLength(1);
 
   spy.mockRestore();
