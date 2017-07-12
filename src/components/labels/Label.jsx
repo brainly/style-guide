2e3652dfd41a9417f0117047d493c18beded46ce
anna-wro <wro.online@gmail.com>
2017-07-12 12:55:47 +0200

    Labels react

---
+++ src/components/labels/Label.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Icon, {COLOR as ICON_COLOR, TYPE as ICON_TYPE} from '../icons/Icon';
+
+const SIZE = {
+  NORMAL: {CLASS_NAME: 'normal', ICON_SIZE: 16},
+  SMALL: {CLASS_NAME: 'small', ICON_SIZE: 14},
+  LARGE: {CLASS_NAME: 'large', ICON_SIZE: 24}
+};
+
+const Label = ({text, number, iconType, iconColor,
+  size = SIZE.NORMAL, secondary, unstyled, emphasised, elementsToTop
+}) => {
+
+  const labelClass = classNames('sg-label', {
+    [`sg-label--${size.CLASS_NAME}`]: size !== SIZE.NORMAL,
+    'sg-label--secondary': secondary,
+    'sg-label--unstyled': unstyled,
+    'sg-label--emphasised': emphasised,
+    'sg-label--elements-to-the-top': elementsToTop
+  });
+
+  if (number) {
+    return <div className={labelClass}>
+      <div className="sg-label__icon">
+        <Icon type={iconType} color={iconColor} size={size.ICON_SIZE}/>
+      </div>
+      <div className="sg-label__text">{text}</div>
+      <div className="sg-label__number">{number}</div>
+    </div>;
+  }
+
+  return <div className={labelClass}>
+    <div className="sg-label__icon">
+      <Icon type={iconType} color={iconColor} size={size.ICON_SIZE}/>
+    </div>
+    <div className="sg-label__text">{text}</div>
+  </div>;
+};
+
+Label.propTypes = {
+  text: PropTypes.string.isRequired,
+  iconColor: PropTypes.oneOf(Object.values(ICON_COLOR)),
+  iconType: PropTypes.oneOf(Object.values(ICON_TYPE)).isRequired,
+  size: PropTypes.oneOf(Object.values(SIZE)),
+  number: PropTypes.number,
+  secondary: PropTypes.bool,
+  unstyled: PropTypes.bool,
+  emphasised: PropTypes.bool,
+  elementsToTop: PropTypes.bool
+};
+
+export default Label;
+export {SIZE, ICON_TYPE, ICON_COLOR};
---
+++ src/components/labels/pages/labels.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import DocsBlock from '../../../docs/DocsBlock';
+import Label, {ICON_COLOR, ICON_TYPE, SIZE} from '../Label';
+
+const longText = 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.';
+const multilineExample1 = 'This is a multiline example with icon in the middle (default behaviur). ' + longText;
+const multilineExample2 = 'This is a multiline example with icon aligned to top. ' + longText;
+
+const Labels = () =>
+  <div>
+    <DocsBlock info="Default">
+      <Label text="Default label" iconType={ICON_TYPE.SEARCH} iconColor={ICON_COLOR.GRAY}/>
+      <Label text="Unstyled label" iconType={ICON_TYPE.PENCIL} iconColor={ICON_COLOR.LAVENDER} unstyled={true}/>
+      <Label text="Mark as brainliest" iconType={ICON_TYPE.EXCELLENT} iconColor={ICON_COLOR.MUSTARD} emphasised={true}/>
+      <Label text="Comment" number={21} iconType={ICON_TYPE.COMMENT} iconColor={ICON_COLOR.LAVENDER}
+        emphasised={true} secondary={true}/>
+      <Label text="Thank you" number={21} iconType={ICON_TYPE.HEART} iconColor={ICON_COLOR.PEACH} emphasised={true}
+        secondary={true}/>
+      <div style={{maxWidth: 250 + 'px'}}>
+        <Label text={multilineExample1} iconType={ICON_TYPE.SEARCH} iconColor={ICON_COLOR.GRAY}/>
+        <br />
+        <Label text={multilineExample2} iconType={ICON_TYPE.SEARCH} iconColor={ICON_COLOR.GRAY} elementsToTop={true}/>
+      </div>
+    </DocsBlock>
+    <DocsBlock info="Small">
+      <Label text="Mark as brainliest" size={SIZE.SMALL} iconType={ICON_TYPE.EXCELLENT}
+        iconColor={ICON_COLOR.MUSTARD} emphasised={true}/>
+      <Label text="Comment" number={21} size={SIZE.SMALL} iconType={ICON_TYPE.COMMENT}
+        iconColor={ICON_COLOR.LAVENDER} emphasised={true} secondary={true}/>
+      <Label text="Thank you" number={21} size={SIZE.SMALL} iconType={ICON_TYPE.HEART}
+        iconColor={ICON_COLOR.PEACH} emphasised={true} secondary={true}/>
+    </DocsBlock>
+    <DocsBlock info="Large">
+      <Label text="Mark as brainliest" size={SIZE.LARGE} iconType={ICON_TYPE.EXCELLENT}
+        iconColor={ICON_COLOR.MUSTARD}/>
+      <Label text="Comment" number={21} size={SIZE.LARGE} iconType={ICON_TYPE.COMMENT}
+        iconColor={ICON_COLOR.LAVENDER} secondary={true}/>
+      <Label text="Thank you" number={21} size={SIZE.LARGE} iconType={ICON_TYPE.HEART}
+        iconColor={ICON_COLOR.PEACH} secondary={true}/>
+    </DocsBlock>
+  </div>;
+
+export default Labels;
