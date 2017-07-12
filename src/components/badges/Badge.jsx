a1dd7c87f45247ae2d9f933b652bfe727edc6a24
Anna Wróbel <anna-wro@users.noreply.github.com>
2017-07-12 09:03:48 +0200

    Badges react (#1095)
    * Badges react
    * 89.2.0

---
+++ src/components/badges/Badge.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Text, {SIZE as TEXT_SIZE, COLOR as TEXT_COLOR, WEIGHT as TEXT_WEIGHT} from '../text/Text';
+
+const SIZE = {
+  NORMAL: 'normal',
+  SMALL: 'small',
+  LARGE: 'large'
+};
+
+const COLOR = {
+  NORMAL: {BADGE: 'light', TEXT: TEXT_COLOR.DEFAULT},
+  PEACH: {BADGE: 'peach', TEXT: TEXT_COLOR.LIGHT},
+  MUSTARD: {BADGE: 'mustard', TEXT: TEXT_COLOR.LIGHT},
+  MINT_SECONDARY: {BADGE: 'mint-secondary', TEXT: TEXT_COLOR.LIGHT},
+  GRAY_SECONDARY: {BADGE: 'gray-secondary', TEXT: TEXT_COLOR.LIGHT},
+  MINT_SECONDARY_LIGHT: {BADGE: 'mint-secondary-light', TEXT: TEXT_COLOR.MINT},
+  PEACH_SECONDARY_LIGHT: {BADGE: 'peach-secondary-light', TEXT: TEXT_COLOR.PEACH},
+  BLUE_SECONDARY_LIGHT: {BADGE: 'blue-secondary-light', TEXT: TEXT_COLOR.BLUE}
+};
+
+const Badge = ({children, color = COLOR.NORMAL, size = SIZE.NORMAL, rounded, withAnimation}) => {
+
+  const badgeClass = classNames('sg-badge', {
+    [`sg-badge--${color.BADGE}`]: color !== COLOR.NORMAL,
+    [`sg-badge--${size}`]: size !== SIZE.NORMAL,
+    'sg-badge--rounded': rounded,
+    'sg-badge--with-animation': withAnimation
+  });
+  const textSize = size === SIZE.LARGE ? TEXT_SIZE.NORMAL : TEXT_SIZE.XSMALL;
+
+  return <div className={badgeClass}>
+    <Text size={textSize} weight={TEXT_WEIGHT.BOLD} color={color.TEXT}>{children}</Text>
+  </div>;
+};
+
+Badge.propTypes = {
+  color: PropTypes.oneOf(Object.values(COLOR)),
+  size: PropTypes.oneOf(Object.values(SIZE)),
+  children: PropTypes.string.isRequired,
+  rounded: PropTypes.bool,
+  withAnimation: PropTypes.bool
+};
+
+export default Badge;
+export {SIZE, COLOR};
