7d68d04ee221cfc1a515c26360a17951e8c9d8b5
Mateusz Wyka <matikrk@gmail.com>
2017-07-12 08:23:23 +0200

    Cleanup components * Split tests

---
+++ src/components/buttons/ButtonPrimary.spec.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import ButtonPrimary, {TYPE} from './ButtonPrimary';
+import {shallow} from 'enzyme';
+
+test('render', () => {
+  const button = shallow(
+    <ButtonPrimary>Some text</ButtonPrimary>
+  );
+
+  expect(button.hasClass('sg-button-primary')).toEqual(true);
+
+});
+
+test('type', () => {
+  const type = TYPE.ALT;
+  const button = shallow(
+    <ButtonPrimary type={type}>Some text</ButtonPrimary>
+  );
+
+  expect(button.hasClass('sg-button-primary--' + type)).toEqual(true);
+});
+
+test('disabled', () => {
+  const button = shallow(
+    <ButtonPrimary disabled={true}>Some text</ButtonPrimary>
+  );
+
+  expect(button.hasClass('sg-button-primary--disabled')).toEqual(true);
+  expect(button.is('[disabled]')).toEqual(true);
+
+});
+test('not disabled', () => {
+  const button = shallow(
+    <ButtonPrimary>Some text</ButtonPrimary>
+  );
+
+  expect(button.hasClass('sg-button-primary--disabled')).toEqual(false);
+  expect(button.is('[disabled]')).toEqual(false);
+
+});
+
+test('primary don\'t have small', () => {
+  const button = shallow(
+    <ButtonPrimary small={true}>Some text</ButtonPrimary>
+  );
+
+  expect(button.hasClass('sg-button-primary--small')).toEqual(false);
+});
+test('wide', () => {
+  const button = shallow(
+    <ButtonPrimary wide={true}>Some text</ButtonPrimary>
+  );
+
+  expect(button.hasClass('sg-button-primary--full-width')).toEqual(true);
+});
+
+test('icon', () => {
+  const icon = <span>:P</span>;
+  const button = shallow(
+    <ButtonPrimary icon={icon}>Some text</ButtonPrimary>
+  );
+
+  expect(button.contains(icon)).toEqual(true);
+  expect(button.find('.sg-button-primary__icon')).toHaveLength(1);
+});
+
+test('no icon', () => {
+  const button = shallow(
+    <ButtonPrimary>Some text</ButtonPrimary>
+  );
+
+  expect(button.find('.sg-button-primary__icon')).toHaveLength(0);
+});
---
+++ src/components/buttons/ButtonRound.spec.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import ButtonRound from './ButtonRound';
+import {shallow} from 'enzyme';
+
+
+test('render', () => {
+  const button = shallow(
+    <ButtonRound>Some text</ButtonRound>
+  );
+
+  expect(button.hasClass('sg-button-primary-round')).toEqual(true);
+});
+
+
+test('href', () => {
+  const href = '#test';
+  const button = shallow(
+    <ButtonRound href={href}>Some text</ButtonRound>
+  );
+
+  expect(button.is(`[href="${href}"]`)).toEqual(true);
+});
+
+test('label', () => {
+  const label = 'example label';
+  const button = shallow(
+    <ButtonRound label={label}>Some text</ButtonRound>
+  );
+
+  expect(button.contains(label)).toEqual(true);
+  expect(button.find('.sg-button-primary-round__label')).toHaveLength(1);
+});
+
+test('no label', () => {
+  const button = shallow(
+    <ButtonRound >Some text</ButtonRound>
+  );
+
+  expect(button.find('sg-button-primary-round__label')).toHaveLength(0);
+});
+
+
+test('func', () => {
+  let counter = 0;
+  const onClick = () => counter++;
+  const button = shallow(
+    <ButtonRound onClick={onClick}>Some text</ButtonRound>
+  );
+
+  expect(counter).toEqual(0);
+  button.simulate('click');
+  expect(counter).toEqual(1);
+});
+
+test('func throw testing 1part - undefined function', () => {
+  const button = shallow(
+    <ButtonRound >Some text</ButtonRound>
+  );
+
+  expect(() => button.simulate('click')).not.toThrow();
+
+  const button2 = shallow(
+    <ButtonRound onClick={undefined}>Some text</ButtonRound>
+  );
+
+  expect(() => button2.simulate('click')).not.toThrow();
+});
+
+test('func throw testing 2part - defined bad type', () => {
+  const spy = jest.spyOn(console, 'error');
+
+  console['error'] = jest.fn();
+
+  const notFunctionObject = 'there should be func not string';
+  const button = shallow(
+    <ButtonRound onClick={notFunctionObject}>Some text</ButtonRound>
+  );
+
+  expect(() => button.simulate('click')).toThrow();
+
+  spy.mockRestore();
+});
