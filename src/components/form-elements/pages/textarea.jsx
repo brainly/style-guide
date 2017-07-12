f379d9a8859f1f407855bda883dd817b8b79b4ed
Mateusz Wyka <matikrk@gmail.com>
2017-07-12 11:04:17 +0200

    Form elements react components - Textarea * examples

---
+++ src/components/form-elements/pages/textarea.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Textarea, {SIZE, VALIDATION} from '../Textarea';
+import DocsBlock from '../../../docs/DocsBlock';
+
+const textareas = () => <div>
+  <DocsBlock info="Default">
+    <Textarea placeholder="placeholder"/>
+  </DocsBlock>
+  <DocsBlock info="Short">
+    <Textarea placeholder="placeholder" size={SIZE.SHORT}/>
+  </DocsBlock>
+  <DocsBlock info="Full width">
+    <Textarea placeholder="placeholder" fullWidth={true}/>
+  </DocsBlock>
+  <DocsBlock info="Valid">
+    <Textarea placeholder="placeholder" valid={VALIDATION.VALID} value="This is valid example"/>
+  </DocsBlock>
+  <DocsBlock info="Invalid">
+    <Textarea placeholder="placeholder" valid={VALIDATION.INVALID} value="This is invalid example"/>
+  </DocsBlock>
+  <DocsBlock info="Tall, XTall" multiContent={[
+    <Textarea placeholder="placeholder" size={SIZE.TALL}/>,
+    <Textarea placeholder="placeholder" size={SIZE.XTALL}/>
+  ]}/>
+
+  <DocsBlock info="Simple">
+    <Textarea placeholder="placeholder" simple={true}/>
+  </DocsBlock>
+  <DocsBlock info="Auto height">
+    <Textarea placeholder="placeholder" autoHeight={true}/>
+  </DocsBlock>
+</div>;
+
+export default textareas;
