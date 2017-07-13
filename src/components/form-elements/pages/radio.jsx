aa47c59cbf3a3a24955b9d7906e64936973d5399
Mateusz Wyka <matikrk@gmail.com>
2017-07-13 14:11:53 +0200

    Form elements react components - Checkbox & Fixes * split to 2 pages

--- src/components/form-elements/pages/radio.jsx
+++ src/components/form-elements/pages/radio.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import Checkbox from '../Checkbox';
 import DocsBlock from '../../../docs/DocsBlock';
 import Label from '../../labels/Label';
 
 const dumpProps = {onChange: () => undefined};
 
 const checkboxesAndRadios = () => <div>
-  <DocsBlock info="Checkboxes">
-    <Checkbox />
-    <Checkbox checked={true} {...dumpProps}/>
-    <br />
-    <Label secondary={true} text="Check me!" htmlFor="checkbox-1" iconContent={
-      <Checkbox id="checkbox-1"/>
-    }/>
-    <Label secondary={true} text="Check me!" htmlFor="checkbox-2" emphasised={true} iconContent={
-      <Checkbox id="checkbox-2"/>
-    }/>
-  </DocsBlock>
   <DocsBlock info="Radio Buttons">
     <div className="sg-radio">
       <input className="sg-radio__element" type="radio" name="group1" id="radio-1"/>
