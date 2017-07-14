763494cc4aa2c1c57c9eb520c52aeda9f222ab54
matikrk <matikrk@gmail.com>
2017-07-14 15:09:27 +0200

    Search component (#1113)
    * Search component
    * add props to TextInput - noBorder && withIcon
    * create component, tests
    
    * Search component
    * examples
    
    * 94.3.0

---
+++ src/components/search/Search.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TextInput, {VALIDATION, COLOR, SIZE} from '../form-elements/TextInput';
+import Icon, {TYPE, COLOR as ICON_COLOR} from '../icons/Icon';
+
+const Search = props => <div className="sg-search">
+  <TextInput {...props} type="search" withIcon={true} />
+  <div className="sg-search__icon">
+    <Icon type={TYPE.SEARCH} color={ICON_COLOR.GRAY_SECONDARY} size={18}/>
+  </div>
+</div>;
+
+Search.propTypes = TextInput.propTypes;
+
+export default Search;
+export {SIZE, COLOR, VALIDATION};
---
+++ src/components/search/pages/search.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Search, {SIZE, COLOR} from '../Search';
+import DocsBlock from '../../../docs/DocsBlock';
+import ContrastBox from '../../../docs/ContrastBox';
+
+const searches = () => <div>
+  <DocsBlock info="Standard">
+    <Search placeholder="Find all the answers..."/>
+  </DocsBlock>
+  <DocsBlock info="Large size">
+    <Search placeholder="Find all the answers..." size={SIZE.LARGE}/>
+  </DocsBlock>
+  <DocsBlock info="Full width">
+    <Search placeholder="Find all the answers..." fullWidth={true}/>
+  </DocsBlock>
+  <DocsBlock info="Light">
+    <ContrastBox fullWidth={true}>
+      <Search placeholder="Find all the answers..." fullWidth={true} color={COLOR.LIGHT}/>
+    </ContrastBox>
+  </DocsBlock>
+  <DocsBlock info="No border">
+    <Search placeholder="Find all the answers..." fullWidth={true} noBorder={true}/>
+  </DocsBlock>
+</div>;
+
+export default searches;
