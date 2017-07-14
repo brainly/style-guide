04c2767d597b0a2ac7d95c0d850288413cd2886e
matikrk <matikrk@gmail.com>
2017-07-14 15:54:54 +0200

    Colors (#1114)

---
+++ src/components/colors/colors.js
@@ -0,0 +1,177 @@
+const color = {
+  'primary': [
+    {
+      'name': 'Black',
+      'variable': '$black',
+      'hex': '020a1b'
+    },
+    {
+      'name': 'Mint',
+      'variable': '$mintPrimary',
+      'hex': '53cf92'
+    },
+    {
+      'name': 'Mint mid-tone',
+      'variable': '$mintSecondary',
+      'hex': 'b4efc2'
+    },
+    {
+      'name': 'Mint light',
+      'variable': '$mintSecondaryLight',
+      'hex': 'ddf7e5'
+    },
+    {
+      'name': 'Blue',
+      'variable': '$bluePrimary',
+      'hex': '57b2f8'
+    },
+    {
+      'name': 'Blue mid-tone',
+      'variable': '$blueSecondary',
+      'hex': 'abe1ff'
+    },
+    {
+      'name': 'Blue light',
+      'variable': '$blueSecondaryLight',
+      'hex': 'daf3ff'
+    }
+  ],
+  'secondary': [
+    {
+      'name': 'Lavender',
+      'variable': '$lavenderPrimary',
+      'hex': '7a8adb'
+    },
+    {
+      'name': 'Lavender mid-tone',
+      'variable': '$lavenderSecondary',
+      'hex': 'b3c4ff'
+    },
+    {
+      'name': 'Lavender light',
+      'variable': '$lavenderSecondaryLight',
+      'hex': 'e2e8ff'
+    },
+    {
+      'name': 'Peach',
+      'variable': '$peachPrimary',
+      'hex': 'ff796b'
+    },
+    {
+      'name': 'Peach mid-tone',
+      'variable': '$peachSecondary',
+      'hex': 'ffb3ae'
+    },
+    {
+      'name': 'Peach light',
+      'variable': '$peachSecondaryLight',
+      'hex': 'ffe2e0'
+    },
+    {
+      'name': 'Mustard',
+      'variable': '$mustardPrimary',
+      'hex': 'fec83c'
+    },
+    {
+      'name': 'Mustard mid-tone',
+      'variable': '$mustardSecondary',
+      'hex': 'ffde8c'
+    },
+    {
+      'name': 'Mustard light',
+      'variable': '$mustardSecondaryLight',
+      'hex': 'fff5d5'
+    }
+  ],
+  'gray': [
+    {
+      'name': 'White',
+      'variable': '$white',
+      'hex': 'ffffff'
+    },
+    {
+      'name': 'Gray',
+      'variable': '$grayPrimary',
+      'hex': '434e66'
+    },
+    {
+      'name': 'Gray mid-tone',
+      'variable': '$graySecondary',
+      'hex': '9fa6b5'
+    },
+    {
+      'name': 'Gray secondary light',
+      'variable': '$graySecondaryLight',
+      'hex': 'e1e8ed'
+    },
+    {
+      'name': 'Gray secondary lightest',
+      'variable': '$graySecondaryLightest',
+      'hex': 'f0f3f5'
+    }
+  ],
+  'additional': [
+    {
+      'name': 'Bronze',
+      'variable': '$bronzePrimary',
+      'hex': 'a76a58'
+    },
+    {
+      'name': 'Bronze mid-tone',
+      'variable': '$bronzeSecondary',
+      'hex': 'db8d6b'
+    },
+    {
+      'name': 'Bronze light',
+      'variable': '$bronzeSecondaryLight',
+      'hex': 'f4d7ca'
+    },
+    {
+      'name': 'Silver',
+      'variable': '$silverPrimary',
+      'hex': '8e969a'
+    },
+    {
+      'name': 'Silver mid-tone',
+      'variable': '$silverSecondary',
+      'hex': 'b6bec2'
+    },
+    {
+      'name': 'Silver light',
+      'variable': '$silverSecondaryLight',
+      'hex': 'e8eef1'
+    },
+    {
+      'name': 'Gold',
+      'variable': '$goldPrimary',
+      'hex': 'c08228'
+    },
+    {
+      'name': 'Gold mid-tone',
+      'variable': '$goldSecondary',
+      'hex': 'ecb444'
+    },
+    {
+      'name': 'Gold light',
+      'variable': '$goldSecondaryLight',
+      'hex': 'fbe4b5'
+    },
+    {
+      'name': 'Navy blue',
+      'variable': '$navybluePrimary',
+      'hex': '1b2e57'
+    },
+    {
+      'name': 'Navy blue mid tone',
+      'variable': '$navyblueSecondary',
+      'hex': '3a548d'
+    },
+    {
+      'name': 'Navy blue light',
+      'variable': '$navyblueSecondaryLight',
+      'hex': 'b3c4e7'
+    }
+  ]
+};
+
+export default color;
