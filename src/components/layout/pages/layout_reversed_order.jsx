d120c0e24b9f0098758576ce51b75538162e79c5
matzimowski <mat.zimowski@gmail.com>
2017-07-17 15:00:38 +0200

    Layout component in React

--- src/components/layout/pages/layout_reversed_order.jsx
+++ src/components/layout/pages/layout_reversed_order.jsx
@@ -9,46 +9,46 @@ import Text from '../../text/Text';
 
 const LayoutReversedOrder = () =>
   <html>
-  <head>
-    <meta charSet="utf-8"/>
-    <link rel="stylesheet" href="../../../style-guide.css"/>
-    <style>{"\
-    body {\
-      background-color: #f0f3f5;\
-    }\
-  "}</style>
-  </head>
-  <body>
-  <Layout>
-    <div className="sg-header sg-header--fixed">
-      <div className="sg-header__container">
-        Header
-      </div>
-    </div>
-    <LayoutContainer reversedOrder={true}>
-      <LayoutContent>
-        <Text>
-          Main content has padding and white background
-        </Text>
-      </LayoutContent>
-      <LayoutAsideContent>
-        <LayoutBox>
-          <Text>
-            Aside content has no padding and no background
-          </Text>
-        </LayoutBox>
-      </LayoutAsideContent>
-    </LayoutContainer>
-    <LayoutFooter>
-      <div className="sg-footer">
-        <div className="sg-footer__container">
-          Footer
+    <head>
+      <meta charSet="utf-8"/>
+      <link rel="stylesheet" href="../../../style-guide.css"/>
+      <style>{'\
+      body {\
+        background-color: #f0f3f5;\
+      }\
+    '}</style>
+    </head>
+    <body>
+      <Layout>
+        <div className="sg-header sg-header--fixed">
+          <div className="sg-header__container">
+            Header
+          </div>
         </div>
-      </div>
-    </LayoutFooter>
-  </Layout>
-  <script src="images/icons.js"></script>
-  </body>
+        <LayoutContainer reversedOrder={true}>
+          <LayoutContent>
+            <Text>
+              Main content has padding and white background
+            </Text>
+          </LayoutContent>
+          <LayoutAsideContent>
+            <LayoutBox>
+              <Text>
+                Aside content has no padding and no background
+              </Text>
+            </LayoutBox>
+          </LayoutAsideContent>
+        </LayoutContainer>
+        <LayoutFooter>
+          <div className="sg-footer">
+            <div className="sg-footer__container">
+              Footer
+            </div>
+          </div>
+        </LayoutFooter>
+      </Layout>
+      <script src="images/icons.js"></script>
+    </body>
   </html>;
 
 export default LayoutReversedOrder;
