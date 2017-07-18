d74e141e73fda4c48b7854b2eb3563c1c5aced45
anna-wro <wro.online@gmail.com>
2017-07-18 12:11:42 +0200

    small fixes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Footer = ({children}) => <footer className="sg-layout__footer">
-  <div className="sg-footer">
-    <div className="sg-footer__container">
-      {children}
-    </div>
+const Footer = ({children}) => <footer className="sg-footer">
+  <div className="sg-footer__container">
+    {children}
   </div>
 </footer>;
 
