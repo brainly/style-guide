e138d77cc62b930db7ef1615ba8d96aa1898bee7
Konrad Dzwinel <kdzwinel@gmail.com>
2017-07-21 18:53:31 +0200

    Refactor component settings

--- src/docs/components/ComponentSettings.jsx
+++ src/docs/components/ComponentSettings.jsx
@@ -1,75 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ComponentSettingsInput from './ComponentSettingsInput';
+import ComponentSettingsSelect from './ComponentSettingsSelect';
 
 function isPlainObject(o) {
   return Object.getPrototypeOf(o) === Object.prototype;
 }
 
 const ComponentSettings = ({settings, values, onChange}) => {
-  function inputChanged(values, e) {
-    const target = e.target;
-    const key = e.target.dataset.key;
-    const type = e.target.dataset.valueType;
-    let value = null;
-
-    if (type === 'boolean') {
-      value = Boolean(target.checked);
-    } else if (type === 'number') {
-      value = Number(target.value);
-    } else if (type === 'object') {
-      value = values[target.value];
-    } else if (type === 'string') {
-      value = target.value;
-    }
-
-    onChange(key, value);
-  }
-
   const content = Object.keys(settings).map(key => {
     const allowedValues = settings[key];
+    const currentValue = values[key];
+    let input = null;
 
     if (isPlainObject(allowedValues)) {
-      const allowedKeys = Object.keys(allowedValues);
-      const selectedKey = allowedKeys.find(optionKey => allowedValues[optionKey] === values[key]);
-
-      return <label key={key}>{key}:
-        <select
-          data-key={key}
-          data-value-type="object"
-          onChange={inputChanged.bind(null, allowedValues)}
-          value={selectedKey}
-        >
-          {allowedKeys.map(optionKey => <option key={optionKey} value={optionKey}>{optionKey}</option>)}
-        </select>
-      </label>;
-    }
-
-    let type;
-    let inputType;
-    const inputValue = values[key];
-    const checked = Boolean(values[key]);
-
-    if (allowedValues === Boolean) {
-      type = 'boolean';
-      inputType = 'checkbox';
-    } else if (allowedValues === Number) {
-      type = 'number';
-      inputType = 'number';
-    } else if (allowedValues === String) {
-      type = 'string';
-      inputType = 'text';
+      input = <ComponentSettingsSelect key={key} values={allowedValues} currentValue={currentValue}
+                                       onChange={value => onChange(key, value)}/>;
     } else {
-      throw new Error('Unsupported value type.');
+      input = <ComponentSettingsInput key={key} values={allowedValues} currentValue={currentValue}
+                                      onChange={value => onChange(key, value)}/>;
     }
 
-    return <label key={key}>{key}:
-      <input type={inputType}
-        data-key={key}
-        data-value-type={type}
-        checked={checked}
-        value={inputValue}
-        onChange={inputChanged.bind(null, allowedValues)}/>
-    </label>;
+    return <label key={key}>{key}: {input} </label>;
   });
 
   return <fieldset className="docs-active-block__settings">{content}</fieldset>;
---
+++ src/docs/components/ComponentSettingsSelect.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ComponentSettingsSelect = ({values, currentValue, onChange}) => {
+  const allowedKeys = Object.keys(values);
+  const selectedKey = allowedKeys.find(optionKey => values[optionKey] === currentValue);
+
+  function inputChanged(e) {
+    const target = e.target;
+    const value = values[target.value];
+
+    onChange(value);
+  }
+
+  return <select
+    data-value-type="object"
+    onChange={inputChanged}
+    value={selectedKey}
+  >
+    {allowedKeys.map(optionKey => <option key={optionKey} value={optionKey}>{optionKey}</option>)}
+  </select>;
+};
+
+ComponentSettingsSelect.propTypes = {
+  values: PropTypes.object.isRequired,
+  currentValue: PropTypes.any,
+  onChange: PropTypes.func.isRequired
+};
+
+export default ComponentSettingsSelect;
