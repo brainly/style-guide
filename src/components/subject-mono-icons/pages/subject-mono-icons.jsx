07dbc1d111619f49446dac5c6252d33a87a26856
patrycjaradaczynska <patrycja.radaczynska@brainly.com>
2017-07-21 16:11:24 +0200

    Adjustments for svg

--- src/components/subject-mono-icons/pages/subject-mono-icons.jsx
+++ src/components/subject-mono-icons/pages/subject-mono-icons.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import DocsBlock from 'DocsBlock';
+import ContrastBox from 'ContrastBox';
 import SubjectIcon, {TYPE} from '../SubjectMonoIcon';
 
 const subjectMonoIcons = () => <div>
   <DocsBlock >
-    <ul className="icons-list">
-      {Object.values(TYPE).map(type => <li className="icons-list__element icons-list__element--wide" key={type}>
-        <SubjectIcon type={type}/>
-        <span className="icons-list__element-info">&nbsp; - {type}</span>
-      </li>)}
-    </ul>
+    <ContrastBox>
+      <ul className="icons-list">
+        {Object.values(TYPE).map(type => <li className="icons-list__element icons-list__element--wide" key={type}>
+          <SubjectIcon type={type}/>
+          <span className="icons-list__element-info">&nbsp; - {type}</span>
+        </li>)}
+      </ul>
+    </ContrastBox>
   </DocsBlock>
 </div>;
 
