a8989312be88c1790e5f4e86da7978834dff249f
Konrad Dzwinel <kdzwinel@gmail.com>
2017-07-24 02:39:20 +0200

    Fixing linting issues

--- src/docs/components/DocsActiveBlockSettings.jsx
+++ src/docs/components/DocsActiveBlockSettings.jsx
@@ -7,25 +7,22 @@ import ActionList from 'action-list/ActionList';
 import SeparatorVertical from 'separators/SeparatorVertical';
 import UUID from 'node-uuid';
 
-const DocsActiveBlockSettings = ({values, onChange}) => {
-
-  return <div className="docs-active-block__settings"><ActionList>
-    <Text size={TEXT_SIZE.SMALL} color={TEXT_COLOR.GRAY}>Code:&nbsp;
-      <label>none <Radio name={UUID.v1()} checked={!values.showCode}
-        onChange={() => onChange('showCode', null)}/></label>&nbsp;
-      <label>JSX <Radio name={UUID.v1()} checked={values.showCode === 'jsx'}
-        onChange={() => onChange('showCode', 'jsx')}/></label>&nbsp;
-      <label>HTML <Radio name={UUID.v1()} checked={values.showCode === 'html'}
-        onChange={() => onChange('showCode', 'html')}/></label>
+const DocsActiveBlockSettings = ({values, onChange}) => <div className="docs-active-block__settings"><ActionList>
+  <Text size={TEXT_SIZE.SMALL} color={TEXT_COLOR.GRAY}>Code:&nbsp;
+    <label>none <Radio name={UUID.v1()} checked={!values.showCode}
+      onChange={() => onChange('showCode', null)}/></label>&nbsp;
+    <label>JSX <Radio name={UUID.v1()} checked={values.showCode === 'jsx'}
+      onChange={() => onChange('showCode', 'jsx')}/></label>&nbsp;
+    <label>HTML <Radio name={UUID.v1()} checked={values.showCode === 'html'}
+      onChange={() => onChange('showCode', 'html')}/></label>
+  </Text>
+  <SeparatorVertical/>
+  <label>
+    <Text size={TEXT_SIZE.SMALL} color={TEXT_COLOR.GRAY}>Background:&nbsp;
+      <Checkbox checked={values.grayBackground} onChange={e => onChange('grayBackground', e.target.checked)}/>
     </Text>
-    <SeparatorVertical/>
-    <label>
-      <Text size={TEXT_SIZE.SMALL} color={TEXT_COLOR.GRAY}>Background:&nbsp;
-        <Checkbox checked={values.grayBackground} onChange={e => onChange('grayBackground', e.target.checked)}/>
-      </Text>
-    </label>
-  </ActionList></div>;
-};
+  </label>
+</ActionList></div>;
 
 DocsActiveBlockSettings.propTypes = {
   values: PropTypes.object.isRequired,
