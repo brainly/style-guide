49dec5b31b0ef3c0ff7fc3494ca4efb23ba215c2
Konrad Dzwinel <kdzwinel@gmail.com>
2017-07-24 09:16:02 +0200

    Fix paths to DocsBlock & ContrastBox component. (#1146)
    * Fix paths to DocsBlock & ContrastBox component.
    Small improvements to the index page.
    
    * Fix copy helper

--- src/components/subject-mono-icons/pages/subject-mono-icons.jsx
+++ src/components/subject-mono-icons/pages/subject-mono-icons.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import DocsBlock from 'DocsBlock';
-import ContrastBox from 'ContrastBox';
+import DocsBlock from 'components/DocsBlock';
+import ContrastBox from 'components/ContrastBox';
 import SubjectIcon, {TYPE} from '../SubjectMonoIcon';
 
 const subjectMonoIcons = () => <div>
