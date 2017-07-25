21dccc7a103be50555dc7367d5b88541f6c0c9cd
anna-wro <wro.online@gmail.com>
2017-07-25 12:18:32 +0200

    DocsActiveBlock style * add dark background

--- src/docs/components/DocsActiveBlockSettings.jsx
+++ src/docs/components/DocsActiveBlockSettings.jsx
@@ -1,28 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Text, {SIZE as TEXT_SIZE, COLOR as TEXT_COLOR} from 'text/Text';
-import Checkbox from 'form-elements/Checkbox';
 import Radio from 'form-elements/Radio';
 import ActionList from 'action-list/ActionList';
 import SeparatorVertical from 'separators/SeparatorVertical';
 import UUID from 'node-uuid';
 
-const DocsActiveBlockSettings = ({values, onChange}) => <div className="docs-active-block__settings"><ActionList>
-  <Text size={TEXT_SIZE.SMALL} color={TEXT_COLOR.GRAY}>Code:&nbsp;
-    <label>none <Radio name={UUID.v1()} checked={!values.showCode}
-      onChange={() => onChange('showCode', null)}/></label>&nbsp;
-    <label>JSX <Radio name={UUID.v1()} checked={values.showCode === 'jsx'}
-      onChange={() => onChange('showCode', 'jsx')}/></label>&nbsp;
-    <label>HTML <Radio name={UUID.v1()} checked={values.showCode === 'html'}
-      onChange={() => onChange('showCode', 'html')}/></label>
-  </Text>
-  <SeparatorVertical/>
-  <label>
-    <Text size={TEXT_SIZE.SMALL} color={TEXT_COLOR.GRAY}>Background:&nbsp;
-      <Checkbox checked={values.grayBackground} onChange={e => onChange('grayBackground', e.target.checked)}/>
+const DocsActiveBlockSettings = ({values, onChange}) => <div className="docs-active-block__settings">
+  <ActionList>
+    <Text size={TEXT_SIZE.SMALL} color={TEXT_COLOR.GRAY}>Code:
+      <label className="docs-active-block__label">
+        <Radio name={UUID.v1()} checked={!values.showCode}
+          onChange={() => onChange('showCode', null)}/> none
+      </label>
+      <label className="docs-active-block__label">
+        <Radio name={UUID.v1()} checked={values.showCode === 'jsx'}
+          onChange={() => onChange('showCode', 'jsx')}/> JSX
+      </label>
+      <label className="docs-active-block__label">
+        <Radio name={UUID.v1()} checked={values.showCode === 'html'}
+          onChange={() => onChange('showCode', 'html')}/> HTML
+      </label>
     </Text>
-  </label>
-</ActionList></div>;
+    <SeparatorVertical/>
+    <Text size={TEXT_SIZE.SMALL} color={TEXT_COLOR.GRAY}>Background:
+      <label className="docs-active-block__label">
+        <Radio name={UUID.v1()} checked={!values.changeBackground}
+          onChange={() => onChange('changeBackground', null)}/> none</label>
+      <label className="docs-active-block__label">
+        <Radio name={UUID.v1()} checked={values.changeBackground === 'light'}
+          onChange={() => onChange('changeBackground', 'light')}/> light
+      </label>
+      <label className="docs-active-block__label">
+        <Radio name={UUID.v1()} checked={values.changeBackground === 'dark'}
+          onChange={() => onChange('changeBackground', 'dark')}/> dark
+      </label>
+    </Text>
+  </ActionList>
+</div>;
 
 DocsActiveBlockSettings.propTypes = {
   values: PropTypes.object.isRequired,
