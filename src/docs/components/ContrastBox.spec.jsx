0097802141479403d7e2cfdceed48a0b77efe3e8
Anna Wróbel <anna-wro@users.noreply.github.com>
2017-08-07 12:05:49 +0200

    Docs tests (#1242)
    * JSX generator - 100% converage
    
    * docs block tests
    
    * DocsBlock 100% coverage
    
    * ContrastBox 100% coverage
    
    * 107.8.0

---
+++ src/docs/components/ContrastBox.spec.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import ContrastBox from './ContrastBox';
+
+test('render', () => {
+  const contrastBox = shallow(
+    <ContrastBox>some text</ContrastBox>
+  );
+
+  expect(contrastBox.hasClass('docs-block__contrast-box')).toEqual(true);
+});
+
+test('error when no children', () => {
+  const spy = jest.spyOn(console, 'error');
+
+  console.error = jest.fn();
+  shallow(
+    <ContrastBox/>
+  );
+  expect(console.error.mock.calls).toHaveLength(1);
+
+  spy.mockRestore();
+});
+
+test('to bottom', () => {
+  const contrastBox = shallow(
+    <ContrastBox toBottom={true}>some text</ContrastBox>
+  );
+
+  expect(contrastBox.hasClass('docs-block__contrast-box--to-bottom')).toEqual(true);
+});
+
+test('small padding', () => {
+  const contrastBox = shallow(
+    <ContrastBox smallPadding={true}>some text</ContrastBox>
+  );
+
+  expect(contrastBox.hasClass('docs-block__contrast-box--small-padding')).toEqual(true);
+});
+
+test('light', () => {
+  const contrastBox = shallow(
+    <ContrastBox light={true}>some text</ContrastBox>
+  );
+
+  expect(contrastBox.hasClass('docs-block__contrast-box--light')).toEqual(true);
+});
+
+test('full width', () => {
+  const contrastBox = shallow(
+    <ContrastBox fullWidth={true}>some text</ContrastBox>
+  );
+
+  expect(contrastBox.hasClass('docs-block__contrast-box--full-width')).toEqual(true);
+});
+
+test('narrow', () => {
+  const contrastBox = shallow(
+    <ContrastBox narrow={true}>some text</ContrastBox>
+  );
+
+  expect(contrastBox.hasClass('docs-block__contrast-box--narrow')).toEqual(true);
+});
