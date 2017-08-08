94051ec5abb985c1d0dc61d6c2c6c2c08abfb107
Anna Wróbel <anna-wro@users.noreply.github.com>
2017-08-08 09:27:06 +0200

    ActionList directions (#1245)
    * actionList directions
    
    * 107.9.1

--- src/components/action-list/ActionList.jsx
+++ src/components/action-list/ActionList.jsx
@@ -4,14 +4,14 @@ import classNames from 'classnames';
 
 const DIRECTION = {
   TO_RIGHT: 'to-right',
-  TO_TOP: 'to-top',
   CENTERED: 'centered',
   SPACE_BETWEEN: 'space-between'
 };
 
-const ActionList = ({children, direction, noWrap, className}) => {
+const ActionList = ({children, toTop, direction, noWrap, className}) => {
   const actionListClass = classNames('sg-actions-list', {
     [`sg-actions-list--${direction}`]: direction,
+    'sg-actions-list--to-top': toTop,
     'sg-actions-list--no-wrap': noWrap
   }, className);
 
@@ -26,7 +26,8 @@ ActionList.propTypes = {
   direction: PropTypes.oneOf(Object.values(DIRECTION)),
   noWrap: PropTypes.bool,
   children: PropTypes.node,
-  className: PropTypes.string
+  className: PropTypes.string,
+  toTop: PropTypes.bool
 };
 
 export default ActionList;
