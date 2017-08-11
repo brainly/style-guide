62fd6947efc7f2ddb2a7cd682782df54c4ba137b
anna-wro <wro.online@gmail.com>
2017-08-11 11:38:11 +0200

    generating pretty JSX 🌹

--- src/docs/components/DocsActiveBlock.jsx
+++ src/docs/components/DocsActiveBlock.jsx
@@ -71,7 +71,7 @@ class DocsActiveBlock extends Component {
       component = React.cloneElement(this.props.children, this.state.props);
 
       if (this.state.showCode === 'jsx') {
-        const jsx = generateJSX(component);
+        const jsx = generateJSX(component, this.props.settings);
 
         code = <DocsBlock><CodeBlock type="jsx">{jsx}</CodeBlock></DocsBlock>;
       } else if (this.state.showCode === 'html') {
--- src/docs/components/JSXGenerator.js
+++ src/docs/components/JSXGenerator.js
@@ -21,23 +21,44 @@ function propToString(prop, inJS = false) {
   return wrapInBraces ? '{' + result + '}' : result;
 }
 
-function generateJSX(component) {
+function generateJSX(component, settings = null) {
   if (!React.isValidElement(component)) {
     return component;
   }
 
   const type = component.type.name || component.type;
 
+  let unchangedProps, propsWithDefaults = [];
+
+  if (Array.isArray(settings)) {
+
+    unchangedProps = Object.keys(component.props)
+      .filter(key => {
+        const propSettings = settings.find(setting => setting.name === key);
+
+        if (!propSettings || propSettings.required) {
+          return true;
+        }
+
+        return false;
+      });
+
+    propsWithDefaults = Object.keys(component.props)
+      .filter(key => unchangedProps.indexOf(key) === -1);
+  }
+
   let jsxProps = Object.keys(component.props)
     .filter(key => key !== 'children')
     .filter(key => component.props[key] !== undefined)
     .filter(key => {
-      const componentWithoutProp = React.cloneElement(component, {[`${key}`]: undefined});
+      if (settings && propsWithDefaults.indexOf(key) > -1) {
+        const componentWithoutProp = React.cloneElement(component, {[`${key}`]: undefined});
+        const codeWithProp = ReactDOMServer.renderToStaticMarkup(component);
+        const codeWithoutProp = ReactDOMServer.renderToStaticMarkup(componentWithoutProp);
 
-      const codeWithProp = ReactDOMServer.renderToStaticMarkup(component);
-      const codeWithoutProp = ReactDOMServer.renderToStaticMarkup(componentWithoutProp);
-      console.log(codeWithoutProp);
-      return codeWithProp !== codeWithoutProp;
+        return codeWithProp !== codeWithoutProp;
+      }
+      return true;
     })
     .map(key => `${key}=${propToString(component.props[key])}`)
     .join(' ');
