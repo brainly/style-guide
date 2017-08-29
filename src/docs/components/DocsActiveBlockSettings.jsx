e99928c4236bf862fb3f63a9938e135c534faaf4
anna-wro <wro.online@gmail.com>
2017-08-29 11:13:11 +0200

    fixed bind issue in DocsActiveBlockSettings

--- src/docs/components/DocsActiveBlockSettings.jsx
+++ src/docs/components/DocsActiveBlockSettings.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable react/jsx-no-bind */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import Text, {SIZE as TEXT_SIZE, COLOR as TEXT_COLOR} from 'text/Text';
@@ -8,40 +6,53 @@ import ActionList from 'action-list/ActionList';
 import SeparatorVertical from 'separators/SeparatorVertical';
 import UUID from 'node-uuid';
 
-const DocsActiveBlockSettings = ({values, onChange}) =>
-  <div className="docs-active-block__settings">
-    <ActionList>
-      <Text size={TEXT_SIZE.SMALL} color={TEXT_COLOR.GRAY}>Code:
-        <label className="docs-active-block__label">
-          <Radio name={UUID.v1()} checked={!values.showCode}
-            onChange={() => onChange('showCode', null)} /> none
-        </label>
-        <label className="docs-active-block__label">
-          <Radio name={UUID.v1()} checked={values.showCode === 'jsx'}
-            onChange={() => onChange('showCode', 'jsx')} /> JSX
-        </label>
-        <label className="docs-active-block__label">
-          <Radio name={UUID.v1()} checked={values.showCode === 'html'}
-            onChange={() => onChange('showCode', 'html')} /> HTML
-        </label>
-      </Text>
-      <SeparatorVertical />
-      <Text size={TEXT_SIZE.SMALL} color={TEXT_COLOR.GRAY}>Background:
-        <label className="docs-active-block__label">
-          <Radio name={UUID.v1()} checked={!values.changeBackground}
-            onChange={() => onChange('changeBackground', null)} /> none
-        </label>
-        <label className="docs-active-block__label">
-          <Radio name={UUID.v1()} checked={values.changeBackground === 'light'}
-            onChange={() => onChange('changeBackground', 'light')} /> light
-        </label>
-        <label className="docs-active-block__label">
-          <Radio name={UUID.v1()} checked={values.changeBackground === 'dark'}
-            onChange={() => onChange('changeBackground', 'dark')} /> dark
-        </label>
-      </Text>
-    </ActionList>
-  </div>;
+class DocsActiveBlockSettings extends React.Component {
+
+  onChangeFunc = (type, value) => this.props.onChange(type, value);
+  onChangeShowNothing = () => this.onChangeFunc('showCode', null);
+  onChangeShowJSX = () => this.onChangeFunc('showCode', 'jsx');
+  onChangeShowHTML = () => this.onChangeFunc('showCode', 'html');
+  onChangeBackgroundDefault = () => this.onChangeFunc('changeBackground', null);
+  onChangeBackgroundLight = () => this.onChangeFunc('changeBackground', 'light');
+  onChangeBackgroundDark = () => this.onChangeFunc('changeBackground', 'dark');
+
+  render() {
+    return (
+      <div className="docs-active-block__settings">
+        <ActionList>
+          <Text size={TEXT_SIZE.SMALL} color={TEXT_COLOR.GRAY}>Code:
+            <label className="docs-active-block__label">
+              <Radio name={UUID.v1()} checked={!this.props.values.showCode}
+                onChange={this.onChangeShowNothing} /> none
+            </label>
+            <label className="docs-active-block__label">
+              <Radio name={UUID.v1()} checked={this.props.values.showCode === 'jsx'}
+                onChange={this.onChangeShowJSX} /> JSX
+            </label>
+            <label className="docs-active-block__label">
+              <Radio name={UUID.v1()} checked={this.props.values.showCode === 'html'}
+                onChange={this.onChangeShowHTML} /> HTML
+            </label>
+          </Text>
+          <SeparatorVertical />
+          <Text size={TEXT_SIZE.SMALL} color={TEXT_COLOR.GRAY}>Background:
+            <label className="docs-active-block__label">
+              <Radio name={UUID.v1()} checked={!this.props.values.changeBackground}
+                onChange={this.onChangeBackgroundDefault} /> none
+            </label>
+            <label className="docs-active-block__label">
+              <Radio name={UUID.v1()} checked={this.props.values.changeBackground === 'light'}
+                onChange={this.onChangeBackgroundLight} /> light
+            </label>
+            <label className="docs-active-block__label">
+              <Radio name={UUID.v1()} checked={this.props.values.changeBackground === 'dark'}
+                onChange={this.onChangeBackgroundDark} /> dark
+            </label>
+          </Text>
+        </ActionList>
+      </div>);
+  }
+}
 
 DocsActiveBlockSettings.propTypes = {
   values: PropTypes.object.isRequired,
