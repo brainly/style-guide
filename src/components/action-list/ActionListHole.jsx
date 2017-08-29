0c6d75f838243357c8738d296f43985aa0e7e0dd
vergilius <michal.pawlinski@arteverest.pl>
2017-08-29 13:08:07 +0200

    Update Action List Hole

--- src/components/action-list/ActionListHole.jsx
+++ src/components/action-list/ActionListHole.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-const ActionListHole = ({children, className, ...props}) => {
-  const finalClassName = classnames('sg-actions-list__hole', className);
+const ActionListHole = ({children, asContainer, noSpacing, noShrink, grow, toEnd, toRight, className, ...props}) => {
+  const actionListHoleClass = classnames('sg-actions-list__hole', {
+    'sg-actions-list__hole--container': asContainer,
+    'sg-actions-list__hole--no-spacing': noSpacing,
+    'sg-actions-list__hole--no-shrink': noShrink,
+    'sg-actions-list__hole--grow': grow,
+    'sg-actions-list__hole--to-end': toEnd,
+    'sg-actions-list__hole--to-right': toRight
+  }, className);
 
   return (
-    <div {...props} className={finalClassName}>
+    <div {...props} className={actionListHoleClass}>
       {children}
     </div>
   );
@@ -14,7 +21,13 @@ const ActionListHole = ({children, className, ...props}) => {
 
 ActionListHole.propTypes = {
   children: PropTypes.node.isRequired,
-  className: PropTypes.string
+  className: PropTypes.string,
+  asContainer: PropTypes.bool,
+  noSpacing: PropTypes.bool,
+  noShrink: PropTypes.bool,
+  grow: PropTypes.bool,
+  toEnd: PropTypes.bool,
+  toRight: PropTypes.bool
 };
 
 export default ActionListHole;
