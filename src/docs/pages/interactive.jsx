4ec95ac359376aab87b4331b0e4aba12c6a82789
Mateusz Wyka <matikrk@gmail.com>
2017-10-20 14:53:40 +0200

    frontend-tools-configs import plugin (#1314)
    * frontend-tools-configs import plugin
    
    * 116.2.2

--- src/docs/pages/interactive.jsx
+++ src/docs/pages/interactive.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ItemsInteractivePage from '../layouts/ItemsInteractivePage';
+
 const interactive = () => <ItemsInteractivePage navigationIdx={3} />;
 
 export default interactive;
