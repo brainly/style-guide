a1f50ab042f0b97235f4955e709de4e0c71a4581
Mateusz Wyka <matikrk@gmail.com>
2017-12-29 10:21:11 +0100

    size, color, type of header from one file

--- src/components/text/HeaderSecondary.jsx
+++ src/components/text/HeaderSecondary.jsx
@@ -1,25 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-
-const HEADER_TYPE = {
-  H1: 'h1',
-  H2: 'h2',
-  H3: 'h3',
-  H4: 'h4',
-  H5: 'h5',
-  H6: 'h6'
-};
-
-const HEADER_SIZE = {
-  NORMAL: 'normal',
-  SMALL: 'small',
-  XSMALL: 'xsmall'
-};
-
-const HEADER_COLOR = {
-  LIGHT: 'light'
-};
+import {HEADER_COLOR, HEADER_SIZE, HEADER_TYPE} from './headerConsts';
 
 const HeaderSecondary = ({children, type = HEADER_TYPE.H1, size = HEADER_SIZE.NORMAL, color, className, ...props}) => {
   const Type = type;
