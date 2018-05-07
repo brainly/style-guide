8577698390e88d6552d3ceceec3792de30daba63
Michał Pawliński <michal.pawlinski@arteverest.pl>
2018-05-07 12:47:25 +0200

    Reorganize Badges (#1415)
    * Reorganize Badges
    
    * Update!
    
    * Update structure
    
    * v134.2.1

---
+++ src/components/badges/TextBadge.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Badge, {BADGE_SIZE, BADGE_COLOR} from './subcomponents/Badge';
+import Text, {SIZE as TEXT_SIZE, COLOR as TEXT_COLOR, WEIGHT as TEXT_WEIGHT} from '../text/Text';
+
+export const TEXT_BADGE_SIZE = BADGE_SIZE;
+export const TEXT_BADGE_COLOR = {
+  NORMAL: {BADGE: BADGE_COLOR.NORMAL, TEXT: TEXT_COLOR.DEFAULT},
+  PEACH: {BADGE: BADGE_COLOR.PEACH, TEXT: TEXT_COLOR.LIGHT},
+  MUSTARD: {BADGE: BADGE_COLOR.MUSTARD, TEXT: TEXT_COLOR.LIGHT},
+  MINT: {BADGE: BADGE_COLOR.MINT, TEXT: TEXT_COLOR.LIGHT},
+  MINT_SECONDARY: {BADGE: BADGE_COLOR.MINT_SECONDARY, TEXT: TEXT_COLOR.LIGHT},
+  BLUE_SECONDARY: {BADGE: BADGE_COLOR.BLUE_SECONDARY, TEXT: TEXT_COLOR.LIGHT},
+  BLUE: {BADGE: BADGE_COLOR.BLUE, TEXT: TEXT_COLOR.LIGHT},
+  GRAY_SECONDARY: {BADGE: BADGE_COLOR.GRAY_SECONDARY, TEXT: TEXT_COLOR.LIGHT},
+  MINT_SECONDARY_LIGHT: {BADGE: BADGE_COLOR.MINT_SECONDARY_LIGHT, TEXT: TEXT_COLOR.MINT},
+  PEACH_SECONDARY_LIGHT: {BADGE: BADGE_COLOR.PEACH_SECONDARY_LIGHT, TEXT: TEXT_COLOR.PEACH},
+  BLUE_SECONDARY_LIGHT: {BADGE: BADGE_COLOR.BLUE_SECONDARY_LIGHT, TEXT: TEXT_COLOR.BLUE}
+};
+
+const TextBadge = ({
+  children,
+  color = TEXT_BADGE_COLOR.NORMAL,
+  size = BADGE_SIZE.NORMAL,
+  rounded,
+  withAnimation,
+  className,
+  ...props
+}) => {
+  const textSize = size === BADGE_SIZE.LARGE ? TEXT_SIZE.NORMAL : TEXT_SIZE.XSMALL;
+
+  return (
+    <Badge {...props} color={color.BADGE} size={size} rounded={rounded} withAnimation={withAnimation}>
+      <Text size={textSize} weight={TEXT_WEIGHT.BOLD} color={color.TEXT}>{children}</Text>
+    </Badge>
+  );
+};
+
+TextBadge.propTypes = {
+  color: PropTypes.oneOf(Object.values(TEXT_BADGE_COLOR)),
+  size: PropTypes.oneOf(Object.values(BADGE_SIZE)),
+  children: PropTypes.string.isRequired,
+  rounded: PropTypes.bool,
+  withAnimation: PropTypes.bool,
+  className: PropTypes.string
+};
+
+export default TextBadge;
---
+++ src/components/badges/TextBadge.spec.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import TextBadge, {TEXT_BADGE_COLOR, TEXT_BADGE_SIZE} from './TextBadge';
+import Badge from './subcomponents/Badge';
+import Text, {SIZE as TEXT_SIZE, COLOR as TEXT_COLOR} from '../text/Text';
+import {shallow, mount} from 'enzyme';
+
+describe('<TextBadge />', () => {
+  it('render itself without error', () => {
+    const component = shallow(
+      <TextBadge>Test</TextBadge>
+    );
+
+    const badge = component.find(Badge);
+
+    expect(badge).toHaveLength(1);
+  });
+
+  it('sets <Text /> component size - default', () => {
+    const badge = mount(
+      <TextBadge>Test</TextBadge>
+    );
+    const text = badge.find(Text);
+
+    expect(text.props().size).toEqual(TEXT_SIZE.XSMALL);
+  });
+
+  it('sets <Text /> component size - small', () => {
+    const badge = shallow(
+      <TextBadge size={TEXT_BADGE_SIZE.SMALL}>Test</TextBadge>
+    );
+
+    expect(badge.find(Badge).children().props().size).toEqual(TEXT_SIZE.XSMALL);
+  });
+
+  it('sets <Text /> component size - large', () => {
+    const badge = shallow(
+      <TextBadge size={TEXT_BADGE_SIZE.LARGE}>Test</TextBadge>
+    );
+
+    expect(badge.find(Badge).children().props().size).toEqual(TEXT_SIZE.NORMAL);
+  });
+
+  it('maps <Text /> and <Badge /> colors', () => {
+    const component = shallow(
+      <TextBadge color={TEXT_BADGE_COLOR.MINT_SECONDARY}>Test</TextBadge>
+    );
+    const badge = component.find(Badge);
+
+    expect(badge.children().props().color).toEqual(TEXT_COLOR.LIGHT);
+  });
+
+  it('animates when withAnimation is passed <Badge />', () => {
+    const component = shallow(
+      <TextBadge withAnimation>Test</TextBadge>
+    );
+    const badge = component.find(Badge);
+
+    expect(badge.props().withAnimation).toEqual(true);
+  });
+
+  it('is is styled differently when rounded is passed <Badge />', () => {
+    const component = shallow(
+      <TextBadge rounded>Test</TextBadge>
+    );
+    const badge = component.find(Badge);
+
+    expect(badge.props().rounded).toEqual(true);
+  });
+
+  it('logs error when has no child', () => {
+    const spy = jest.spyOn(console, 'error');
+
+    console.error = jest.fn();
+    shallow(<TextBadge />);
+    expect(console.error.mock.calls).toHaveLength(1);
+
+    spy.mockRestore();
+  });
+
+  it('logs error when passing a react element as a child', () => {
+    const spy = jest.spyOn(console, 'error');
+
+    console.error = jest.fn();
+    shallow(
+      <TextBadge>
+        <div>asd</div>
+      </TextBadge>);
+    expect(console.error.mock.calls).toHaveLength(1);
+
+    spy.mockRestore();
+  });
+});
