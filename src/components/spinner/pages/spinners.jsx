f1b124190ffeddd67d0dc5a729dd9e3735564873
Bartosz Lorek <bery.lorek@gmail.com>
2018-07-12 14:39:53 +0200

    add spinner sizes and change color (#1465)
    * add spinner sizes and change color
    
    * v136.4.0

--- src/components/spinner/pages/spinners.jsx
+++ src/components/spinner/pages/spinners.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import DocsBlock from 'components/DocsBlock';
-import Spinner from '../Spinner';
+import ContrastBox from 'components/ContrastBox';
+import Spinner, {SPINNER_SIZE} from '../Spinner';
 
 const Spinners = () => (
   <React.Fragment>
-    <DocsBlock info="Default">
+    <DocsBlock info="Normal" additionalInfo="(Default)">
       <Spinner />
     </DocsBlock>
-    <DocsBlock info="Gray">
-      <Spinner gray />
+    <DocsBlock info="Small">
+      <Spinner size={SPINNER_SIZE.SMALL} />
+    </DocsBlock>
+    <DocsBlock info="XSmall">
+      <Spinner size={SPINNER_SIZE.XSMALL} />
+    </DocsBlock>
+    <DocsBlock info="XXSmall">
+      <Spinner size={SPINNER_SIZE.XXSMALL} />
+    </DocsBlock>
+    <DocsBlock info="Light">
+      <ContrastBox>
+        <Spinner light />
+      </ContrastBox>
     </DocsBlock>
   </React.Fragment>
 );
