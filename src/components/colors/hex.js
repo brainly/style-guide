96fa4da66c2f4d6f0b8c1584a0de413932fd862c
bartoszlorek <bartosz.lorek@brainly.com>
2018-07-27 15:13:03 +0200

    export colors as a hex values

---
+++ src/components/colors/hex.js
@@ -0,0 +1,11 @@
+import colors from './colors';
+
+const hex = {};
+
+Object.keys(colors).forEach(groupName => {
+  colors[groupName].forEach(color => {
+    hex[color.variable.slice(1)] = `#${color.hex}`;
+  });
+});
+
+export default hex;
