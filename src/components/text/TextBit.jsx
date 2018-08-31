2f0ca13583d7f0cb83bacf10d69becb52986e017
patrycjaradaczynska <patrycja.radaczynska@brainly.com>
2018-08-31 18:00:55 +0200

    Update BIT text sizes and namespaces

--- src/components/text/TextBit.jsx
+++ src/components/text/TextBit.jsx
@@ -15,24 +15,22 @@ export const TYPE = {
 export const SIZE = {
   SMALL: 'small',
   LARGE: 'large',
-  XLARGE: 'xlarge',
-  XXLARGE: 'xxlarge'
+  XLARGE: 'xlarge'
 };
 
 export const COLOR = {
-  ALT: 'alt',
-  LIGHT: 'light',
-  DARK: 'dark',
-  GRAY: 'gray',
-  WARNING: 'warning'
+  BLUE_SECONDARY: 'blue-secondary',
+  WHITE: 'white',
+  BLACK: 'black',
+  GRAY_SECONDARY: 'gray-secondary',
+  PEACH_PRIMARY: 'peach-primary'
 };
 
 const TextBit = ({children, type = TYPE.H1, color, size, notResponsive, className, ...props}) => {
   const Type = type;
   const textClass = classNames('sg-text-bit', {
     [`sg-text-bit--${size}`]: size,
-    [`sg-text-bit--${color}`]: color,
-    'sg-text-bit--not-responsive': notResponsive
+    [`sg-text-bit--${color}`]: color
   }, className);
 
   return (
