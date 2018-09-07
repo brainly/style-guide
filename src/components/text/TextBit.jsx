24b4c2d1bec295e3ecd60c35611ca05aa323f47a
Patrycja Radaczyńska <trycjapa@gmail.com>
2018-09-07 11:54:16 +0200

    Bit update (#1485)

--- src/components/text/TextBit.jsx
+++ src/components/text/TextBit.jsx
@@ -15,24 +15,22 @@ export const TYPE = {
 export const SIZE = {
   SMALL: 'small',
   LARGE: 'large',
-  XLARGE: 'xlarge',
-  XXLARGE: 'xxlarge'
+  XLARGE: 'xlarge'
 };
 
 export const COLOR = {
-  ALT: 'alt',
-  LIGHT: 'light',
-  DARK: 'dark',
-  GRAY: 'gray',
-  WARNING: 'warning'
+  BLUE_SECONDARY: 'blue-secondary',
+  WHITE: 'white',
+  BLACK: 'black',
+  GRAY_SECONDARY: 'gray-secondary',
+  PEACH_PRIMARY: 'peach-primary'
 };
 
-const TextBit = ({children, type = TYPE.H1, color, size, notResponsive, className, ...props}) => {
+const TextBit = ({children, type = TYPE.H1, color, size, className, ...props}) => {
   const Type = type;
   const textClass = classNames('sg-text-bit', {
     [`sg-text-bit--${size}`]: size,
-    [`sg-text-bit--${color}`]: color,
-    'sg-text-bit--not-responsive': notResponsive
+    [`sg-text-bit--${color}`]: color
   }, className);
 
   return (
@@ -47,7 +45,6 @@ TextBit.propTypes = {
   size: PropTypes.oneOf(Object.values(SIZE)),
   color: PropTypes.oneOf(Object.values(COLOR)),
   type: PropTypes.oneOf(Object.values(TYPE)),
-  notResponsive: PropTypes.bool,
   className: PropTypes.string
 };
 
