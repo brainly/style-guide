02e5caef7a11c084037dabba6897afad98ec7fc6
Andrzej Maciurzynski <andrzej.maciurzynski@brainly.com>
2018-10-15 12:37:57 +0200

    up

--- src/components/content-box/ContentBoxConstants.jsx
+++ src/components/content-box/ContentBoxConstants.jsx
@@ -1,5 +1,5 @@
 // @flow
-const SIZE = {
+const SIZE = Object.freeze({
   XXSMALL: 'xxsmall',
   XSMALL: 'xsmall',
   SMALL: 'small',
@@ -7,12 +7,12 @@ const SIZE = {
   LARGE: 'large',
   XLARGE: 'xlarge',
   XXLARGE: 'xxlarge'
-};
+});
 
-const ALIGNMENT = {
+const ALIGNMENT = Object.freeze({
   LEFT: 'left',
   CENTER: 'center',
   RIGHT: 'right'
-};
+});
 
 export {SIZE, ALIGNMENT};
