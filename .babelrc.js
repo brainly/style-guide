da219d5f8735af6280010b44cfb7b7323a0b527c
IgorSbitnev <PinkaminaDianePie@gmail.com>
2019-02-05 14:20:45 +0100

    bump babel to v7

---
+++ .babelrc.js
@@ -0,0 +1,24 @@
+const babelEnv = params => [
+  '@babel/preset-env',
+  {
+    targets: '> 0.2%, not dead, not ie < 11',
+    ...params,
+  }
+];
+
+module.exports = api => {
+  api.cache(true);
+  return {
+    presets: [babelEnv({modules: false}), '@babel/preset-react'],
+    plugins: [
+      '@babel/plugin-transform-flow-strip-types',
+      '@babel/plugin-proposal-object-rest-spread',
+      '@babel/plugin-proposal-class-properties',
+    ],
+    env: {
+      test: {
+        presets: [babelEnv({modules: 'auto'})],
+      }
+    }
+  }
+};
