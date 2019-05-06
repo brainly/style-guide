faf7278ed3145575cbb1335b4657e437d06452a4
IgorSbitnev <PinkaminaDianePie@gmail.com>
2019-05-06 09:37:23 +0200

    fixed recursive copy on build

--- scripts/tasks/copy-latest.js
+++ scripts/tasks/copy-latest.js
@@ -1,6 +1,6 @@
 module.exports = function(gulp, plugins, consts) {
   return function() {
-    const input = plugins.path.join(consts.VERSIONED_DIST, '*');
+    const input = plugins.path.join(consts.VERSIONED_DIST, '**/*');
     const latestDist = plugins.path.join(consts.LATEST_DIST);
 
     return gulp.src([input])
