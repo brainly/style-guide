e4758e756b5131fa4020255306d166f2114a3c54
Patrycja Radaczyńska <patrycja.radaczynska@brainly.com>
2019-07-22 09:24:42 +0200

    Introduce new set of icons with STD prefix (#1599)

--- src/docs/layouts/ItemsInteractivePage.jsx
+++ src/docs/layouts/ItemsInteractivePage.jsx
@@ -21,6 +21,7 @@ const ItemsInteractivePage = ({navigationIdx}: PropsType) => {
       <body>
 
         <script src="images/icons.js" async />
+        <script src="images/std-icons.js" async />
         <script src="images/subjects-icons.js" async />
         <script src="images/subjects-mono-icons.js" async />
         <script src="images/math-symbols-icons.js" async />
