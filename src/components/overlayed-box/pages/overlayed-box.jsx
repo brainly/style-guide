58d0b1e542be4d67dacd8bbbeb69882f44f6af4b
matzimowski <mat.zimowski@gmail.com>
2019-08-27 12:30:22 +0200

    Remove example with comment icon

--- src/components/overlayed-box/pages/overlayed-box.jsx
+++ src/components/overlayed-box/pages/overlayed-box.jsx
@@ -38,9 +38,6 @@ const overlayedBoxs = () => (
         <OverlayedBox overlay={<Sticker type={STICKER_TYPE.STD_SEARCH} />}>
           <Avatar imgSrc="https://source.unsplash.com/64x64/?man" />
         </OverlayedBox>
-        <OverlayedBox overlay={<Sticker type={STICKER_TYPE.STD_COMMENT} />}>
-          <Avatar />
-        </OverlayedBox>
         <OverlayedBox overlay={<Sticker type={STICKER_TYPE.STD_FRIENDS} />}>
           <Avatar />
         </OverlayedBox>
