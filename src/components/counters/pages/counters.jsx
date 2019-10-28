3adba60124c5221882b10f8186c87c712cd90988
patrycjaradaczynska <patrycja.radaczynska@brainly.com>
2019-10-28 18:16:57 +0100

    Adjust basic small

--- src/components/counters/pages/counters.jsx
+++ src/components/counters/pages/counters.jsx
@@ -4,13 +4,22 @@ import Counter from '../Counter';
 
 const Counters = () => (
   <div>
-    <DocsBlock info="Notifications counter">
-      <Counter type="basic" size="small">
-        1
-      </Counter>
+    <DocsBlock info="Basic counter">
+      <Counter type="basic">1</Counter>
       <Counter type="basic">123455</Counter>
       <Counter type="basic" withAnimation>
         12
+      </Counter>
+    </DocsBlock>
+    <DocsBlock info="Basic counter small">
+      <Counter type="basic" size="small">
+        5
+      </Counter>
+      <Counter type="basic" size="small">
+        55
+      </Counter>
+      <Counter type="basic" size="small" withAnimation>
+        5
       </Counter>
     </DocsBlock>
 
@@ -18,6 +27,8 @@ const Counters = () => (
       <Counter type="points" withPointer>
         5 pts
       </Counter>
+    </DocsBlock>
+    <DocsBlock info="Points counter small">
       <Counter type="points" size="small">
         10
       </Counter>
