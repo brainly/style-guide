46762790104661cbe65d006d292b2bbc2a694e1d
Patrycja Radaczyńska <patrycja.radaczynska@brainly.com>
2019-11-12 16:23:20 +0100

    Bugfix export of counter (#1667)

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ export {default as ButtonSecondary} from './components/buttons/ButtonSecondary';
 export {default as Card} from './components/card/Card';
 export {default as CardHole} from './components/card/CardHole';
 export {default as ContentBox} from './components/content-box/ContentBox';
+export {default as Counter} from './components/counters/Counter';
 export {
   default as ContentBoxActions,
 } from './components/content-box/ContentBoxActions';
