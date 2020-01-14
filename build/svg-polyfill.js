2d9eb4c78280e5ff6d68c3444f95be3f347b7d1b
Maciej Watroba <maciej.watroba@brainly.com>
2020-01-14 15:36:15 +0100

    Switch to whitelist in package task

--- build/svg-polyfill.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  var svgWrapper = document.createElement('div');
-  svgWrapper.style.display = 'none';
-  svgWrapper.innerHTML = '#SVG#';
-  document.body.insertBefore(svgWrapper, document.body.firstChild);
-})();
