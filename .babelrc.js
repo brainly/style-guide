9753e7c5659b45328c8555d530e39390604ec5be
Maciej Watroba <maciej.watroba@brainly.com>
2020-01-15 12:07:00 +0100

    Build both esm and commonjs

--- .babelrc.js
+++ .babelrc.js
@@ -1,23 +1,36 @@
 const babelEnv = params => [
-  '@babel/preset-env',
-  Object.assign(  {
-    targets: '> 0.2%, not dead, not ie < 11',
-  }, params)
+  "@babel/preset-env",
+  Object.assign(
+    {
+      targets: "> 0.2%, not dead, not ie < 11"
+    },
+    params
+  )
 ];
 
 module.exports = api => {
   api.cache(true);
   return {
-    presets: [babelEnv({modules: false}), '@babel/preset-react', '@babel/preset-flow'],
+    presets: [
+      babelEnv({ modules: false }),
+      "@babel/preset-react",
+      "@babel/preset-flow"
+    ],
     plugins: [
-      '@babel/plugin-transform-runtime',
-      '@babel/plugin-proposal-object-rest-spread',
-      '@babel/plugin-proposal-class-properties',
+      "@babel/plugin-proposal-object-rest-spread",
+      "@babel/plugin-proposal-class-properties"
     ],
     env: {
       test: {
-        presets: [babelEnv({modules: 'auto'})],
+        presets: [babelEnv({ modules: "auto" })]
+      },
+      commonjs: {
+        presets: [babelEnv({ modules: "auto" })],
+      },
+      esm: {
+        presets: [babelEnv({ modules: false })],
+        plugins: [["@babel/plugin-transform-runtime", { useESModules: true }]]
       }
     }
-  }
+  };
 };
