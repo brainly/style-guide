2290a3711c50365e2d462837540cf619ff796971
Maciej Watroba <maciej.watroba@brainly.com>
2020-01-15 09:47:53 +0100

    Prepare for publishing

--- build/components/colors/hex.spec.js
+++
@@ -1,6 +0,0 @@
-import colors from './colors';
-import hex from './hex';
-test('should export colors as a hex values', function () {
-  expect(hex.black).toBe("#".concat(colors.primary[0].hex));
-  expect(hex.lavenderPrimaryDark).toBe("#".concat(colors.secondary[0].hex));
-});
--- build/js/generateRandomString.js
+++
@@ -1,5 +0,0 @@
-var generateRandomString = function generateRandomString() {
-  return Math.random().toString(32).slice(2);
-};
-
-export default generateRandomString;
