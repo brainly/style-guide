9ad9001e4fae39dbfdd8e9c12ac0aa66ec7e73f8
Mateusz Wyka <matikrk@gmail.com>
2020-02-12 14:55:49 +0100

    Close gulp task when no sending file (#1730)

--- scripts/tasks/upload-files.js
+++ scripts/tasks/upload-files.js
@@ -13,28 +13,32 @@ module.exports = function(gulp, plugins, consts) {
         Bucket: consts.BUCKET_NAME,
         Key: `${consts.VERSION}/style-guide.css`,
       },
-      function(notExits) {
-        if (notExits) {
-          const params = {
-            localDir: consts.DIST,
-            s3Params: {
-              Bucket: consts.BUCKET_NAME,
-            },
-          };
-          const uploader = client.uploadDir(params);
-
-          uploader.on('error', function(err) {
-            console.error('unable to upload:', err.stack);
-          });
-          uploader.on('progress', function() {
-            console.log(
-              'Upload progress',
-              uploader.progressAmount,
-              uploader.progressTotal
-            );
-          });
-          uploader.on('end', done);
+      function(notExist) {
+        if (!notExist) {
+          // no upload when file exist
+          done();
+          return;
         }
+
+        const params = {
+          localDir: consts.DIST,
+          s3Params: {
+            Bucket: consts.BUCKET_NAME,
+          },
+        };
+        const uploader = client.uploadDir(params);
+
+        uploader.on('error', function(err) {
+          console.error('unable to upload:', err.stack);
+        });
+        uploader.on('progress', function() {
+          console.log(
+            'Upload progress',
+            uploader.progressAmount,
+            uploader.progressTotal
+          );
+        });
+        uploader.on('end', done);
       }
     );
   };
