88b409c6f0421af2e7d26a08537df1334400950f
Artur Malinowski <malinicz@gmail.com>
2020-03-05 09:49:41 +0100

    Replace old dropdown (#1733)
    * replace old dropdown
    
    * remove unused variables
    
    * rename all dropdown files to deprecated
    
    * Make dropdown not deprecated, add additional prop 'initiallyOpened'

--- src/components/dropdowns/Dropdown.spec.jsx
+++ src/components/dropdowns/Dropdown.spec.jsx
@@ -1,27 +1,66 @@
 import React from 'react';
+import {shallow, mount} from 'enzyme';
+import {act} from 'react-dom/test-utils';
+
 import Dropdown from './Dropdown';
-import {shallow} from 'enzyme';
 
-const item1st = {id: 'csdsd', text: '1st item'};
-const item2nd = {id: 'sdfsdfg', text: '2nd item'};
-const item3rd = {id: 'fdpks', text: '3rd item'};
-const defaultItems = [item1st, item2nd, item3rd];
-const defaultLabel = 'default Label';
-const defaultProps = {items: defaultItems, label: defaultLabel};
+const testItems = [
+  {label: 'text-1', url: 'url-1'},
+  {label: 'text-2', url: 'url-2'},
+  {label: 'text-3', url: 'url-3'},
+];
 
-test('render', () => {
-  const dropDown = shallow(<Dropdown {...defaultProps} />);
+const testProps = {
+  links: testItems,
+  name: 'brainly',
+};
 
-  expect(dropDown.hasClass('sg-dropdown')).toEqual(true);
-});
+describe('<Dropdown />', () => {
+  it('should render name', () => {
+    const component = shallow(<Dropdown {...testProps} />);
 
-test('default full width', () => {
-  const dropDown = shallow(<Dropdown {...defaultProps} />);
+    expect(component.find('p').text()).toMatch(testProps.name);
+  });
 
-  expect(dropDown.hasClass('sg-dropdown--full-width')).toEqual(true);
-});
-test('no full width', () => {
-  const dropDown = shallow(<Dropdown {...defaultProps} fullWidth={false} />);
+  it('should render links with proper data', () => {
+    const component = shallow(<Dropdown {...testProps} />);
 
-  expect(dropDown.hasClass('sg-dropdown--full-width')).toEqual(false);
+    component.simulate('click');
+
+    const link = component.find('a').at(0);
+
+    expect(link.prop('href')).toBe(testItems[0].url);
+  });
+
+  it('should not be open by default', () => {
+    const component = shallow(<Dropdown {...testProps} />);
+
+    expect(component.find('.sg-dropdown__items')).toHaveLength(0);
+  });
+
+  it('should open dropdown on click', () => {
+    const component = shallow(<Dropdown {...testProps} />);
+
+    component.simulate('click');
+    expect(component.find('.sg-dropdown__items')).toHaveLength(1);
+  });
+
+  it('should close dropdown when click outside', () => {
+    const component = mount(<Dropdown {...testProps} />);
+
+    act(() => {
+      component.simulate('click');
+      document.dispatchEvent(new Event('click')); // simulate bubbling
+    });
+
+    component.update();
+    expect(component.find('.sg-dropdown__items')).toHaveLength(1);
+
+    act(() => {
+      document.dispatchEvent(new Event('click'));
+    });
+
+    component.update();
+    expect(component.find('.sg-dropdown__items')).toHaveLength(0);
+  });
 });
