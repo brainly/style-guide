d86542065d8a6d1e003cc35779977242ae918a79
matzimowski <mat.zimowski@gmail.com>
2020-06-08 11:19:09 +0200

    Update color category names

--- src/components/colors/hex.spec.js
+++ src/components/colors/hex.spec.js
@@ -2,6 +2,6 @@ import colors from './colors';
 import hex from './hex';
 
 test('should export colors as a hex values', () => {
-  expect(hex.black).toBe(`#${colors.primary[0].hex}`);
-  expect(hex.lavenderPrimaryDark).toBe(`#${colors.secondary[0].hex}`);
+  expect(hex.black).toBe(`#${colors.core[0].hex}`);
+  expect(hex.lavenderPrimaryDark).toBe(`#${colors.additional[0].hex}`);
 });
