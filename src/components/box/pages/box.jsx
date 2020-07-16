086031a74c6db32c0c9f4677e9140837d51cbe9e
malinicz <malinicz@gmail.com>
2020-07-16 18:13:46 +0200

    refactor box page

--- src/components/box/pages/box.jsx
+++ src/components/box/pages/box.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import DocsBlock from 'components/DocsBlock';
-
-import Box from '../Box';
 import Text from 'text/Text';
+import Headline from 'text/Headline';
 import Flex from 'flex/Flex';
 import Icon from 'icons/Icon';
-import Headline from '../../text/Headline';
+
+import Box, {COLOR, PADDING} from '../Box';
 
 const Boxes = () => (
   <div>
@@ -32,161 +32,25 @@ const Boxes = () => (
 
     <DocsBlock info="Border colors">
       <Flex wrap>
-        <Flex marginRight="s" marginBottom="s">
-          <Box border borderColor="gray-secondary-lightest" padding="xs">
-            <Text size="small">Gray secondary lightest (default)</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box border borderColor="gray-secondary-ultra-light" padding="xs">
-            <Text size="small">Gray secondary ultra light</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box border borderColor="dark" padding="xs">
-            <Text size="small">Dark</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box border borderColor="blue" padding="xs">
-            <Text size="small">Blue</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box border borderColor="blue-secondary" padding="xs">
-            <Text size="small">Blue secondary</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box border borderColor="blue-secondary-light" padding="xs">
-            <Text size="small">Blue secondary light</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box border borderColor="lavender" padding="xs">
-            <Text size="small">Lavender</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box border borderColor="mint" padding="xs">
-            <Text size="small">Mint</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box border borderColor="mint-secondary" padding="xs">
-            <Text size="small">Mint secondary</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box border borderColor="mint-secondary-light" padding="xs">
-            <Text size="small">Mint secondary light</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box border borderColor="mint-secondary-ultra-light" padding="xs">
-            <Text size="small">Mint secondary ultra light</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box border borderColor="mustard-primary" padding="xs">
-            <Text size="small">Mustard primary</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box border borderColor="peach" padding="xs">
-            <Text size="small">Peach</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box border borderColor="peach-secondary" padding="xs">
-            <Text size="small">Peach secondary</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box border borderColor="peach-secondary-light" padding="xs">
-            <Text size="small">Peach secondary light</Text>
-          </Box>
-        </Flex>
+        {Object.values(COLOR).map(color => (
+          <Flex key={color} marginRight="s" marginBottom="s">
+            <Box border borderColor={color} padding="xs">
+              <Text size="small">{color}</Text>
+            </Box>
+          </Flex>
+        ))}
       </Flex>
     </DocsBlock>
 
     <DocsBlock info="Background colors">
       <Flex wrap>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="gray-secondary-lightest" padding="xs">
-            <Text size="small">Gray secondary lightest</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="gray-secondary-ultra-light" padding="xs">
-            <Text size="small">Gray secondary ultra light</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="dark" padding="xs">
-            <Text size="small">Dark</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="blue" padding="xs">
-            <Text size="small">Blue</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="blue-secondary" padding="xs">
-            <Text size="small">Blue secondary</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="blue-secondary-light" padding="xs">
-            <Text size="small">Blue secondary light</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="lavender" padding="xs">
-            <Text size="small">Lavender</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="mint" padding="xs">
-            <Text size="small">Mint</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="mint-secondary" padding="xs">
-            <Text size="small">Mint secondary</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="mint-secondary-light" padding="xs">
-            <Text size="small">Mint secondary light</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="mint-secondary-ultra-light" padding="xs">
-            <Text size="small">Mint secondary ultra light</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="mustard-primary" padding="xs">
-            <Text size="small">Mustard primary</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="peach" padding="xs">
-            <Text size="small">Peach</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="peach-secondary" padding="xs">
-            <Text size="small">Peach secondary</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="peach-secondary-light" padding="xs">
-            <Text size="small">Peach secondary light</Text>
-          </Box>
-        </Flex>
+        {Object.values(COLOR).map(color => (
+          <Flex key={color} marginRight="s" marginBottom="s">
+            <Box color={color} padding="xs">
+              <Text size="small">{color}</Text>
+            </Box>
+          </Flex>
+        ))}
       </Flex>
     </DocsBlock>
 
@@ -197,36 +61,14 @@ const Boxes = () => (
             <Text size="small">No padding</Text>
           </Box>
         </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="peach-secondary-light" padding="xxs">
-            <Text size="small">Padding xxs</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="peach-secondary-light" padding="xs">
-            <Text size="small">Padding xs</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="peach-secondary-light" padding="s">
-            <Text size="small">Padding s</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="peach-secondary-light" padding="m">
-            <Text size="small">Padding m (default)</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="peach-secondary-light" padding="l">
-            <Text size="small">Padding l</Text>
-          </Box>
-        </Flex>
-        <Flex marginRight="s" marginBottom="s">
-          <Box color="peach-secondary-light" padding="xl">
-            <Text size="small">Padding xl</Text>
-          </Box>
-        </Flex>
+
+        {Object.values(PADDING).map(padding => (
+          <Flex key={padding} marginRight="s" marginBottom="s">
+            <Box color="peach-secondary-light" padding={padding}>
+              <Text size="small">Padding {padding}</Text>
+            </Box>
+          </Flex>
+        ))}
       </Flex>
     </DocsBlock>
 
