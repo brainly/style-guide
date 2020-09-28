461bb672f2abce85ba4f993b4cd9f668a2edecbe
coderitual <skowronkow@gmail.com>
2020-09-28 11:27:09 +0200

    Update intro docs

--- src/sass/pages/utils/introduction.jsx
+++ src/sass/pages/utils/introduction.jsx
@@ -7,8 +7,24 @@ const index = () => (
       Utilities are single purpose flexible classes based on a design system
       variables. They are meant to provide building blocks for custom designs in
       application and styleguide without writing additional CSS. Because of the
-      nature of utility classes they are good choice to override other styles in
-      places which need additional tweaking.
+      nature of utility classes they are also good choice to override other
+      styles in places which need additional tweaking.
+    </DocsBlock>
+    <DocsBlock info="Responsive design">
+      Utility classes can provide responsive versions available by prefix the
+      utility with the breakpoint name using following formula:
+      <code>[breakpoint]:sg-[utility-class]</code>
+    </DocsBlock>
+    <DocsBlock info="Pseudo variants">
+      Utility classes can be also applied or changed per pseudo class versions
+      available by prefix the utility with the pseudo variant name using
+      following formula:
+      <code>[pseudo]:sg-[utility-class]</code> character.
+    </DocsBlock>
+    <DocsBlock info="Mix variants">
+      Responsive design prefix work with pseudo variants using the following
+      forumla:
+      <code>[breakpoint]:[pseudo]:sg-[utility-class]</code> character.
     </DocsBlock>
   </div>
 );
