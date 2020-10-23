9a6d1fc426454fb015096e9c760c9657b5eae220
coderitual <skowronkow@gmail.com>
2020-10-23 10:09:14 +0200

    Remove unused

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -6,30 +6,11 @@ export default create({
   colorPrimary: '#60d399',
   colorSecondary: '#60d399',
 
-  // UI
-  // appBg: 'white',
-  // appContentBg: 'white',
-  // appBorderColor: 'grey',
-  // appBorderRadius: 8,
-
   // Typography
   fontBase: '"ProximaNova", "Helvetica", "Arial", sans-serif',
-  // fontCode: 'monospace',
+  fontCode: 'monospace',
 
-  // Text colors
-  // textColor: 'black',
-  // textInverseColor: 'rgba(255,255,255,0.9)',
-
-  // Toolbar default and active colors
-  // barTextColor: 'silver',
   barSelectedColor: '#60d399',
-  // barBg: 'pink',
-
-  // // Form colors
-  // inputBg: 'pink',
-  // inputBorder: 'silver',
-  // inputTextColor: 'black',
-  // inputBorderRadius: 4,
 
   brandTitle: 'Brainly',
   brandUrl: '/',
