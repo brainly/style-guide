6f6624749967cce9696716de9b2330ecb52fefd6
coderitual <skowronkow@gmail.com>
2020-12-07 17:13:53 +0100

    Add font setting

--- src/tailwind/tailwind.config.js
+++ src/tailwind/tailwind.config.js
@@ -1,6 +1,8 @@
 const colors = require('./colors');
 const spacing = require('./spacing');
 const screens = require('./screens');
+const fontSize = require('./fontSize');
+const fontWeight = require('./fontWeight');
 
 module.exports = {
   purge: [],
@@ -155,32 +157,8 @@ module.exports = {
         'monospace',
       ],
     },
-    fontSize: {
-      xs: ['0.75rem', {lineHeight: '1rem'}],
-      sm: ['0.875rem', {lineHeight: '1.25rem'}],
-      base: ['1rem', {lineHeight: '1.5rem'}],
-      lg: ['1.125rem', {lineHeight: '1.75rem'}],
-      xl: ['1.25rem', {lineHeight: '1.75rem'}],
-      '2xl': ['1.5rem', {lineHeight: '2rem'}],
-      '3xl': ['1.875rem', {lineHeight: '2.25rem'}],
-      '4xl': ['2.25rem', {lineHeight: '2.5rem'}],
-      '5xl': ['3rem', {lineHeight: '1'}],
-      '6xl': ['3.75rem', {lineHeight: '1'}],
-      '7xl': ['4.5rem', {lineHeight: '1'}],
-      '8xl': ['6rem', {lineHeight: '1'}],
-      '9xl': ['8rem', {lineHeight: '1'}],
-    },
-    fontWeight: {
-      thin: '100',
-      extralight: '200',
-      light: '300',
-      normal: '400',
-      medium: '500',
-      semibold: '600',
-      bold: '700',
-      extrabold: '800',
-      black: '900',
-    },
+    fontSize,
+    fontWeight,
     gap: theme => theme('spacing'),
     gradientColorStops: theme => theme('colors'),
     gridAutoColumns: {
