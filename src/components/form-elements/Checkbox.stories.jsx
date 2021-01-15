50f16eaeb68866705793d7dfcb0ef390650d78d3
coderitual <skowronkow@gmail.com>
2021-01-15 17:09:11 +0100

    Add stories for checkbox and radio

---
+++ src/components/form-elements/Checkbox.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Checkbox from './Checkbox';
+
+export default {
+  title: 'Components/Checkbox',
+  parameters: {
+    component: Checkbox,
+  },
+};
+
+export const Default = () => <Checkbox>Button</Checkbox>;
---
+++ src/components/form-elements/Radio.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Radio from './Radio';
+
+export default {
+  title: 'Components/Radio',
+  parameters: {
+    component: Radio,
+  },
+};
+
+export const Default = () => <Radio>Button</Radio>;
