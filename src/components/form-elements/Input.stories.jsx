8cad59013716dd64642bc31634027bfaa45eede7
Mateusz Ścirka <mateusz.scirka@brainly.com>
2021-02-25 18:59:04 +0100

    Prettier

--- src/components/form-elements/Input.stories.jsx
+++ src/components/form-elements/Input.stories.jsx
@@ -1,50 +1,50 @@
-import React, {useState, useEffect} from 'react';
-import Input from './Input'
+import React, { useState } from "react";
+import Input from "./Input";
 
 export default {
-  title: 'Components/Form/Input',
+  title: "Components/Form/Input",
   parameters: {
-    component: Input,
-  },
+    component: Input
+  }
 };
 
-const useInput = (args) => {
-  const [value, setValue] = useState(args.value)
+const useInput = args => {
+  const [value, setValue] = useState(args.value);
 
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
+  const onChange = event => {
+    setValue(event.target.value);
+  };
 
-  return {value, onChange}
-}
+  return { value, onChange };
+};
 
 export const Default = args => {
-  const props = useInput(args)
+  const props = useInput(args);
 
   return <Input {...args} {...props} />;
-}
+};
 
 export const ErroredState = args => {
-  const props = useInput(args)
+  const props = useInput(args);
 
   return <Input {...args} {...props} />;
-}
+};
 
 const args = {
-  value: 'Some input value',
+  value: "Some input value"
 };
 
 const argTypes = {
   value: {
-    type: {name: 'string', required: false},
+    type: { name: "string", required: false }
   },
   errorMessage: {
-    type: {name: 'string', required: false},
-  },
+    type: { name: "string", required: false }
+  }
 };
 
-Default.args = args
-Default.argTypes = argTypes
+Default.args = args;
+Default.argTypes = argTypes;
 
-ErroredState.args = {...args, errorMessage: 'Some error', invalid: true}
-ErroredState.argTypes = argTypes
+ErroredState.args = { ...args, errorMessage: "Some error", invalid: true };
+ErroredState.argTypes = argTypes;
