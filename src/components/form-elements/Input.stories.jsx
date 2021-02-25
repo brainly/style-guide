c937e42d374b5af6c357ff4443deb060ec40be3f
Mateusz Ścirka <mateusz.scirka@brainly.com>
2021-02-25 18:48:52 +0100

    Added input to storybook

--- src/components/form-elements/Input.stories.jsx
+++ src/components/form-elements/Input.stories.jsx
@@ -8,12 +8,43 @@ export default {
   },
 };
 
-export const Default = args => {
+const useInput = (args) => {
   const [value, setValue] = useState(args.value)
 
-  useEffect(() => {
-    console.log(args)
-  }, [args])
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
 
-  return <Input {...args} value={value} onChange={(e) => setValue(e.target.value)} />
+  return {value, onChange}
+}
+
+export const Default = args => {
+  const props = useInput(args)
+
+  return <Input {...args} {...props} />;
+}
+
+export const ErroredState = args => {
+  const props = useInput(args)
+
+  return <Input {...args} {...props} />;
+}
+
+const args = {
+  value: 'Some input value',
 };
+
+const argTypes = {
+  value: {
+    type: {name: 'string', required: false},
+  },
+  errorMessage: {
+    type: {name: 'string', required: false},
+  },
+};
+
+Default.args = args
+Default.argTypes = argTypes
+
+ErroredState.args = {...args, errorMessage: 'Some error', invalid: true}
+ErroredState.argTypes = argTypes
