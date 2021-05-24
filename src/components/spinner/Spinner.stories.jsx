af3f2ce1db7b1f60e8234c647d3d9e81837466ac
coderitual <skowronkow@gmail.com>
2021-05-24 16:12:54 +0200

    Add sizes to the spinner

--- src/components/spinner/Spinner.stories.jsx
+++ src/components/spinner/Spinner.stories.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Spinner from './Spinner';
+import Spinner, {SPINNER_SIZE} from './Spinner';
 
 export default {
   title: 'Components/Spinner',
@@ -9,6 +9,16 @@ export default {
 };
 
 export const Default = args => <Spinner {...args} />;
+
+export const Sizes = () => (
+  <div style={{display: 'flex'}}>
+    {Object.values(SPINNER_SIZE).map((size, index) => (
+      <div key={index} style={{padding: 8}}>
+        <Spinner size={size} />
+      </div>
+    ))}
+  </div>
+);
 
 export const Light = args => <Spinner {...args} />;
 
