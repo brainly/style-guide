cdbce2587fb41b65ca8343ae97eca88ee194c138
coderitual <skowronkow@gmail.com>
2021-05-27 16:54:18 +0200

    Improve button story

--- src/components/buttons/Button.stories.jsx
+++ src/components/buttons/Button.stories.jsx
@@ -1,10 +1,27 @@
 import * as React from 'react';
 import Button from './Button';
+import Icon, {TYPE as ICON_TYPES, ICON_COLOR} from 'icons/Icon';
+
+const allIcons = Object.entries(ICON_TYPES).reduce(
+  (acc, [key, type]) => ({
+    ...acc,
+    [key]: <Icon type={type} color={ICON_COLOR.ADAPTIVE} size={24} />,
+  }),
+  {}
+);
 
 export default {
   title: 'Components/Button',
   parameters: {
     component: Button,
+  },
+  argTypes: {
+    icon: {
+      control: {
+        type: 'select',
+        options: allIcons,
+      },
+    },
   },
 };
 
