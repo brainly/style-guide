4ca6cbfe103515f2d6ac05bd2a256244a8df86ff
Dominika Podgórska <dpodgorskad@gmail.com>
2021-09-29 15:04:08 +0200

    Added counter with suffix to the variants story

--- src/components/counters/Counters.stories.jsx
+++ src/components/counters/Counters.stories.jsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import Counter, { COUNTER_SIZE } from './Counter';
-import { TYPE as ICON_TYPE } from '../icons/Icon';
+import Counter, {COUNTER_SIZE} from './Counter';
+import {TYPE as ICON_TYPE} from '../icons/Icon';
 import Flex from '../flex/Flex';
-import { StoryVariant, StoryVariantTable } from '../../_docs/utils';
+import {StoryVariant, StoryVariantTable} from '../../_docs/utils';
 import Headline from '../text/Headline';
+import Text from '../text/Text';
 
 export default {
   title: 'Components/Counter',
@@ -15,9 +16,9 @@ export default {
     size: 'xs',
   },
   argTypes: {
-    size: { control: { type: 'select', options: COUNTER_SIZE } },
-    withAnimation: { control: 'boolean' },
-    icon: { control: { type: 'select', options: Object.values(ICON_TYPE) } },
+    size: {control: {type: 'select', options: COUNTER_SIZE}},
+    withAnimation: {control: 'boolean'},
+    icon: {control: {type: 'select', options: Object.values(ICON_TYPE)}},
   },
 };
 
@@ -103,8 +104,16 @@ export const Variants = args => (
             </td>
             <td>
               <Flex justifyContent="center">
-                <Counter key={size} {...args} size={size} icon={ICON_TYPE.POINTS}>
+                <Counter
+                  key={size}
+                  {...args}
+                  icon={ICON_TYPE.POINTS}
+                  size={size}
+                >
                   +15
+                  <Text type="span" color="gray-secondary" inherited>
+                    {size !== 'xxs' && ` pts`}{' '}
+                  </Text>
                 </Counter>
               </Flex>
             </td>
