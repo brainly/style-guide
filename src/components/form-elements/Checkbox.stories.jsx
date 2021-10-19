52806c7bb9961b965ff9944a9b89eae18f65d32d
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2021-10-19 11:45:28 +0200

    update radio and checkbox stories look based on figma templates (#2154)
    * update radio stories look based on figma templates
    
    * update checkbox stories based on figma templates

--- src/components/form-elements/Checkbox.stories.jsx
+++ src/components/form-elements/Checkbox.stories.jsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import {StoryVariantTable} from '../../_docs/utils';
+import Flex from '../flex/Flex';
+import Headline from '../text/Headline';
 import Checkbox from './Checkbox';
 
 export default {
@@ -6,11 +9,62 @@ export default {
   parameters: {
     component: Checkbox,
   },
-  args: {
-    children: 'Checkbox',
-  },
 };
 
 export const Default = args => <Checkbox {...args} />;
 
-export const Checked = args => <Checkbox {...args} checked />;
+export const StylesAndStates = args => (
+  <StoryVariantTable>
+    <tbody>
+      <tr>
+        <td />
+        <td>
+          <Headline
+            extraBold
+            transform="uppercase"
+            type="span"
+            color="gray-secondary-light"
+            size="medium"
+          >
+            neutral state
+          </Headline>
+        </td>
+        <td>
+          <Headline
+            extraBold
+            transform="uppercase"
+            type="span"
+            color="gray-secondary-light"
+            size="medium"
+          >
+            checked state
+          </Headline>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <Headline
+            extraBold
+            transform="uppercase"
+            type="span"
+            color="gray-secondary-light"
+            size="medium"
+            align="to-right"
+          >
+            default
+          </Headline>
+        </td>
+        <td>
+          <Flex justifyContent="center">
+            <Checkbox {...args} />
+          </Flex>
+        </td>
+        <td>
+          <Flex justifyContent="center">
+            <Checkbox {...args} checked />
+          </Flex>
+        </td>
+      </tr>
+    </tbody>
+  </StoryVariantTable>
+);
