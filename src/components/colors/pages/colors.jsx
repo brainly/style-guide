a20b78a72e2afe9707fa6c55bd58a7a7cdec388b
Dominika Podgórska <dpodgorskad@gmail.com>
2021-11-23 12:48:16 +0100

    Remove old colors from SG docs

--- src/components/colors/pages/colors.jsx
+++ src/components/colors/pages/colors.jsx
@@ -16,30 +16,6 @@ const colors = () => (
         ))}
       </ol>
     </DocsBlock>
-    <DocsBlock info="Additional colors">
-      <ol className="colors-list">
-        {color.additional.map(({name, variable, hex}) => (
-          <li className="color-box" style={{background: `#${hex}`}} key={name}>
-            <span className="color-box__name">
-              {name} / #{hex}
-            </span>
-            <span className="color-box__variable">{variable}</span>
-          </li>
-        ))}
-      </ol>
-    </DocsBlock>
-    <DocsBlock info="Neutrals">
-      <ol className="colors-list">
-        {color.neutrals.map(({name, variable, hex}) => (
-          <li className="color-box" style={{background: `#${hex}`}} key={name}>
-            <span className="color-box__name">
-              {name} / #{hex}
-            </span>
-            <span className="color-box__variable">{variable}</span>
-          </li>
-        ))}
-      </ol>
-    </DocsBlock>
   </div>
 );
 
