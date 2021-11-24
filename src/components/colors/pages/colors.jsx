d4409a68801a043574b0633f5620b23d57dbcb54
Dominika Podgórska <dpodgorskad@gmail.com>
2021-11-24 11:50:10 +0100

    Add Text and Icon colors documentation to SG and storybook

--- src/components/colors/pages/colors.jsx
+++ src/components/colors/pages/colors.jsx
@@ -16,6 +16,32 @@ const colors = () => (
         ))}
       </ol>
     </DocsBlock>
+
+    <DocsBlock info="Text colors">
+      <ol className="colors-list">
+        {color.text.map(({name, variable, hex}) => (
+          <li className="color-box" style={{background: `#${hex}`}} key={name}>
+            <span className="color-box__name">
+              {name} / #{hex}
+            </span>
+            <span className="color-box__variable">{variable}</span>
+          </li>
+        ))}
+      </ol>
+    </DocsBlock>
+    <DocsBlock info="Icon colors">
+      <ol className="colors-list">
+        {color.icon.map(({name, variable, hex}) => (
+          <li className="color-box" style={{background: `#${hex}`}} key={name}>
+            <span className="color-box__name">
+              {name} / #{hex}
+            </span>
+            <span className="color-box__variable">{variable}</span>
+          </li>
+        ))}
+      </ol>
+    </DocsBlock>
+
   </div>
 );
 
