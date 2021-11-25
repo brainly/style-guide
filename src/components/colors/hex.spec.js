094e673a71a424c26c8ed6c0db0abd766d7468f8
Dominika Podgórska <dpodgorskad@gmail.com>
2021-11-25 12:11:30 +0100

    Fix test

--- src/components/colors/hex.spec.js
+++ src/components/colors/hex.spec.js
@@ -3,5 +3,5 @@ import hex from './hex';
 
 test('should export colors as a hex values', () => {
   expect(hex.black).toBe(`#${colors.fullColorPalette[0].hex}`);
-  expect(hex['text-white']).toBe(`#${colors.text[0].hex}`);
+  expect(hex['text-black']).toBe(`#${colors.text[0].hex}`);
 });
