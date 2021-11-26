cd04d93029da3c1026d9ede012124002e3881dc0
Dominika Podgórska <dpodgorskad@gmail.com>
2021-11-26 15:07:34 +0100

    Bring back adaptive icon color

--- src/components/icon-as-button/IconAsButton.jsx
+++ src/components/icon-as-button/IconAsButton.jsx
@@ -65,7 +65,7 @@ const IconAsButton = ({
 
   if (type) {
     content = (
-      <Icon type={type} color={ICON_COLOR['icon-black']} size={ICON_SIZE[size]} />
+      <Icon type={type} size={ICON_SIZE[size]} color="adaptive" />
     );
   } else {
     content = children;
