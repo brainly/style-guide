abd8d5d5e217f8df17a8b63bd2004fd8f0741547
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2021-11-26 13:10:58 +0100

    test using external images for img src attribute

--- src/components/logo/Logo.chromatic.stories.jsx
+++ src/components/logo/Logo.chromatic.stories.jsx
@@ -1,6 +1,14 @@
+import * as React from 'react';
 import * as Logo from './Logo.stories.jsx';
 import {mergeStories} from '../../chromatic/utils';
 
-export const Default = mergeStories(Logo);
+const TestStory = () => (
+  <img
+    className="sg-logo__image"
+    src="https://styleguide.brainly.com/images/bug_box@2x-9b0cc7d970.png"
+  />
+);
+
+export const Default = mergeStories({...Logo, TestStory});
 
 export default Logo.default;
