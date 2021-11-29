1fd353feffc7df9255042dbb8d4d93b276909338
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2021-11-29 11:01:39 +0100

    Revert "test using external images for img src attribute"
    This reverts commit abd8d5d5e217f8df17a8b63bd2004fd8f0741547.

--- src/components/logo/Logo.chromatic.stories.jsx
+++ src/components/logo/Logo.chromatic.stories.jsx
@@ -1,14 +1,6 @@
-import * as React from 'react';
 import * as Logo from './Logo.stories.jsx';
 import {mergeStories} from '../../chromatic/utils';
 
-const TestStory = () => (
-  <img
-    className="sg-logo__image"
-    src="https://styleguide.brainly.com/images/bug_box@2x-9b0cc7d970.png"
-  />
-);
-
-export const Default = mergeStories({...Logo, TestStory});
+export const Default = mergeStories(Logo);
 
 export default Logo.default;
