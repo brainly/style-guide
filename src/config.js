bf426794140da3964a97b1d8d369c2b5d9350864
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2021-12-06 16:59:57 +0100

    fix invalid path to logos

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@
 export const LOGOS_BASE_URL =
   process.env.STORYBOOK_ENV === 'dev' ||
   process.env.STORYBOOK_ENV === 'chromatic'
-    ? '/images/logos/'
+    ? 'images/logos/'
     : 'https://styleguide.brainly.com/images/logos/';
