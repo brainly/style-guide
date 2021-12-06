e256a5435365bde41b8078ed170198e72e54dd7d
katarzynatobis <tobiskatarzyna@gmail.com>
2021-12-06 12:05:49 +0100

    remove stories

--- src/components/list/MenuList.chromatic.stories.jsx
+++
@@ -1,6 +0,0 @@
-import * as MenuList from './MenuList.stories.jsx';
-import {mergeStories} from '../../chromatic/utils';
-
-export const Default = mergeStories(MenuList);
-
-export default MenuList.default;
