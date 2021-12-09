87d5051260c8a86755a8da743ea82e190ad53946
katarzynatobis <tobiskatarzyna@gmail.com>
2021-12-09 15:59:28 +0100

    avatar a11y tests

---
+++ src/components/avatar/Avatar.a11y.spec.jsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import {render, screen} from '@testing-library/react';
+import Avatar from './Avatar';
+import {testA11y} from '../../axe';
+import userEvent from '@testing-library/user-event';
+
+describe('Avatar a11y', () => {
+  it('renders empty avatar ', async () => {
+    const alt = 'alt';
+    const avatar = render(<Avatar alt={alt} />);
+
+    await testA11y(avatar.container);
+    expect(avatar.queryByRole('img')).toBeFalsy();
+    expect(avatar.queryByAltText(alt)).toBeFalsy();
+  });
+
+  it('renders empty avatar with link and label', async () => {
+    const label = 'link label';
+    const avatar = render(<Avatar link="#" linkLabel={label} />);
+
+    await testA11y(avatar.container);
+    expect(avatar.getByRole('link', {name: label})).toBeTruthy();
+    expect(avatar.queryByRole('img')).toBeFalsy();
+  });
+
+  it('renders avatar with image that has alt text', async () => {
+    const imgAlt = 'image alt';
+    const avatar = render(<Avatar imgSrc="#" alt={imgAlt} />);
+
+    await testA11y(avatar.container);
+    expect(avatar.getByRole('img')).toBeTruthy();
+    expect(screen.getByAltText(imgAlt)).toBeTruthy();
+  });
+
+  it('is focusable if link is provided', async () => {
+    const label = 'link label';
+    const avatar = render(<Avatar link="#" imgSrc="#" linkLabel={label} />);
+
+    userEvent.click(avatar.getByLabelText(label));
+
+    expect(avatar.getByRole('link')).toEqual(document.activeElement);
+  });
+
+  it('is not focusable if link is not provided', async () => {
+    const label = 'link label';
+    const avatar = render(<Avatar imgSrc="#" alt={label} />);
+
+    userEvent.click(avatar.getByAltText(label));
+
+    expect(document.body).toEqual(document.activeElement);
+  });
+});
