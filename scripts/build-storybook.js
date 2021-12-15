83778c70409ee66829a8039f96b13ac47e284271
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2021-12-15 15:25:59 +0100

    some fixes

--- scripts/build-storybook.js
+++ scripts/build-storybook.js
@@ -16,7 +16,7 @@ client.s3.getObject(
   },
   function(err, data) {
     if (!data || (data && data.toString('utf-8') !== version)) {
-      execSync('yarn build-storybook');
+      execSync('yarn build-storybook --prod');
       fs.writeFileSync('.storybook-static/.sg-version', `${version}`, 'utf-8');
     } else {
       console.log(
--- scripts/tasks/fingerprint.js
+++ scripts/tasks/fingerprint.js
@@ -20,6 +20,7 @@ module.exports = function(gulp, plugins, consts) {
       .pipe(plugins.rev())
       .pipe(gulp.dest(consts.DIST)) // write rev'd assets to build dir
       .pipe(plugins.rev.manifest())
+      .pipe(gulp.dest(consts.DIST))
       .pipe(gulp.dest(consts.VERSIONED_DIST)); // write manifest to build dir
   };
 };
