c6741c224c3176ab62284712c9a23de9a14e5652
coderitual <skowronkow@gmail.com>
2021-12-16 00:19:52 +0100

    Build yaml containing deprecated components list

--- scripts/constants.js
+++ scripts/constants.js
@@ -7,9 +7,14 @@ const CSS_SRC = path.join(CSS_SRC_DIR, 'index.css');
 const CSS_OUTPUT_DIR = path.join(projectRoot, './css');
 const CSS_CONFIG = path.resolve(projectRoot, './src/css/config/index.js');
 
+const DEPRECATED_JSON_PATH = path.join(projectRoot, './deprecated.json');
+const DEPRECATED_YAML_PATH = path.join(projectRoot, './deprecated.yaml');
+
 module.exports = {
   CSS_SRC_DIR,
   CSS_SRC,
   CSS_OUTPUT_DIR,
   CSS_CONFIG,
+  DEPRECATED_JSON_PATH,
+  DEPRECATED_YAML_PATH,
 };
