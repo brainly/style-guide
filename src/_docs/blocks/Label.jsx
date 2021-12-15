20874c6b174a121416876694fe6c54c5c526e505
coderitual <skowronkow@gmail.com>
2021-12-15 14:10:30 +0100

    Update deprecated label style

--- src/_docs/blocks/Label.jsx
+++ src/_docs/blocks/Label.jsx
@@ -5,16 +5,16 @@ import {styled} from '@storybook/theming';
 const Label = styled.div(({theme}) => ({
   fontFamily: theme.typography.fonts.base,
   fontSize: theme.typography.size.s1,
-  fontWeight: '400',
-  lineHeight: '14px',
-  color: '#323c45',
-  backgroundColor: '#fff',
+  fontWeight: '600',
+  lineHeight: '16px',
+  color: '#a15c20',
+  backgroundColor: '#fff5cf',
   borderRadius: '8px',
-  padding: '0px 7px',
+  padding: '0px 12px',
   display: 'inline-block',
   marginLeft: '4px',
-  border: '1px solid #c3d1dd',
   textTransform: 'capitalize',
+  boxShadow: 'rgb(161 92 32 / 10%) 0px 0px 0px 1px',
 }));
 
 export {Label};
