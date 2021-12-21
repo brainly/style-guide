eec59d7a8faa4b38816fcfd9bef3666f99fe3dd9
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2021-12-21 10:48:54 +0100

    format babelrc

--- .babelrc.js
+++ .babelrc.js
@@ -1,18 +1,18 @@
-const babelEnv = params => [
+const babelEnv = (params) => [
   '@babel/preset-env',
   Object.assign(
     {
       targets: '> 0.2%, not dead, not ie < 11',
     },
-    params
+    params,
   ),
 ];
 
-module.exports = api => {
+module.exports = (api) => {
   api.cache(true);
   return {
     presets: [
-      babelEnv({modules: false}),
+      babelEnv({ modules: false }),
       '@babel/preset-react',
       '@babel/preset-flow',
     ],
@@ -28,15 +28,15 @@ module.exports = api => {
     ],
     env: {
       test: {
-        presets: [babelEnv({modules: 'auto'})],
+        presets: [babelEnv({ modules: 'auto' })],
         plugins: [['@babel/plugin-transform-runtime']],
       },
       commonjs: {
-        presets: [babelEnv({modules: 'auto'})],
+        presets: [babelEnv({ modules: 'auto' })],
       },
       esm: {
-        presets: [babelEnv({modules: false})],
-        plugins: [['@babel/plugin-transform-runtime', {useESModules: true}]],
+        presets: [babelEnv({ modules: false })],
+        plugins: [['@babel/plugin-transform-runtime', { useESModules: true }]],
       },
     },
   };
