b0be127d50fe7968bd24bcf4192139509e6b3150
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2022-02-03 16:37:45 +0100

    add buildspecs for styleguide (#2279)
    * add buildspecs for styleguide
    
    * deploy styleguide with terraform

--- scripts/build-styleguide-s3.js
+++ scripts/build-styleguide-s3.js
@@ -1,7 +1,6 @@
 const {version} = require('../package.json');
 const s3 = require('@brainly/s3');
 const {execSync} = require('child_process');
-const fs = require('fs');
 
 const client = s3.createClient({
   s3Options: {
@@ -9,22 +8,22 @@ const client = s3.createClient({
   },
 });
 
-client.s3.getObject(
+client.s3.headObject(
   {
-    Bucket: 'style-guide.brainly.com',
-    Key: `.sg-version`,
+    Bucket: 'styleguide.brainly.com',
+    Key: `new-deploy/${version}/style-guide.css`,
   },
   function (err, data) {
     if (err) {
       console.log(err, err.stack);
+      return;
     }
 
     if (!data || (data && data.toString('utf-8') !== version)) {
-      execSync('STORYBOOK_ENV=prod yarn build-storybook');
-      fs.writeFileSync('storybook-static/.sg-version', `${version}`, 'utf-8');
+      execSync('yarn build');
     } else {
       console.log(
-        'No version change detected in package.json, skipping storybook build.'
+        'No version change detected in package.json, skipping build.'
       );
     }
   }
