3c8828ad892b46392849d61d7097247141760dac
Dominika Podgórska <dominika.podgorska@brainly.com>
2022-02-04 14:19:37 +0100

    Update color maps. Apply colors change to all JSX elements from components arg

--- codemods/transforms/replace-colors-rebranding.js
+++ codemods/transforms/replace-colors-rebranding.js
@@ -9,7 +9,6 @@ const chalk = require('chalk');
 // or params
 //   new RegExp('<Headline([^>]|\n)*(color={)([^>]|\n)*[>]'),
 
-// Icons color maps:
 const iconsColorsMap = {
   dark: 'icon-black',
   light: 'icon-white',
@@ -25,7 +24,6 @@ const iconsColorsMap = {
   'gray-light': 'icon-gray-40',
 };
 
-// Text color maps:
 const textColorsMap = {
   dark: 'text-black',
   black: 'text-black',
@@ -57,39 +55,11 @@ const textColorsMap = {
   'gray-light': 'text-gray-40',
 };
 
-// Link color maps:
 const linkColorsMap = {
-  dark: 'text-black',
-  black: 'text-black',
-  light: 'text-white',
-  white: 'text-white',
-  blue: 'text-blue-60',
-  'blue-dark': 'text-blue-60',
-  'blue-primary': 'text-blue-60',
-  'blue-secondary': 'text-blue-40',
-  lavender: 'text-indigo-60',
-  'lavender-dark': 'text-indigo-60',
-  'lavender-primary': 'text-indigo-60',
-  'lavender-secondary': 'text-indigo-40',
-  mint: 'text-green-60',
-  'mint-dark': 'text-green-60',
-  'mint-primary': 'text-green-60',
-  'mint-secondary': 'text-green-40',
-  mustard: 'text-yellow-60',
-  'mustard-dark': 'text-yellow-60',
-  'mustard-primary': 'text-yellow-60',
-  'mustard-secondary': 'text-yellow-40',
-  peach: 'text-red-60',
-  'peach-dark': 'text-red-60',
-  'peach-primary': 'text-red-60',
-  'peach-secondary': 'text-red-40',
-  gray: 'text-gray-70',
-  'gray-secondary': 'text-gray-60',
+  ...textColorsMap,
   'gray-secondary-light': 'text-gray-50',
-  'gray-light': 'text-gray-40',
 };
 
-// Label color maps:
 const labelColorsMap = {
   mint: 'green',
   lavender: 'indigo',
@@ -97,7 +67,20 @@ const labelColorsMap = {
   mustard: 'yellow',
 };
 
-// Box color maps:
+const bubbleColorsMap = {
+  'navyblue-secondary': 'white',
+  dark: 'gray-40',
+  'gray-secondary-light': 'gray-20',
+  blue: 'blue-40',
+  'blue-secondary': 'blue-30',
+  'blue-secondary-light': 'blue-20',
+  lavender: 'indigo-40',
+  mint: 'green-40',
+  'mint-secondary': 'green-30',
+  'mint-secondary-light': 'green-20',
+  peach: 'red-40',
+};
+
 const boxColorsMap = {
   dark: 'gray-40',
   light: 'white',
@@ -124,58 +107,118 @@ const boxColorsMap = {
   transparent: 'transparent',
 };
 
-// Bubble color maps:
-const bubbleColorsMap = {
-  'navyblue-secondary': 'white',
-  dark: 'gray-40',
-  'gray-secondary-light': 'gray-20',
+const cardHoleColorsMap = {
   blue: 'blue-40',
   'blue-secondary': 'blue-30',
   'blue-secondary-light': 'blue-20',
   lavender: 'indigo-40',
+  'lavender-secondary': 'indigo-30',
+  'lavender-secondary-light': 'indigo-20',
   mint: 'green-40',
   'mint-secondary': 'green-30',
   'mint-secondary-light': 'green-20',
-  peach: 'red-40',
+  mustard: 'yellow-40',
+  'mustard-secondary': 'yellow-30',
+  'mustard-secondary-light': 'yellow-20',
+  gray: 'gray-70',
+  'gray-secondary': 'gray-50',
+  'gray-secondary-light': 'gray-40',
+  'gray-secondary-lightest': 'gray-20',
+  'gray-secondary-ultra-light': 'gray-10',
 };
 
-const colorsMap = boxColorsMap;
-const componentName = 'Box';
-const unmatchedFiles = [];
+const spinnerColorsMap = {
+  black: 'black',
+  white: 'white',
+  'gray-900': 'gray-70',
+  'gray-700': 'gray-50',
+  'peach-700': 'red-40',
+  'mustard-700': 'yellow-40',
+  'blue-700': 'blue-40',
+};
+
+const counterColorsMap = {
+  'peach-900': 'red-50',
+  'blue-900': 'blue-50',
+};
+
+const overlayColorsMap = {
+  dark: 'black',
+  blue: 'blue',
+};
+
+const fileHandlerColorsMap = {
+  'gray-secondary-light': 'gray-20',
+  white: 'white',
+};
+
+const componentsColorsMap = {
+  Icon: iconsColorsMap,
+  MobileIcon: iconsColorsMap,
+  SubjectIcon: iconsColorsMap,
+  MathSymbol: iconsColorsMap,
+  IconAsButton: iconsColorsMap,
+  Text: textColorsMap,
+  TextBit: textColorsMap,
+  Link: linkColorsMap,
+  Headline: textColorsMap,
+  Bubble: bubbleColorsMap,
+  Box: boxColorsMap,
+  Label: labelColorsMap,
+  CardHole: cardHoleColorsMap,
+  Spinner: spinnerColorsMap,
+  SpinnerContainer: spinnerColorsMap,
+  Counter: counterColorsMap,
+  Overlay: overlayColorsMap,
+  FileHandler: fileHandlerColorsMap,
+};
 
 module.exports = (file, api, options) => {
   const jsc = api.jscodeshift;
-
   const root = jsc(file.source);
 
+  const componentsArr = options.components.split(',');
+
   // Code transformations
-  // Replace old color values from <componentName component
+  // Replace old color values from <componentName components
   let wasMatchFound = false;
 
   root
+    // Search for JSX opening elements in a file
     .find(jsc.JSXOpeningElement)
-    .filter(path => path.value.name.name === componentName)
+    // Check if we should modify all components
+    // or if JSX element is on the list of components that we want to modify
+    .filter(
+      path =>
+        componentsArr.includes('all') ||
+        componentsArr.includes(path.value.name.name)
+    )
     .forEach(path => {
-      const color = path.value.attributes.find(
+      const colorAttr = path.value.attributes.find(
         attribute => attribute.name && attribute.name.name === 'color'
       );
 
-      if (color) {
-        // Check if this color was already changed
-        const isCorrectColor = Object.values(colorsMap).includes(
-          color.value.value
-        );
+      // If there is no "color" attribute, there is nothing to change
+      if (!colorAttr) return;
 
-        if (isCorrectColor) return;
+      const currentComponentName = path.value.name.name;
+      const colorsMap = componentsColorsMap[currentComponentName];
 
-        if (colorsMap[color.value.value]) {
-          wasMatchFound = true;
-          color.value.value = colorsMap[color.value.value];
-        } else {
-          // If there was 'color' attribute found, but it doesn't match any of strings it means there is a param or some logic
-          // unmatchedFiles.push(file.path);
-          console.log(chalk.cyan('unmatched:'), file.path);
-        }
+      // Check if this color was already changed to correct one
+      const isCorrectColor = Object.values(colorsMap).includes(
+        colorAttr.value.value
+      );
+
+      if (isCorrectColor) return;
+
+      // If this color needs to be changed
+      if (colorsMap[colorAttr.value.value]) {
+        wasMatchFound = true;
+        colorAttr.value.value = colorsMap[colorAttr.value.value];
+      } else {
+        // If there was 'color' attribute found, but it doesn't match any of strings it means there is a param or some logic
+        // unmatchedFiles.push(file.path);
+        console.log(chalk.cyan('unmatched:'), file.path);
       }
     });
 
