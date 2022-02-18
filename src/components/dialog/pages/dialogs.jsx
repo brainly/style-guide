72f3edadff9a47adc740ee1c175f50c90c0f06f2
Dominika Podgórska <dominika.podgorska@brainly.com>
2022-02-18 16:11:28 +0100

    Add Dialog documentation section

---
+++ src/components/dialog/pages/dialogs.jsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+import DocsBlock from 'components/DocsBlock';
+import Link from '../../text/Link';
+
+const Dialogs = () => {
+  return (
+    <div>
+      <DocsBlock info="Dialog">
+        Please go to{' '}
+        <Link href="interactive.html#dialogs">interactive docs</Link> or see
+        Dialog documentation in the{' '}
+        <Link
+          href="https://style-guide.brainly.com/?path=/story/components-dialog--default"
+          target="_blank"
+        >
+          storybook
+        </Link>
+      </DocsBlock>
+    </div>
+  );
+};
+
+export default Dialogs;
