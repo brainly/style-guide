5a020bf6af48c3d5c65998810b3e0e486a3a0796
Dominika Podgórska <dominika.podgorska@brainly.com>
2022-02-28 13:36:25 +0100

    Change file import in Logo chromatic stories

--- src/components/logo/Logo.chromatic.stories.jsx
+++ src/components/logo/Logo.chromatic.stories.jsx
@@ -1,4 +1,4 @@
-import * as Logo from './Logo.stories.jsx';
+import * as Logo from './Logo.stories.mdx';
 import {mergeStories} from '../../chromatic/utils';
 
 export const Default = mergeStories(Logo);
