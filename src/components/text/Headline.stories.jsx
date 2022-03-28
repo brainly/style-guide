0e74de7c702edd8ec540ae4377a50e2d6772cf7a
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2022-03-28 11:58:29 +0200

    improve responsive props knobs for Headline

--- src/components/text/Headline.stories.jsx
+++ src/components/text/Headline.stories.jsx
@@ -1,12 +1,76 @@
 import * as React from 'react';
 import Headline from './Headline';
 import {TEXT_COLOR} from './textConsts';
+import {
+  HEADLINE_ALIGN,
+  HEADLINE_SIZE,
+  HEADLINE_TRANSFORM,
+} from './headlineConsts';
 
 export default {
   title: 'Components/Headline',
   component: Headline,
   argTypes: {
     children: 'string',
+    size: {
+      description: '(Responsive)',
+      table: {
+        type: {
+          summary: Object.values(HEADLINE_SIZE).join('|'),
+        },
+      },
+      control: {
+        type: 'select',
+        options: Object.values(HEADLINE_SIZE),
+      },
+    },
+    color: {
+      description: '(Responsive)',
+      table: {
+        type: {
+          summary: Object.values(TEXT_COLOR).join('|'),
+        },
+      },
+      control: {
+        type: 'select',
+        options: Object.values(TEXT_COLOR),
+      },
+    },
+    transform: {
+      description: '(Responsive)',
+      table: {
+        type: {
+          summary: Object.values(HEADLINE_TRANSFORM).join('|'),
+        },
+      },
+      control: {
+        type: 'select',
+        options: Object.values(HEADLINE_TRANSFORM),
+      },
+    },
+    align: {
+      description: '(Responsive)',
+      table: {
+        type: {
+          summary: Object.values(HEADLINE_ALIGN).join('|'),
+        },
+      },
+      control: {
+        type: 'select',
+        options: Object.values(HEADLINE_ALIGN),
+      },
+    },
+    extraBold: {
+      description: '(Responsive)',
+      table: {
+        type: {
+          summary: 'boolean',
+        },
+      },
+      control: {
+        type: 'boolean',
+      },
+    },
   },
   args: {
     children: 'some text',
