7a7c39825e256a4331badff66bd15de728eef1a6
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2022-04-11 16:06:30 +0200

    Add responsive props to Box (#2372)
    * make Box shadow prop to responsive
    
    * make Box noBorderRadius prop responsive
    
    * make Box border prop responsive
    
    * add responsive props to Box
    
    * change noBorderRadius prop to ResponsivePropType
    
    * refactor mergeResponsiveProps and generateResponsiveClassNames
    
    * more consistent look of Box stories
    
    * remove console log
    
    * add 'none' option for padding
    
    * remove test story

--- src/components/utils/responsive-props.js
+++ src/components/utils/responsive-props.js
@@ -1,5 +1,22 @@
 // @flow strict
 
+export type ResponsivePropObjectType = {
+  sm?: mixed,
+  md?: mixed,
+  lg?: mixed,
+  xl?: mixed,
+};
+
+export type ResponsivePropConjuctionType =
+  | mixed
+  | Array<?mixed>
+  | {
+      sm?: mixed,
+      md?: mixed,
+      lg?: mixed,
+      xl?: mixed,
+    };
+
 export type ResponsivePropType<T> =
   | T
   | Array<?T>
@@ -10,11 +27,83 @@ export type ResponsivePropType<T> =
       xl?: T,
     };
 
+const breakpoints = ['sm', 'md', 'lg', 'xl'];
+const responsivePrefixes = ['', 'md', 'lg', 'xl'];
+
+export function mergeResponsiveProps(
+  props: Array<ResponsivePropConjuctionType>
+) {
+  // convert to objects
+  const propObjects = props.map(prop => {
+    if (prop === null || prop === undefined) {
+      return {};
+    } else if (typeof prop !== 'object') {
+      return {
+        sm: prop,
+        md: prop,
+        lg: prop,
+        xl: prop,
+      };
+    } else if (Array.isArray(prop)) {
+      return (prop.length > 4 ? prop.slice(0, 4) : prop).reduce(
+        (acc, next, index) => {
+          if (next !== null && next !== undefined) {
+            acc[breakpoints[index]] = next;
+          }
+          return acc;
+        },
+        {}
+      );
+    } else {
+      return {...prop};
+    }
+  });
+
+  // fill empty breakpoints when other props have values
+  let lastRowValues = [];
+
+  breakpoints.forEach(breakpoint => {
+    const valueBreakpointExist = propObjects.some(
+      propObject =>
+        propObject[breakpoint] !== null && propObject[breakpoint] !== undefined
+    );
+
+    if (valueBreakpointExist) {
+      // eslint-disable-next-line no-loop-func
+      propObjects.forEach((propObject, propObjectsIndex) => {
+        if (lastRowValues[propObjectsIndex] && !propObject[breakpoint]) {
+          propObject[breakpoint] = lastRowValues[propObjectsIndex];
+        }
+      });
+    }
+
+    lastRowValues = propObjects.map(propObject => propObject[breakpoint]);
+  });
+
+  breakpoints.forEach;
+
+  // merge
+  return breakpoints.reduce((acc, breakpoint) => {
+    if (
+      propObjects.every(
+        propObject =>
+          propObject[breakpoint] !== null &&
+          propObject[breakpoint] !== undefined
+      )
+    ) {
+      acc[breakpoint] = propObjects.map<mixed>(
+        propObject => propObject[breakpoint]
+      );
+    }
+    return acc;
+  }, {});
+}
+
 export function generateResponsiveClassNames<T>(
   createBaseClassName: T => string,
   prop?: ResponsivePropType<T>
 ): Array<string> {
-  if (!prop) {
+  if (prop === null || prop === undefined) {
     return [];
   }
 
@@ -23,16 +112,16 @@ export function generateResponsiveClassNames<T>(
   }
 
   if (Array.isArray(prop)) {
-    const breakpoints = ['', 'md', 'lg', 'xl'];
-
     return (prop.length > 4 ? prop.slice(0, 4) : prop).reduce(
       (acc, propBreakpointValue, index) => {
         if (propBreakpointValue === null || propBreakpointValue === undefined) {
           return acc;
+        } else if (!createBaseClassName(propBreakpointValue)) {
+          return acc;
         } else {
           acc.push(
             `${
-              breakpoints[index] ? `${breakpoints[index]}:` : ''
+              responsivePrefixes[index] ? `${responsivePrefixes[index]}:` : ''
             }${createBaseClassName(propBreakpointValue)}`
           );
           return acc;
@@ -42,9 +131,11 @@ export function generateResponsiveClassNames<T>(
     );
   }
 
-  return ['sm', 'md', 'lg', 'xl']
+  return breakpoints
     .map(breakpoint => {
       if (prop[breakpoint] === null || prop[breakpoint] === undefined) {
+        return '';
+      } else if (!createBaseClassName(prop[breakpoint])) {
         return '';
       } else {
         return breakpoint === 'sm'
