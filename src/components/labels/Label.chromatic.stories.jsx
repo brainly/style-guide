bd303f4aee4176237d8f7ad32d6d32d9d9bde417
Dominika Podgórska <dominika.podgorska@brainly.com>
2022-04-14 15:20:42 +0200

    Label to MDX (#2391)
    * Label to MDX
    
    * Fix story name
    
    * Remove redundant arg

--- src/components/labels/Label.chromatic.stories.jsx
+++ src/components/labels/Label.chromatic.stories.jsx
@@ -1,6 +1,8 @@
-import * as Label from './Label.stories.jsx';
+import * as Label from './Label.stories.mdx';
 import {mergeStories} from '../../chromatic/utils';
 
 export const Default = mergeStories(Label);
 
-export default Label.default;
+const {includeStories, ...meta} = Label.default;
+
+export default meta;
