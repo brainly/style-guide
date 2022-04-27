3377dc24a5a33f58f94cca0308370a77e75983ec
Dominika Podgórska <dpodgorskad@gmail.com>
2022-04-27 12:40:31 +0200

    Add check icon, style focus and checked states

--- src/components/checkbox/Checkbox.jsx
+++ src/components/checkbox/Checkbox.jsx
@@ -6,6 +6,17 @@ import cx from 'classnames';
 import generateRandomString from '../../js/generateRandomString';
 import Text from '../text/Text';
 
+const CheckIcon = () => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M7.74651 0.405156L3.87975 4.27195L2.27694 2.66884C2.04115 2.43305 1.69748 2.34097 1.37539 2.42727C1.0533 2.51358 0.801722 2.76516 0.715418 3.08725C0.629114 3.40934 0.721199 3.75301 0.956985 3.9888L3.21953 6.25136C3.58504 6.61484 4.17423 6.61484 4.53895 6.25214L9.06533 1.726C9.42862 1.36044 9.42862 0.771583 9.06621 0.406913C8.7002 0.0427156 8.11121 0.0427156 7.74651 0.405156Z" />
+  </svg>
+);
+
 export type CheckboxPropsType = {
   checked?: boolean,
   children?: React.Node,
@@ -57,11 +68,12 @@ const Checkbox = ({
         />
         <span
           className="sg-checkbox-new__icon"
-          checked={checked}
           // This element is purely decorative so
           // we hide it for screen readers
           aria-hidden="true"
-        />
+        >
+          <CheckIcon />
+        </span>
         {children && (
           <Text
             size="medium"
