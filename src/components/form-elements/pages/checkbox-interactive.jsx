d04a436472267f6ceb177d3a542c875a1856ce1d
Dominika Podgórska <dpodgorskad@gmail.com>
2022-05-10 14:08:16 +0200

    Add Checkbox docs to the styleguide

--- src/components/form-elements/pages/checkbox-interactive.jsx
+++ src/components/form-elements/pages/checkbox-interactive.jsx
@@ -5,15 +5,43 @@ import DocsActiveBlock from 'components/DocsActiveBlock';
 const Checkboxes = () => {
   const settings = [
     {
+      name: 'color',
+      values: {dark: 'dark', light: 'light'},
+    },
+    {
       name: 'checked',
       values: Boolean,
+    },
+    {
+      name: 'children',
+      values: String,
+    },
+    {
+      name: 'description',
+      values: String,
+    },
+    {
+      name: 'indeterminate',
+      values: Boolean,
+    },
+    {
+      name: 'disabled',
+      values: Boolean,
+    },
+    {
+      name: 'invalid',
+      values: Boolean,
+    },
+    {
+      name: 'errorMessage',
+      values: String,
     },
   ];
 
   return (
     <div>
       <DocsActiveBlock settings={settings}>
-        <Checkbox>Checkbox</Checkbox>
+        <Checkbox>Checkbox label</Checkbox>
       </DocsActiveBlock>
     </div>
   );
