57bee9fb3cfc10f0950eb6ef943e1ceea502b937
Dominika Podgórska <dpodgorskad@gmail.com>
2022-05-12 15:07:01 +0200

    Add ids to label and input, render label only when children was passed

--- src/components/radio/Radio.jsx
+++ src/components/radio/Radio.jsx
@@ -3,6 +3,7 @@
 import * as React from 'react';
 import classNames from 'classnames';
 import Text from '../text/Text';
+import generateRandomString from '../../js/generateRandomString';
 
 type RadioColorType = 'light' | 'dark';
 
@@ -10,6 +11,8 @@ export type RadioPropsType = {
   children?: React.Node,
   className?: ?string,
   color?: ?RadioColorType,
+  id?: string,
+  customLabelId?: string,
   ...
 };
 
@@ -17,24 +20,38 @@ const Radio = ({
   color = 'dark',
   children,
   className,
+  id,
+  customLabelId,
   ...props
 }: RadioPropsType) => {
+  const {current: radioId} = React.useRef(
+    id === undefined || id === '' ? generateRandomString() : id
+  );
+
   const radioClass = classNames('sg-radio-new', className);
+  const labelId = customLabelId || `${radioId}-label`;
 
   return (
-    <label {...props} className={radioClass}>
-      <input className="sg-radio-new__element" type="radio" />
+    <div>
+      <input
+        className="sg-radio-new__element"
+        type="radio"
+        id={radioId}
+        aria-labelledby={labelId}
+      />
       {children !== undefined && children !== null && (
-        <Text
-          size="small"
-          type="span"
-          weight="bold"
-          className="sg-radio-new__label"
-        >
-          {children}
-        </Text>
+        <label {...props} id={labelId} className={radioClass} htmlFor={radioId}>
+          <Text
+            size="small"
+            type="span"
+            weight="bold"
+            className="sg-radio-new__label"
+          >
+            {children}
+          </Text>
+        </label>
       )}
-    </label>
+    </div>
   );
 };
 
