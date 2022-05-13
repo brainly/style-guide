d3e60e8491fef11b14eff70b843487b824e647b6
Tomasz Trzaskoma <tomasz.trzaskoma@brainly.com>
2022-05-13 19:04:31 +0200

    Simplify Skip Link

---
+++ src/components/skip-link/SkipLink.a11y.spec.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import SkipLink from './SkipLink';
+import {testA11y} from '../../axe';
+
+const renderSkipLink = () =>
+  render(<SkipLink id="main">skip to main content</SkipLink>);
+
+describe('<SkipLink /> a11y', () => {
+  it('should have no a11y violations when link, label, imgSrc and alt are provided', async () => {
+    await testA11y(<SkipLink id="main">skip to main content</SkipLink>);
+  });
+
+  it('should be focusable', () => {
+    renderSkipLink();
+
+    const link = screen.getByRole('link', {name: /skip to main content/i});
+
+    expect(link).not.toHaveFocus();
+    link.focus();
+    expect(link).toHaveFocus();
+  });
+});
---
+++ src/components/skip-link/SkipLink.spec.jsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+// import userEvent from '@testing-library/user-event';
+
+import SkipLink from './SkipLink';
+
+beforeAll(() => {
+  Object.defineProperty(window, 'location', {
+    value: {
+      href: '/',
+      replace: () => undefined,
+    },
+    writable: true,
+  });
+});
+
+const linkProps = {
+  name: 'skip to main content',
+  href: '#main',
+};
+
+const renderSkipLink = () =>
+  render(<SkipLink id="main">skip to main content</SkipLink>);
+
+describe('<SkipLink />', () => {
+  it('renders single SkipLink', () => {
+    renderSkipLink();
+
+    expect(
+      screen.getByRole('link', {name: linkProps.name})
+    ).toBeInTheDocument();
+  });
+
+  it('creates link with proper href attribute', async () => {
+    renderSkipLink();
+    const link = screen.getByRole('link', {name: linkProps.name});
+
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute('href', linkProps.href);
+    // userEvent.keyboard('{enter}');
+    // expect(window.location.href).toEqual('#main');
+  });
+});
