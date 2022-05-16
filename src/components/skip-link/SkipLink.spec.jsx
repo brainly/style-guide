c2ea9344cab8a1df8e1e6997739146bc493094bf
Tomasz Trzaskoma <tomasz.trzaskoma@brainly.com>
2022-05-16 12:14:34 +0200

    enhance tests

--- src/components/skip-link/SkipLink.spec.jsx
+++ src/components/skip-link/SkipLink.spec.jsx
@@ -1,19 +1,8 @@
 import * as React from 'react';
 import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
-// import userEvent from '@testing-library/user-event';
 
 import SkipLink from './SkipLink';
-
-beforeAll(() => {
-  Object.defineProperty(window, 'location', {
-    value: {
-      href: '/',
-      replace: () => undefined,
-    },
-    writable: true,
-  });
-});
 
 const linkProps = {
   name: 'skip to main content',
@@ -38,7 +27,5 @@ describe('<SkipLink />', () => {
 
     expect(link).toBeInTheDocument();
     expect(link).toHaveAttribute('href', `#${linkProps.id}`);
-    // userEvent.keyboard('{enter}');
-    // expect(window.location.href).toEqual('#main');
   });
 });
