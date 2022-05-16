637ccec1d727e409b09b260961d994e21fc2467b
Dominika Podgórska <dpodgorskad@gmail.com>
2022-05-16 16:03:33 +0200

    Extend API

--- src/components/radio/Radio.jsx
+++ src/components/radio/Radio.jsx
@@ -8,20 +8,34 @@ import generateRandomString from '../../js/generateRandomString';
 type RadioColorType = 'light' | 'dark';
 
 export type RadioPropsType = {
+  checked?: boolean,
   children?: React.Node,
   className?: ?string,
   color?: ?RadioColorType,
+  disabled?: boolean,
   id?: string,
+  invalid?: boolean,
   customLabelId?: string,
+  name?: string,
+  onChange: (SyntheticInputEvent<HTMLInputElement>) => void,
+  required?: boolean,
+  value?: string,
   ...
 };
 
 const Radio = ({
+  checked = false,
   color = 'dark',
   children,
   className,
+  disabled = false,
   id,
+  invalid = false,
   customLabelId,
+  name,
+  onChange,
+  required = false,
+  value,
   ...props
 }: RadioPropsType) => {
   const {current: radioId} = React.useRef(
@@ -32,16 +46,23 @@ const Radio = ({
   const labelId = customLabelId || `${radioId}-label`;
 
   return (
-    <div>
+    <div className={radioClass}>
       <input
         className="sg-radio-new__element"
         type="radio"
         id={radioId}
+        checked={checked}
+        disabled={disabled}
+        name={name}
+        onChange={onChange}
+        required={required}
+        value={value}
         aria-labelledby={labelId}
+        aria-invalid={invalid ? true : undefined}
         {...props}
       />
       {children !== undefined && children !== null && (
-        <label id={labelId} className={radioClass} htmlFor={radioId}>
+        <label id={labelId} htmlFor={radioId}>
           <Text
             size="small"
             type="span"
