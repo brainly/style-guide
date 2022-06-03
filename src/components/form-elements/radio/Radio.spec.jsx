8ba988056708821c074734e858181eab4fc4e546
Dominika Podgórska <dpodgorskad@gmail.com>
2022-06-03 09:53:07 +0200

    Add Radio tests

---
+++ src/components/form-elements/radio/Radio.spec.jsx
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import Radio from './Radio';
+
+import {render} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('<Radio />', () => {
+  const renderRadio = props =>
+    render(<Radio onChange={() => jest.fn()} {...props} />);
+
+  it('it renders unchecked radio input, without label', () => {
+    const radio = renderRadio({});
+    const radioInput = radio.getByRole('radio');
+
+    expect(radioInput.checked).toBe(false);
+    expect(radio.queryByLabelText('my label')).toBeNull();
+  });
+
+  it('checks when either input or label is clicked', () => {
+    const radio = renderRadio({children: 'my label'});
+    const radioInput = radio.getByRole('radio');
+
+    expect(radioInput.checked).toBe(false);
+    userEvent.click(radio.queryByLabelText('my label'));
+    expect(radioInput).toEqual(document.activeElement);
+    expect(radioInput.checked).toBe(true);
+  });
+
+  it('checks when either input is clicked', () => {
+    const radio = renderRadio({children: 'my label'});
+    const radioInput = radio.getByRole('radio');
+
+    expect(radioInput.checked).toBe(false);
+    userEvent.click(radioInput);
+    expect(radioInput.checked).toBe(true);
+  });
+
+  it('it renders as initially checked', () => {
+    const radio = renderRadio({checked: true});
+    const radioInput = radio.getByRole('radio');
+
+    expect(radioInput.checked).toBe(true);
+  });
+
+  it('it displays description', () => {
+    const descriptionText = 'Cool radio it is';
+
+    const radio = renderRadio({
+      description: descriptionText,
+      invalid: true,
+      children: 'Click me pls',
+    });
+
+    expect(radio.getByText(descriptionText)).toBeTruthy();
+  });
+
+  it('it displays description', () => {
+    const descriptionText = 'Cool radio it is';
+
+    const radio = renderRadio({
+      id: 'myid',
+      description: descriptionText,
+      invalid: true,
+      children: 'Click me pls',
+    });
+    const radioInput = radio.getByRole('radio');
+
+    expect(radioInput.id).toEqual('myid');
+    expect(radio.getByText(descriptionText)).toBeTruthy();
+    expect(radio.getByText(descriptionText).id).toEqual('myid-description');
+  });
+
+  it("it doesn't allow checking disabled radio", () => {
+    const labelText = 'Click me pls';
+    const onChange = jest.fn();
+    const radio = renderRadio({
+      children: labelText,
+      checked: false,
+      disabled: true,
+      onChange,
+    });
+    const radioInput = radio.getByRole('radio');
+
+    expect(radioInput.checked).toBe(false);
+    userEvent.click(radioInput);
+    expect(onChange).not.toHaveBeenCalled();
+    expect(radioInput.checked).toBe(false);
+    userEvent.click(radio.queryByLabelText(labelText));
+    expect(onChange).not.toHaveBeenCalled();
+    expect(radioInput.checked).toBe(false);
+  });
+});
