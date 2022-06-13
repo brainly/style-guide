a2a003210f3c5609e48c3d581f24c8e6afdf0bef
katarzynatobis <60467496+katarzynatobis@users.noreply.github.com>
2022-06-13 16:18:39 +0200

    remove footer (#2426)

--- src/components/footer/Footer.chromatic.stories.jsx
+++
@@ -1,7 +0,0 @@
-import * as Footer from './Footer.stories.mdx';
-import {mergeStories} from '../../chromatic/utils';
-
-export const Default = mergeStories(Footer);
-const {includeStories, ...meta} = Footer.default;
-
-export default meta;
