33f8d0c708e4d7c3316336bc94149a8a2d1743e3
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2022-06-24 11:50:32 +0200

    add WithTooltip chromatic story

--- src/components/accordion/Accordion.chromatic.stories.jsx
+++ src/components/accordion/Accordion.chromatic.stories.jsx
@@ -1,8 +1,38 @@
-import * as Accordion from './Accordion.stories.mdx';
+import * as React from 'react';
+import * as AccordionStories from './Accordion.stories.mdx';
 import {mergeStories} from '../../chromatic/utils';
+import AccordionItem from './AccordionItem';
+import Accordion from './Accordion';
 
-export const Default = mergeStories(Accordion);
+const copy = {
+  title: 'Is this title for accordion?',
+  description:
+    // eslint-disable-next-line max-len
+    "Now your family member just needs to open the link and hit 'accept'. If they aren't already on Brainly, we'll hook them up with that too. Create your unique family link with a click and share it with your family via email, Messenger, WhatsApp, whatever you like.",
+};
 
-const {includeStories, ...meta} = Accordion.default;
+const WithTooltip = args => {
+  const handleChange = () => null;
+  const {expanded: propsExpanded, ...props} = args;
+
+  return (
+    <Accordion onChange={handleChange} expanded={args.expanded} {...props}>
+      <AccordionItem title={copy.title} key="1" id="accordion_item_1">
+        {copy.description}
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+WithTooltip.args = {
+  expanded: 'accordion_item_1',
+};
+
+export const Default = mergeStories({
+  ...AccordionStories,
+  WithTooltip,
+});
+
+const {includeStories, ...meta} = AccordionStories.default;
 
 export default meta;
