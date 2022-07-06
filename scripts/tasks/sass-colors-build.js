2ebb515bd8e35425996fb8af12168d45e82dc585
Dominika Podgórska <dpodgorskad@gmail.com>
2022-07-06 15:50:17 +0200

    Move colors declaration to a separate file, add build step for generating scss and css color variables

---
+++ scripts/tasks/sass-colors-build.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const fs = require('fs-extra');
+const colorMap = require('../../src/components/colors/colors');
+
+module.exports = function (gulp, plugins, consts) {
+  return function (done) {
+    const colorsFileDest = path.resolve(consts.SRC, 'sass', '_colors.scss');
+
+    const transformToScssMap = function (map) {
+      const colors = {};
+
+      /* $black: #000000; */
+      Object.keys(map).forEach(groupName => {
+        map[groupName].forEach(color => {
+          colors[
+            `$${color.name.toLocaleLowerCase().replace(/ /g, '-')}`
+          ] = `#${color.hex}`;
+        });
+      });
+
+      return colors;
+    };
+
+    const transformToCssMap = function (map) {
+      const colors = {};
+
+      /* --black: #{$black}; */
+      Object.keys(map).forEach(color => {
+        colors[`--${color.substring(1)}`] = `#{${color}}`;
+      });
+
+      return colors;
+    };
+
+    const mapColors = function (map) {
+      const colorsStringsMap = [];
+
+      Object.keys(map).forEach(color => {
+        colorsStringsMap.push(`${color}: ${map[color]};`);
+      });
+      return colorsStringsMap;
+    };
+
+    const scssVariables = transformToScssMap(colorMap);
+    const cssVariables = transformToCssMap(scssVariables);
+    const colorsStringsMap = [];
+
+    colorsStringsMap.push(...mapColors(scssVariables));
+    colorsStringsMap.push(':root {');
+    colorsStringsMap.push(...mapColors(cssVariables));
+    colorsStringsMap.push('}');
+
+    const fileContent = colorsStringsMap.join('\n');
+    const buffered = Buffer.from(fileContent);
+
+    fs.writeFile(colorsFileDest, buffered);
+
+    done();
+  };
+};
