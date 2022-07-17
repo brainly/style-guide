a7471eca110448713564bfb3c21e09f72abcd161
lewandowskid <53941885+lewandowskid@users.noreply.github.com>
2022-07-17 18:42:33 +0200

    sort all icons alphabetically

--- src/components/icons/Icon.jsx
+++ src/components/icons/Icon.jsx
@@ -6,54 +6,73 @@ import {generateId} from '../utils';
 
 export type IconTypeType =
   | 'academic_cap'
+  | 'add_more'
   | 'all_questions'
   | 'answer'
   | 'answers'
-  | 'chevron_double_down'
-  | 'chevron_down'
-  | 'chevron_left'
-  | 'chevron_right'
-  | 'chevron_up'
+  | 'apple'
+  | 'arrow_double_right'
+  | 'arrow_top_right'
   | 'ask_bubble'
   | 'ask_parent_to_pay'
   | 'attachment'
   | 'bell_checked'
   | 'bell_outlined'
   | 'bold'
-  | 'bulb'
   | 'bulb_checked'
+  | 'bulb'
   | 'bulleted_list'
   | 'calendar'
   | 'camera'
+  | 'caret_down'
+  | 'caret_up'
   | 'chapter'
   | 'check'
+  | 'chevron_double_down'
+  | 'chevron_double_right'
+  | 'chevron_down'
+  | 'chevron_left'
+  | 'chevron_right'
+  | 'chevron_up'
+  | 'circle'
+  | 'clear'
   | 'clipboard'
   | 'close'
-  | 'comment'
+  | 'collapse'
   | 'comment_outlined'
+  | 'comment'
   | 'counter'
   | 'credit_card'
-  | 'crown'
+  | 'crop'
   | 'crown_outlined'
+  | 'crown'
   | 'cup'
+  | 'cursor_select'
+  | 'cyrillic'
+  | 'dot'
+  | 'draw'
+  | 'drawing_mode'
   | 'envelope'
   | 'equation'
+  | 'eraser'
+  | 'european'
   | 'exclamation_mark'
   | 'facebook'
-  | 'google'
-  | 'apple'
   | 'filters'
   | 'friend_add'
-  | 'friend_remove'
-  | 'friend_pending'
   | 'friend_checked'
+  | 'friend_pending'
+  | 'friend_remove'
   | 'friends'
   | 'fullscreen'
   | 'funnel'
   | 'globe'
+  | 'google'
+  | 'greek'
   | 'heading'
-  | 'heart'
   | 'heart_outlined'
+  | 'heart'
+  | 'highlight'
   | 'image'
   | 'influence'
   | 'info'
@@ -61,6 +80,7 @@ export type IconTypeType =
   | 'italic'
   | 'keyboard'
   | 'less'
+  | 'line'
   | 'linkedin'
   | 'lock_with_play'
   | 'logout'
@@ -70,25 +90,31 @@ export type IconTypeType =
   | 'messages'
   | 'mic'
   | 'money_transfer'
-  | 'add_more'
+  | 'more'
+  | 'multiselect_checked'
+  | 'multiselect_unchecked'
   | 'notifications'
   | 'numbered_list'
   | 'open_in_new_tab'
   | 'padlock'
+  | 'pause'
   | 'pencil'
+  | 'pi'
   | 'play'
   | 'plus'
   | 'points'
-  | 'profile'
-  | 'profile_view'
   | 'profile_settings'
+  | 'profile_view'
+  | 'profile'
   | 'question'
+  | 'quote'
   | 'recent_questions'
+  | 'rectangle'
   | 'reload'
-  | 'report_flag'
   | 'report_flag_outlined'
-  | 'rotate'
+  | 'report_flag'
   | 'rotate_90'
+  | 'rotate'
   | 'search'
   | 'seen'
   | 'send'
@@ -96,55 +122,29 @@ export type IconTypeType =
   | 'share'
   | 'shield'
   | 'sms'
-  | 'star'
-  | 'star_half'
+  | 'spark'
   | 'star_half_outlined'
+  | 'star_half'
   | 'star_outlined'
+  | 'star'
   | 'subtitle'
+  | 'sup_sub'
   | 'symbols'
   | 'textbook'
-  | 'thumb_down'
   | 'thumb_down_outlined'
-  | 'thumb_up'
+  | 'thumb_down'
   | 'thumb_up_outlined'
+  | 'thumb_up'
   | 'title'
   | 'toughest_questions'
   | 'trash'
+  | 'triangle'
   | 'twitter'
   | 'underlined'
   | 'unseen'
   | 'verified'
   | 'warning'
-  | 'youtube'
-  | 'arrow_top_right'
-  | 'circle'
-  | 'crop'
-  | 'cyrillic'
-  | 'draw'
-  | 'drawing_mode'
-  | 'european'
-  | 'greek'
-  | 'highlight'
-  | 'line'
-  | 'more'
-  | 'pause'
-  | 'rectangle'
-  | 'sup_sub'
-  | 'triangle'
-  | 'pi'
-  | 'quote'
-  | 'spark'
-  | 'dot'
-  | 'clear'
-  | 'collapse'
-  | 'cursor_select'
-  | 'eraser'
-  | 'multiselect_checked'
-  | 'multiselect_unchecked'
-  | 'caret_down'
-  | 'caret_up'
-  | 'arrow_double_right'
-  | 'chevron_double_right';
+  | 'youtube';
 
 export type IconColorType =
   | 'adaptive'
@@ -165,54 +165,73 @@ export type IconSizeType = 16 | 24 | 32 | 40 | 56 | 80 | 104;
 
 export const TYPE: {
   ACADEMIC_CAP: 'academic_cap',
+  ADD_MORE: 'add_more',
   ALL_QUESTIONS: 'all_questions',
   ANSWER: 'answer',
   ANSWERS: 'answers',
-  CHEVRON_DOUBLE_DOWN: 'chevron_double_down',
-  CHEVRON_DOWN: 'chevron_down',
-  CHEVRON_LEFT: 'chevron_left',
-  CHEVRON_RIGHT: 'chevron_right',
-  CHEVRON_UP: 'chevron_up',
+  APPLE: 'apple',
+  ARROW_DOUBLE_RIGHT: 'arrow_double_right',
+  ARROW_TOP_RIGHT: 'arrow_top_right',
   ASK_BUBBLE: 'ask_bubble',
   ASK_PARENT_TO_PAY: 'ask_parent_to_pay',
   ATTACHMENT: 'attachment',
   BELL_CHECKED: 'bell_checked',
   BELL_OUTLINED: 'bell_outlined',
   BOLD: 'bold',
-  BULB: 'bulb',
   BULB_CHECKED: 'bulb_checked',
+  BULB: 'bulb',
   BULLETED_LIST: 'bulleted_list',
   CALENDAR: 'calendar',
   CAMERA: 'camera',
+  CARET_DOWN: 'caret_down',
+  CARET_UP: 'caret_up',
   CHAPTER: 'chapter',
   CHECK: 'check',
+  CHEVRON_DOUBLE_DOWN: 'chevron_double_down',
+  CHEVRON_DOUBLE_RIGHT: 'chevron_double_right',
+  CHEVRON_DOWN: 'chevron_down',
+  CHEVRON_LEFT: 'chevron_left',
+  CHEVRON_RIGHT: 'chevron_right',
+  CHEVRON_UP: 'chevron_up',
+  CIRCLE: 'circle',
+  CLEAR: 'clear',
   CLIPBOARD: 'clipboard',
   CLOSE: 'close',
-  COMMENT: 'comment',
+  COLLAPSE: 'collapse',
   COMMENT_OUTLINED: 'comment_outlined',
+  COMMENT: 'comment',
   COUNTER: 'counter',
   CREDIT_CARD: 'credit_card',
-  CROWN: 'crown',
+  CROP: 'crop',
   CROWN_OUTLINED: 'crown_outlined',
+  CROWN: 'crown',
   CUP: 'cup',
+  CURSOR_SELECT: 'cursor_select',
+  CYRILLIC: 'cyrillic',
+  DOT: 'dot',
+  DRAW: 'draw',
+  DRAWING_MODE: 'drawing_mode',
   ENVELOPE: 'envelope',
   EQUATION: 'equation',
+  ERASER: 'eraser',
+  EUROPEAN: 'european',
   EXCLAMATION_MARK: 'exclamation_mark',
   FACEBOOK: 'facebook',
-  GOOGLE: 'google',
-  APPLE: 'apple',
   FILTERS: 'filters',
   FRIEND_ADD: 'friend_add',
-  FRIEND_REMOVE: 'friend_remove',
-  FRIEND_PENDING: 'friend_pending',
   FRIEND_CHECKED: 'friend_checked',
+  FRIEND_PENDING: 'friend_pending',
+  FRIEND_REMOVE: 'friend_remove',
   FRIENDS: 'friends',
   FULLSCREEN: 'fullscreen',
   FUNNEL: 'funnel',
   GLOBE: 'globe',
+  GOOGLE: 'google',
+  GREEK: 'greek',
   HEADING: 'heading',
-  HEART: 'heart',
   HEART_OUTLINED: 'heart_outlined',
+  HEART: 'heart',
+  HIGHLIGHT: 'highlight',
   IMAGE: 'image',
   INFLUENCE: 'influence',
   INFO: 'info',
@@ -220,6 +239,7 @@ export const TYPE: {
   ITALIC: 'italic',
   KEYBOARD: 'keyboard',
   LESS: 'less',
+  LINE: 'line',
   LINKEDIN: 'linkedin',
   LOCK_WITH_PLAY: 'lock_with_play',
   LOGOUT: 'logout',
@@ -229,25 +249,31 @@ export const TYPE: {
   MESSAGES: 'messages',
   MIC: 'mic',
   MONEY_TRANSFER: 'money_transfer',
-  ADD_MORE: 'add_more',
+  MORE: 'more',
+  MULTISELECT_CHECKED: 'multiselect_checked',
+  MULTISELECT_UNCHECKED: 'multiselect_unchecked',
   NOTIFICATIONS: 'notifications',
   NUMBERED_LIST: 'numbered_list',
   OPEN_IN_NEW_TAB: 'open_in_new_tab',
   PADLOCK: 'padlock',
+  PAUSE: 'pause',
   PENCIL: 'pencil',
+  PI: 'pi',
   PLAY: 'play',
   PLUS: 'plus',
   POINTS: 'points',
-  PROFILE: 'profile',
-  PROFILE_VIEW: 'profile_view',
   PROFILE_SETTINGS: 'profile_settings',
+  PROFILE_VIEW: 'profile_view',
+  PROFILE: 'profile',
   QUESTION: 'question',
+  QUOTE: 'quote',
   RECENT_QUESTIONS: 'recent_questions',
+  RECTANGLE: 'rectangle',
   RELOAD: 'reload',
-  REPORT_FLAG: 'report_flag',
   REPORT_FLAG_OUTLINED: 'report_flag_outlined',
-  ROTATE: 'rotate',
+  REPORT_FLAG: 'report_flag',
   ROTATE_90: 'rotate_90',
+  ROTATE: 'rotate',
   SEARCH: 'search',
   SEEN: 'seen',
   SEND: 'send',
@@ -255,105 +281,98 @@ export const TYPE: {
   SHARE: 'share',
   SHIELD: 'shield',
   SMS: 'sms',
-  STAR: 'star',
-  STAR_HALF: 'star_half',
+  SPARK: 'spark',
   STAR_HALF_OUTLINED: 'star_half_outlined',
+  STAR_HALF: 'star_half',
   STAR_OUTLINED: 'star_outlined',
+  STAR: 'star',
   SUBTITLE: 'subtitle',
+  SUP_SUB: 'sup_sub',
   SYMBOLS: 'symbols',
   TEXTBOOK: 'textbook',
-  THUMB_DOWN: 'thumb_down',
   THUMB_DOWN_OUTLINED: 'thumb_down_outlined',
-  THUMB_UP: 'thumb_up',
+  THUMB_DOWN: 'thumb_down',
   THUMB_UP_OUTLINED: 'thumb_up_outlined',
+  THUMB_UP: 'thumb_up',
   TITLE: 'title',
   TOUGHEST_QUESTIONS: 'toughest_questions',
   TRASH: 'trash',
+  TRIANGLE: 'triangle',
   TWITTER: 'twitter',
   UNDERLINED: 'underlined',
   UNSEEN: 'unseen',
   VERIFIED: 'verified',
   WARNING: 'warning',
   YOUTUBE: 'youtube',
-  ARROW_TOP_RIGHT: 'arrow_top_right',
-  CIRCLE: 'circle',
-  CROP: 'crop',
-  CYRILLIC: 'cyrillic',
-  DRAW: 'draw',
-  DRAWING_MODE: 'drawing_mode',
-  EUROPEAN: 'european',
-  GREEK: 'greek',
-  HIGHLIGHT: 'highlight',
-  LINE: 'line',
-  MORE: 'more',
-  PAUSE: 'pause',
-  RECTANGLE: 'rectangle',
-  SUP_SUB: 'sup_sub',
-  TRIANGLE: 'triangle',
-  PI: 'pi',
-  QUOTE: 'quote',
-  SPARK: 'spark',
-  DOT: 'dot',
-  CLEAR: 'clear',
-  COLLAPSE: 'collapse',
-  CURSOR_SELECT: 'cursor_select',
-  ERASER: 'eraser',
-  MULTISELECT_CHECKED: 'multiselect_checked',
-  MULTISELECT_UNCHECKED: 'multiselect_unchecked',
-  CARET_DOWN: 'caret_down',
-  CARET_UP: 'caret_up',
-  ARROW_DOUBLE_RIGHT: 'arrow_double_right',
-  CHEVRON_DOUBLE_RIGHT: 'chevron_double_right',
 } = {
+  ARROW_DOUBLE_RIGHT: 'arrow_double_right',
   ACADEMIC_CAP: 'academic_cap',
+  ADD_MORE: 'add_more',
   ALL_QUESTIONS: 'all_questions',
   ANSWER: 'answer',
   ANSWERS: 'answers',
-  CHEVRON_DOUBLE_DOWN: 'chevron_double_down',
-  CHEVRON_DOWN: 'chevron_down',
-  CHEVRON_LEFT: 'chevron_left',
-  CHEVRON_RIGHT: 'chevron_right',
-  CHEVRON_UP: 'chevron_up',
+  APPLE: 'apple',
+  ARROW_TOP_RIGHT: 'arrow_top_right',
   ASK_BUBBLE: 'ask_bubble',
   ASK_PARENT_TO_PAY: 'ask_parent_to_pay',
   ATTACHMENT: 'attachment',
   BELL_CHECKED: 'bell_checked',
   BELL_OUTLINED: 'bell_outlined',
   BOLD: 'bold',
-  BULB: 'bulb',
   BULB_CHECKED: 'bulb_checked',
+  BULB: 'bulb',
   BULLETED_LIST: 'bulleted_list',
   CALENDAR: 'calendar',
   CAMERA: 'camera',
+  CARET_DOWN: 'caret_down',
+  CARET_UP: 'caret_up',
   CHAPTER: 'chapter',
   CHECK: 'check',
+  CHEVRON_DOUBLE_DOWN: 'chevron_double_down',
+  CHEVRON_DOUBLE_RIGHT: 'chevron_double_right',
+  CHEVRON_DOWN: 'chevron_down',
+  CHEVRON_LEFT: 'chevron_left',
+  CHEVRON_RIGHT: 'chevron_right',
+  CHEVRON_UP: 'chevron_up',
+  CIRCLE: 'circle',
+  CLEAR: 'clear',
   CLIPBOARD: 'clipboard',
   CLOSE: 'close',
-  COMMENT: 'comment',
+  COLLAPSE: 'collapse',
   COMMENT_OUTLINED: 'comment_outlined',
+  COMMENT: 'comment',
   COUNTER: 'counter',
   CREDIT_CARD: 'credit_card',
-  CROWN: 'crown',
+  CROP: 'crop',
   CROWN_OUTLINED: 'crown_outlined',
+  CROWN: 'crown',
   CUP: 'cup',
+  CURSOR_SELECT: 'cursor_select',
+  CYRILLIC: 'cyrillic',
+  DOT: 'dot',
+  DRAW: 'draw',
+  DRAWING_MODE: 'drawing_mode',
   ENVELOPE: 'envelope',
   EQUATION: 'equation',
+  ERASER: 'eraser',
+  EUROPEAN: 'european',
   EXCLAMATION_MARK: 'exclamation_mark',
   FACEBOOK: 'facebook',
-  GOOGLE: 'google',
-  APPLE: 'apple',
   FILTERS: 'filters',
   FRIEND_ADD: 'friend_add',
-  FRIEND_REMOVE: 'friend_remove',
-  FRIEND_PENDING: 'friend_pending',
   FRIEND_CHECKED: 'friend_checked',
+  FRIEND_PENDING: 'friend_pending',
+  FRIEND_REMOVE: 'friend_remove',
   FRIENDS: 'friends',
   FULLSCREEN: 'fullscreen',
   FUNNEL: 'funnel',
   GLOBE: 'globe',
+  GOOGLE: 'google',
+  GREEK: 'greek',
   HEADING: 'heading',
-  HEART: 'heart',
   HEART_OUTLINED: 'heart_outlined',
+  HEART: 'heart',
+  HIGHLIGHT: 'highlight',
   IMAGE: 'image',
   INFLUENCE: 'influence',
   INFO: 'info',
@@ -361,6 +380,7 @@ export const TYPE: {
   ITALIC: 'italic',
   KEYBOARD: 'keyboard',
   LESS: 'less',
+  LINE: 'line',
   LINKEDIN: 'linkedin',
   LOCK_WITH_PLAY: 'lock_with_play',
   LOGOUT: 'logout',
@@ -370,25 +390,31 @@ export const TYPE: {
   MESSAGES: 'messages',
   MIC: 'mic',
   MONEY_TRANSFER: 'money_transfer',
-  ADD_MORE: 'add_more',
+  MORE: 'more',
+  MULTISELECT_CHECKED: 'multiselect_checked',
+  MULTISELECT_UNCHECKED: 'multiselect_unchecked',
   NOTIFICATIONS: 'notifications',
   NUMBERED_LIST: 'numbered_list',
   OPEN_IN_NEW_TAB: 'open_in_new_tab',
   PADLOCK: 'padlock',
+  PAUSE: 'pause',
   PENCIL: 'pencil',
+  PI: 'pi',
   PLAY: 'play',
   PLUS: 'plus',
   POINTS: 'points',
-  PROFILE: 'profile',
-  PROFILE_VIEW: 'profile_view',
   PROFILE_SETTINGS: 'profile_settings',
+  PROFILE_VIEW: 'profile_view',
+  PROFILE: 'profile',
   QUESTION: 'question',
+  QUOTE: 'quote',
   RECENT_QUESTIONS: 'recent_questions',
+  RECTANGLE: 'rectangle',
   RELOAD: 'reload',
-  REPORT_FLAG: 'report_flag',
   REPORT_FLAG_OUTLINED: 'report_flag_outlined',
-  ROTATE: 'rotate',
+  REPORT_FLAG: 'report_flag',
   ROTATE_90: 'rotate_90',
+  ROTATE: 'rotate',
   SEARCH: 'search',
   SEEN: 'seen',
   SEND: 'send',
@@ -396,55 +422,29 @@ export const TYPE: {
   SHARE: 'share',
   SHIELD: 'shield',
   SMS: 'sms',
-  STAR: 'star',
-  STAR_HALF: 'star_half',
+  SPARK: 'spark',
   STAR_HALF_OUTLINED: 'star_half_outlined',
+  STAR_HALF: 'star_half',
   STAR_OUTLINED: 'star_outlined',
+  STAR: 'star',
   SUBTITLE: 'subtitle',
+  SUP_SUB: 'sup_sub',
   SYMBOLS: 'symbols',
   TEXTBOOK: 'textbook',
-  THUMB_DOWN: 'thumb_down',
   THUMB_DOWN_OUTLINED: 'thumb_down_outlined',
-  THUMB_UP: 'thumb_up',
+  THUMB_DOWN: 'thumb_down',
   THUMB_UP_OUTLINED: 'thumb_up_outlined',
+  THUMB_UP: 'thumb_up',
   TITLE: 'title',
   TOUGHEST_QUESTIONS: 'toughest_questions',
   TRASH: 'trash',
+  TRIANGLE: 'triangle',
   TWITTER: 'twitter',
   UNDERLINED: 'underlined',
   UNSEEN: 'unseen',
   VERIFIED: 'verified',
   WARNING: 'warning',
   YOUTUBE: 'youtube',
-  ARROW_TOP_RIGHT: 'arrow_top_right',
-  CIRCLE: 'circle',
-  CROP: 'crop',
-  CYRILLIC: 'cyrillic',
-  DRAW: 'draw',
-  DRAWING_MODE: 'drawing_mode',
-  EUROPEAN: 'european',
-  GREEK: 'greek',
-  HIGHLIGHT: 'highlight',
-  LINE: 'line',
-  MORE: 'more',
-  PAUSE: 'pause',
-  RECTANGLE: 'rectangle',
-  SUP_SUB: 'sup_sub',
-  TRIANGLE: 'triangle',
-  PI: 'pi',
-  QUOTE: 'quote',
-  SPARK: 'spark',
-  DOT: 'dot',
-  CLEAR: 'clear',
-  COLLAPSE: 'collapse',
-  CURSOR_SELECT: 'cursor_select',
-  ERASER: 'eraser',
-  MULTISELECT_CHECKED: 'multiselect_checked',
-  MULTISELECT_UNCHECKED: 'multiselect_unchecked',
-  CARET_DOWN: 'caret_down',
-  CARET_UP: 'caret_up',
-  ARROW_DOUBLE_RIGHT: 'arrow_double_right',
-  CHEVRON_DOUBLE_RIGHT: 'chevron_double_right',
 };
 
 export const ICON_COLOR: {
