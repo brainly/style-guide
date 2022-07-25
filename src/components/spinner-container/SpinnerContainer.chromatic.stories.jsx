b7429df6f12fd87f55827159ebfa58ff84fdf567
katarzynatobis <tobiskatarzyna@gmail.com>
2022-07-25 12:37:46 +0200

    SpinnerContainer stories to mdx

--- src/components/spinner-container/SpinnerContainer.chromatic.stories.jsx
+++ src/components/spinner-container/SpinnerContainer.chromatic.stories.jsx
@@ -1,6 +1,8 @@
-import * as SpinnerContainer from './SpinnerContainer.stories.jsx';
+import * as SpinnerContainer from './SpinnerContainer.stories.mdx';
 import {mergeStories} from '../../chromatic/utils';
 
 export const Default = mergeStories(SpinnerContainer);
 
-export default SpinnerContainer.default;
+const {includeStories, ...meta} = SpinnerContainer.default;
+
+export default meta;
