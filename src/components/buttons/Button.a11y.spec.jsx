aee33b8d1bebb1530f5d7e4194d5334b1adbc3f4
katarzynatobis <tobiskatarzyna@gmail.com>
2022-08-12 13:26:23 +0200

    Button button (without href)

--- src/components/buttons/Button.a11y.spec.jsx
+++ src/components/buttons/Button.a11y.spec.jsx
@@ -1,46 +1,128 @@
 import * as React from 'react';
-import {render} from '@testing-library/react';
+import {render, within} from '@testing-library/react';
 import {testA11y} from '../../axe';
 import Button from './Button';
 import userEvent from '@testing-library/user-event';
 
 describe('Button', () => {
+  describe('without `href`', () => {
+    it('has a button role and an accessible label', () => {
+      const label = 'Load more Mathematic questions';
+      const button = render(
+        <Button as="button" aria-label={label}>
+          Load more
+        </Button>
+      );
+
+      expect(button.getByRole('button', {name: label})).toBeTruthy();
+    });
+
+    it('is focusable', () => {
+      const button = render(<Button as="button">Read more</Button>);
+
+      button.getByRole('button').focus();
+
+      expect(button.getByRole('button')).toBe(document.activeElement);
+    });
+
+    it('is not focusable and clickable when disabled', () => {
+      const handleOnClick = jest.fn();
+      const label = 'Load more';
+      const button = render(
+        <Button as="button" disabled onClick={handleOnClick}>
+          {label}
+        </Button>
+      );
+
+      button.getByText(label).focus();
+      expect(button.getByText(label)).not.toBe(document.activeElement);
+
+      userEvent.click(button.getByText(label));
+      expect(handleOnClick).not.toHaveBeenCalled();
+    });
+
+    it('fires onClick on click, space and enter', () => {
+      const handleOnClick = jest.fn();
+      const label = 'Load more';
+      const button = render(
+        <Button as="button" onClick={handleOnClick}>
+          {label}
+        </Button>
+      );
+
+      userEvent.click(button.getByRole('button', {name: label}));
+      expect(handleOnClick).toHaveBeenCalledTimes(1);
+
+      button.getByText(label).focus();
+
+      userEvent.keyboard('{space}');
+      expect(handleOnClick).toHaveBeenCalledTimes(2);
+
+      userEvent.keyboard('{enter}');
+      expect(handleOnClick).toHaveBeenCalledTimes(3);
+    });
+
+    it('informs about the loading state and is then disabled', () => {
+      const label = 'Load more';
+      const loadingAriaLabel = 'loading more';
+
+      const button = render(
+        <Button
+          loadingAriaLabel={loadingAriaLabel}
+          loadingAriaLive="assertive"
+          loading
+        >
+          {label}
+        </Button>
+      );
+
+      const status = button.getByRole('status');
+
+      expect(status.getAttribute('aria-live')).toBe('assertive');
+      expect(within(status).getByText(loadingAriaLabel)).toBeTruthy();
+
+      expect(button.getByRole('button').disabled).toBeTruthy();
+    });
+  });
   describe('with `href`', () => {
     it('has a link role and an accessible label', () => {
       const label = 'read more about products';
-      const link = render(
+      const button = render(
         <Button href="https://example.com/" aria-label={label}>
           Read more
         </Button>
       );
 
-      expect(link.getByRole('link', {name: label})).toBeTruthy();
+      expect(button.getByRole('link', {name: label})).toBeTruthy();
+      expect(button.getByRole('link').getAttribute('href')).toBe(
+        'https://example.com/'
+      );
     });
 
     it('is focusable', () => {
-      const link = render(
+      const button = render(
         <Button href="https://example.com/">Read more</Button>
       );
 
-      link.getByRole('link').focus();
+      button.getByRole('link').focus();
 
-      expect(link.getByRole('link')).toBe(document.activeElement);
+      expect(button.getByRole('link')).toBe(document.activeElement);
     });
 
     it('is not focusable and clickable when disabled', () => {
       const label = 'read more';
       const onClick = jest.fn();
-      const link = render(
+      const button = render(
         <Button href="https://example.com/" disabled onClick={onClick}>
           {label}
         </Button>
       );
 
-      link.getByText(label).focus();
+      button.getByText(label).focus();
 
-      expect(link.getByText(label)).not.toBe(document.activeElement);
+      expect(button.getByText(label)).not.toBe(document.activeElement);
 
-      userEvent.click(link.getByText(label));
+      userEvent.click(button.getByText(label));
       expect(onClick).not.toHaveBeenCalled();
     });
 
@@ -48,7 +130,7 @@ describe('Button', () => {
       const label = 'read more';
       const newTabLabel = 'in new tab';
 
-      const link = render(
+      const button = render(
         <Button
           href="https://example.com/"
           target="_blank"
@@ -58,16 +140,31 @@ describe('Button', () => {
         </Button>
       );
 
-      expect(link.getByText(newTabLabel)).toBeTruthy();
+      expect(button.getByText(newTabLabel)).toBeTruthy();
     });
-  });
-
-  describe('as button', () => {
-    /* empty */
   });
 });
 
 describe('Button a11y', () => {
+  describe('without `href`', () => {
+    it('should have no a11y violations', async () => {
+      await testA11y(<Button>Read more</Button>);
+    });
+
+    it('should have no a11y violations when aria-label is provided', async () => {
+      await testA11y(
+        <Button aria-label="read more about us">Read more</Button>
+      );
+    });
+
+    it('should have no a11y violations when disabled', async () => {
+      await testA11y(<Button disabled>Read more</Button>);
+    });
+
+    it('should have no a11y violations in loading state', async () => {
+      await testA11y(<Button loading>Read more</Button>);
+    });
+  });
   describe('with `href`', () => {
     it('should have no a11y violations', async () => {
       await testA11y(<Button href="https://example.com/">Read more</Button>);
@@ -96,9 +193,5 @@ describe('Button a11y', () => {
         </Button>
       );
     });
-  });
-
-  describe('as button', () => {
-    /* empty */
   });
 });
