5a6f5973d0cfef6ee03a557ccb4e0149de56e175
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2022-10-18 16:58:23 +0200

    remove console log

--- src/components/dialog/useFocusTrap.js
+++ src/components/dialog/useFocusTrap.js
@@ -32,7 +32,6 @@ export function useFocusTrap({
     }
 
     function handleFocusTrap(event: FocusEvent) {
-      console.log(event.target);
       if (
         event.target instanceof Node &&
         dialogElement.contains(event.target)
