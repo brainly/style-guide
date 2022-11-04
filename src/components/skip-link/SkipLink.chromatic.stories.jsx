56b15a50b6acd982a1938f6d1c9bd3e463ee2c78
coderitual <skowronkow@gmail.com>
2022-11-04 14:43:13 +0100

    Pause animation on end

--- src/components/skip-link/SkipLink.chromatic.stories.jsx
+++ src/components/skip-link/SkipLink.chromatic.stories.jsx
@@ -6,6 +6,10 @@ const {play, ...SkipLinkStories} = SkipLink;
 export const Default = mergeStories(SkipLinkStories);
 
 Default.play = play;
+Default.parameters = {
+  // Notifies Chromatic to pause the animations when they finish for the specific story.
+  chromatic: {pauseAnimationAtEnd: true, delay: 300},
+};
 
 const {includeStories, ...meta} = SkipLink.default;
 
