d3537d2d56f54cb32719eda98465a402b607f28f
Dominika Podgórska <dominika.podgorska@brainly.com>
2022-11-21 13:09:58 +0100

    Radio and Checkbox - documentation and improvements (#2569)
    * Docs
    
    * Add usage and types documentation
    
    * Change description
    
    * Add types
    
    * Fix Radio and RadioGroup onChange event type
    
    * Add missing Button variant
    
    * Do not set Radio animations conditionally
    
    * Turn on animations only when component has rendered
    
    * Improve syntax highlighting for the context type
    
    * Turn on animations after checked state was changed, cleanup
    
    * Create custom wrapper for radio
    
    * Fix attribute name
    
    * Fix Flex margin
    
    * Extend style type to include css variables in Checkbox and Radio
    
    * Add link to useRadioContext documentation
    
    * Disable eslint error
    
    * Rework Radio animation applying logic. Create useIsFirstRender hook
    
    * Do not play checkbox animation on initial render, component improvements. Fix focus animation interfering with checkbox animation by adding wrapper to the box
    
    * Do not set Radio icon class name inline
    
    * fix ref types (#2580)
    
    * Fix instance check
    
    * Do not use % values in stroke-dasharray and stroke-dashoffset, use the same values as in icon viewbox
    
    * Display focus ring only on keyboard navigation
    
    * Display focus ring only on keyboard navigation
    
    * Force reflow so checkbox icon gets painted before transition
    
    * Fix types
    
    * Improve custom wrappers documentation in the RadioGroup docs
    
    Co-authored-by: Bartosz Adamczyk <bartoszadamczykk@gmail.com>

--- src/components/form-elements/checkbox/CheckboxIcon.jsx
+++ src/components/form-elements/checkbox/CheckboxIcon.jsx
@@ -2,45 +2,60 @@
 
 import * as React from 'react';
 
-const CheckIcon = () => (
-  <svg
-    width="16"
-    height="16"
-    viewBox="0 0 16 16"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M3.475 8.01008L6.48494 11.0201L12.5249 4.98"
-      strokeWidth="2.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
+type CheckIconPropsType = {};
+
+const CheckIcon = React.forwardRef<CheckIconPropsType, Element>(
+  (props, ref) => {
+    return (
+      <svg
+        ref={ref}
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M3.475 8.01008L6.48494 11.0201L12.5249 4.98"
+          strokeWidth="2.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    );
+  }
 );
 
-const IndeterminateIcon = () => (
-  <svg
-    width="16"
-    height="16"
-    viewBox="0 0 16 16"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      className="indeterminate-path indeterminate-path--left"
-      d="M4 8H8"
-      strokeWidth="2.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      className="indeterminate-path indeterminate-path--right"
-      d="M8 8H12"
-      strokeWidth="2.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
+type IndeterminateIconPropsType = {};
+
+const IndeterminateIcon = React.forwardRef<IndeterminateIconPropsType, Element>(
+  (props, ref) => {
+    return (
+      <svg
+        ref={ref}
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          className="indeterminate-path indeterminate-path--left"
+          d="M4 8H8"
+          strokeWidth="2.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          className="indeterminate-path indeterminate-path--right"
+          d="M8 8H12"
+          strokeWidth="2.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    );
+  }
 );
 
 export {CheckIcon, IndeterminateIcon};
