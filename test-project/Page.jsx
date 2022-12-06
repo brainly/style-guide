bda430354794eef82b9018c2a2b932d0dd049747
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2022-12-06 14:28:49 +0100

    remove test code

--- test-project/Page.jsx
+++
@@ -1,7 +0,0 @@
-import {AccordionItem, Accordion} from 'brainly-style-guide';
-
-export const Page = () => (
-  <Accordion>
-    <AccordionItem title="Item 1">Accordion Item Description</AccordionItem>
-  </Accordion>
-);
