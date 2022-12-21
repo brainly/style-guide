ba1933158199c80c5089965657bb0e3bbc5c7edb
Mateusz Zimowski <mat.zimowski@gmail.com>
2022-12-21 12:04:29 +0100

    Fix Dialog and Transition not to break SSR (#2612)
    * Fix Dialog and Transition not to break SSR
    
    * Up
    
    * Remove boolean

--- src/components/dialog/Dialog.jsx
+++ src/components/dialog/Dialog.jsx
@@ -9,7 +9,8 @@ import DialogOverlay, {SLOTS} from './DialogOverlay';
 
 // https://github.com/jsdom/jsdom/issues/1781
 const supportsTransitions = () =>
-  Boolean(window && window.TransitionEvent !== undefined);
+  typeof window !== 'undefined' &&
+  typeof window.TransitionEvent !== 'undefined';
 
 export type DialogPropsType = $ReadOnly<{
   open: boolean,
