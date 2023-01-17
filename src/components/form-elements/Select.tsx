9bb4787e781b8383c47d5dd930168d61da5c564e
coderitual <skowronkow@gmail.com>
2023-01-17 17:10:42 +0100

    Fix type: select

--- src/components/form-elements/Select.tsx
+++ src/components/form-elements/Select.tsx
@@ -27,7 +27,7 @@ const ICON_SIZE_MAP = {
   [SIZE.L]: 32,
   [SIZE.M]: 24,
   [SIZE.S]: 16,
-};
+} as const;
 
 export type SelectPropsType = {
   /**
@@ -105,77 +105,79 @@ const getOptionElement = ({value, text}: OptionsPropsType) => (
   </option>
 );
 
-const Select = React.forwardRef((props: SelectPropsType, ref) => {
-  const {
-    valid,
-    invalid,
-    capitalized,
-    fullWidth,
-    value,
-    size = SIZE.M,
-    color,
-    className,
-    options = [],
-    ...additionalProps
-  } = props;
+const Select = React.forwardRef<HTMLSelectElement, SelectPropsType>(
+  (props: SelectPropsType, ref) => {
+    const {
+      valid,
+      invalid,
+      capitalized,
+      fullWidth,
+      value,
+      size = SIZE.M,
+      color,
+      className,
+      options = [],
+      ...additionalProps
+    } = props;
 
-  if (valid === true && invalid === true) {
-    throw {
-      name: 'WrongValidation',
-      message: 'Select can be either valid or invalid!',
-    };
-  }
-
-  const selectClass = classnames(
-    'sg-select',
-    {
-      'sg-select--selected': value,
-      'sg-select--valid': valid,
-      'sg-select--invalid': invalid,
-      'sg-select--capitalized': capitalized,
-      'sg-select--full-width': fullWidth,
-      [`sg-select--${String(color)}`]: color,
-      [`sg-select--${String(size)}`]: size && size !== 'm',
-    },
-    className
-  );
-  const optionsElements = options.map((item, index) => {
-    if (item.options) {
-      return (
-        <optgroup key={item.label + index} label={item.label}>
-          {item.options.map(getOptionElement)}
-        </optgroup>
-      );
+    if (valid === true && invalid === true) {
+      throw {
+        name: 'WrongValidation',
+        message: 'Select can be either valid or invalid!',
+      };
     }
 
-    if (item.text || item.value) {
-      return getOptionElement(item);
-    }
+    const selectClass = classnames(
+      'sg-select',
+      {
+        'sg-select--selected': value,
+        'sg-select--valid': valid,
+        'sg-select--invalid': invalid,
+        'sg-select--capitalized': capitalized,
+        'sg-select--full-width': fullWidth,
+        [`sg-select--${String(color)}`]: color,
+        [`sg-select--${String(size)}`]: size && size !== 'm',
+      },
+      className
+    );
+    const optionsElements = options.map((item, index) => {
+      if ('options' in item) {
+        return (
+          <optgroup key={item.label + index} label={item.label}>
+            {item.options.map(getOptionElement)}
+          </optgroup>
+        );
+      }
 
-    return null;
-  });
+      if (item.text || item.value) {
+        return getOptionElement(item);
+      }
 
-  return (
-    <div className={selectClass}>
-      <div className="sg-select__icon">
-        <Icon
-          type="caret_down"
-          color={ICON_COLOR['icon-gray-50']}
-          size={ICON_SIZE_MAP[size]}
-        />
+      return null;
+    });
+
+    return (
+      <div className={selectClass}>
+        <div className="sg-select__icon">
+          <Icon
+            type="caret_down"
+            color={ICON_COLOR['icon-gray-50']}
+            size={ICON_SIZE_MAP[size]}
+          />
+        </div>
+
+        <select
+          {...additionalProps}
+          className="sg-select__element"
+          value={value}
+          ref={ref}
+        >
+          {optionsElements}
+        </select>
       </div>
-
-      <select
-        {...additionalProps}
-        className="sg-select__element"
-        value={value}
-        ref={ref}
-      >
-        {optionsElements}
-      </select>
-    </div>
-  );
-});
+    );
+  }
+);
 
 Select.displayName = 'Select';
 export default Select;
