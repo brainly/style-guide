9a16ea102e0861f9169e8354ba4e1dc3cecb979f
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2023-01-27 14:00:34 +0100

    rewrite bubble tests with react testing library

--- src/components/bubble/Bubble.spec.tsx
+++ src/components/bubble/Bubble.spec.tsx
@@ -1,67 +1,77 @@
 import * as React from 'react';
 import Bubble, {DIRECTION, ALIGNMENT} from './Bubble';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
 
 test('render', () => {
-  const bubble = shallow(<Bubble direction={DIRECTION.TOP}>Some text</Bubble>);
+  const bubble = render(<Bubble direction={DIRECTION.TOP}>Some text</Bubble>);
+  const root = bubble.container.firstElementChild;
 
-  expect(bubble.hasClass('sg-bubble')).toEqual(true);
+  expect(root.classList.contains('sg-bubble')).toEqual(true);
 });
+
 test('render top', () => {
-  const bubble = shallow(<Bubble direction={DIRECTION.TOP}>Some text</Bubble>);
+  const bubble = render(<Bubble direction={DIRECTION.TOP}>Some text</Bubble>);
+  const root = bubble.container.firstElementChild;
 
-  expect(bubble.hasClass('sg-bubble--top')).toEqual(true);
+  expect(root.classList.contains('sg-bubble--top')).toEqual(true);
   // default position of bubble is center
-  expect(bubble.hasClass('sg-bubble--row-start')).toEqual(false);
-  expect(bubble.hasClass('sg-bubble--row-start')).toEqual(false);
+  expect(root.classList.contains('sg-bubble--row-start')).toEqual(false);
 });
+
 test('render top start', () => {
-  const bubble = shallow(
+  const bubble = render(
     <Bubble direction={DIRECTION.TOP} alignment={ALIGNMENT.START}>
       Some text
     </Bubble>
   );
+  const root = bubble.container.firstElementChild;
 
-  expect(bubble.hasClass('sg-bubble--top')).toEqual(true);
-  expect(bubble.hasClass('sg-bubble--row-start')).toEqual(true);
+  expect(root.classList.contains('sg-bubble--top')).toEqual(true);
+  expect(root.classList.contains('sg-bubble--row-start')).toEqual(true);
 });
+
 test('render right', () => {
-  const bubble = shallow(
-    <Bubble direction={DIRECTION.RIGHT}>Some text</Bubble>
-  );
+  const bubble = render(<Bubble direction={DIRECTION.RIGHT}>Some text</Bubble>);
+  const root = bubble.container.firstElementChild;
 
-  expect(bubble.hasClass('sg-bubble--right')).toEqual(true);
+  expect(root.classList.contains('sg-bubble--right')).toEqual(true);
   // default position of bubble is center (for left/right direction we use column alignment)
-  expect(bubble.hasClass('sg-bubble--column-end')).toEqual(false);
-  expect(bubble.hasClass('sg-bubble--column-end')).toEqual(false);
+  expect(root.classList.contains('sg-bubble--column-end')).toEqual(false);
+  expect(root.classList.contains('sg-bubble--column-end')).toEqual(false);
 });
+
 test('render right end', () => {
-  const bubble = shallow(
+  const bubble = render(
     <Bubble direction={DIRECTION.RIGHT} alignment={ALIGNMENT.END}>
       Some text
     </Bubble>
   );
+  const root = bubble.container.firstElementChild;
 
-  expect(bubble.hasClass('sg-bubble--right')).toEqual(true);
-  expect(bubble.hasClass('sg-bubble--column-end')).toEqual(true);
+  expect(root.classList.contains('sg-bubble--right')).toEqual(true);
+  expect(root.classList.contains('sg-bubble--column-end')).toEqual(true);
   // direction right/left use column alignment, no row alignment
-  expect(bubble.hasClass('sg-bubble--row-end')).toEqual(false);
+  expect(root.classList.contains('sg-bubble--row-end')).toEqual(false);
 });
+
 test('render full', () => {
-  const bubble = shallow(
+  const bubble = render(
     <Bubble direction={DIRECTION.LEFT} full>
       Some text
     </Bubble>
   );
+  const root = bubble.container.firstElementChild;
 
-  expect(bubble.hasClass('sg-bubble--full')).toEqual(true);
+  expect(root.classList.contains('sg-bubble--full')).toEqual(true);
 });
+
 test('renders without shadow', () => {
-  const bubble = shallow(
+  const bubble = render(
     <Bubble direction={DIRECTION.LEFT} noShadow>
       Some text
     </Bubble>
   );
+  const root = bubble.container.firstElementChild;
 
-  expect(bubble.hasClass('sg-bubble--no-shadow')).toEqual(true);
+  expect(root.classList.contains('sg-bubble--no-shadow')).toEqual(true);
 });
