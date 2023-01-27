ca9074e909adf2d34596ceefaad5b1e329902197
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2023-01-27 13:32:52 +0100

    rewrite breadcrumb tests with react testing library

--- src/components/breadcrumb/Breadcrumb.spec.tsx
+++ src/components/breadcrumb/Breadcrumb.spec.tsx
@@ -1,24 +1,27 @@
 import * as React from 'react';
 import Breadcrumb from './Breadcrumb';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
 
 const elements = ['Coments (9)', 'Report', 'Follow'];
 
 test('render', () => {
-  const breadcrumb = shallow(<Breadcrumb elements={elements} />);
+  const breadcrumb = render(<Breadcrumb elements={elements} />);
+  const root = breadcrumb.container.firstChild as HTMLElement;
 
-  expect(breadcrumb.hasClass('sg-breadcrumb-list')).toEqual(true);
-  expect(breadcrumb.find('.sg-breadcrumb-list__element')).toHaveLength(
-    elements.length
-  );
+  expect(root.classList.contains('sg-breadcrumb-list')).toEqual(true);
+  expect(breadcrumb.getAllByRole('listitem')).toHaveLength(elements.length);
 });
+
 test('short', () => {
-  const breadcrumb = shallow(<Breadcrumb elements={elements} short />);
+  const breadcrumb = render(<Breadcrumb elements={elements} short />);
+  const root = breadcrumb.container.firstChild as HTMLElement;
 
-  expect(breadcrumb.hasClass('sg-breadcrumb-list--short')).toEqual(true);
+  expect(root.classList.contains('sg-breadcrumb-list--short')).toEqual(true);
 });
-test('adaptive', () => {
-  const breadcrumb = shallow(<Breadcrumb elements={elements} adaptive />);
 
-  expect(breadcrumb.hasClass('sg-breadcrumb-list--adaptive')).toEqual(true);
+test('adaptive', () => {
+  const breadcrumb = render(<Breadcrumb elements={elements} adaptive />);
+  const root = breadcrumb.container.firstChild as HTMLElement;
+
+  expect(root.classList.contains('sg-breadcrumb-list--adaptive')).toEqual(true);
 });
