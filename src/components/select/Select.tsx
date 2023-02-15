c36fc9c89526092e4f145198bf8366f6eb4c4c3a
Dominika Podgórska <dpodgorskad@gmail.com>
2023-02-15 15:26:15 +0100

    Add props documentation. Attach onClick event to the component

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -42,66 +42,147 @@ export const COLOR = {
 
 export type SelectPropsType = {
   /**
-   * Optional specification for select placeholder
-   * @example <Select value="Option1" placeholder="Select an option" options={[{value: 'option1', label: 'Option1'},{value: 'option2', label: 'Select selector'}]} />
+   * Optional string. Additional class names.
+   */
+  className?: string;
+
+  /**
+   * Optional string. Select placeholder text.
+   * @example <Select placeholder="Select an option" options={[{value: 'option1', label: 'Option1'},{value: 'option2', label: 'Select selector'}]} />
+   * @default 'Select...'
    */
   placeholder?: string;
 
   /**
-   * Optional boolean to specified if it's valid
+   * Optional boolean. Set to true if select is valid.
    * @example <Select valid options={[{value: 'option1', label: 'Option1'},{value: 'option2', label: 'Select selector'}]} />
-   */
+   * @default false
+   * */
   valid?: boolean;
 
   /**
-   * Optional boolean to specified if it's invalid
+   * Optional boolean. Set to true if select is invalid.
    * @example <Select invalid options={[{value: 'option1', label: 'Option1'},{value: 'option2', label: 'Select selector'}]} />
+   * @default false
    */
   invalid?: boolean;
 
   /**
-   * Optional array of options of the select
+   * Optional array. Select options.
    * @example <Select options={[{value: 'option1', label: 'Option1'},{value: 'option2', label: 'Select selector'}]} />
+   * @default []
    */
   options?: ReadonlyArray<SelectOptionType>;
+
+  /**
+   * Optional array. Select selected options.
+   * @example <Select options={[{value: 'option1', label: 'Option1'},{value: 'option2', label: 'Select selector'}]} selectedOptions={[{value: 'option1', label: 'Option1'}]} />
+   * @default []
+   */
   selectedOptions?: ReadonlyArray<SelectOptionType>;
 
-  onClick?: (arg0: React.MouseEvent<HTMLDivElement>) => unknown;
-
-  onOptionChange: (SelectOptionType) => unknown;
-
-  onToggle: (boolean) => unknown;
-
+  /**
+   * Optional boolean. Set to true, if user can select multiple options.
+   * @example <Select multiSelect options={[{value: 'option1', label: 'Option1'},{value: 'option2', label: 'Select selector'}]} />
+   */
   multiSelect?: boolean;
+
+  /**
+   * Optional boolean. Set when you want to manually controll select popup menu appearance. If not set, the component will manage it itself.
+   * @example <Select expanded={false} options={[{value: 'option1', label: 'Option1'},{value: 'option2', label: 'Select selector'}]} selectedOptions={[{value: 'option1', label: 'Option1'}]} />
+   * @default undefined
+   */
   expanded?: boolean;
+
+  /**
+   * Optional boolean. Set when you want to display select popup menu on first render, but you want the component manage it's expanded state itself.
+   * @example <Select defaultExpanded options={[{value: 'option1', label: 'Option1'},{value: 'option2', label: 'Select selector'}]} selectedOptions={[{value: 'option1', label: 'Option1'}]} />
+   * @default undefined
+   */
   defaultExpanded?: boolean;
+
+  /**
+   * Optional boolean. Set to `true` when you want to display option icon (if it has any).
+   * @example <Select withIcons options={[{value: 'option1', label: 'Option1', iconName: 'physics'},{value: 'option2', label: 'Select selector', iconName: 'chemistry'}]} selectedOptions={[{value: 'option1', label: 'Option1'}]} />
+   * @default false
+   */
   withIcons?: boolean;
+
+  /**
+   * Optional boolean. Set to `true` you want the select to stretch out to the parent full width.
+   * @example <Select fullWidth />
+   * @default false
+   */
   fullWidth?: boolean;
+
+  /**
+   * Optional boolean. Set to `true` if you want to disable select.
+   * @example <Select disabled />
+   */
   disabled?: boolean;
 
   /**
-   * There are two sizes options for most of the form elements
-   * @example <Select size="m" options={[{value: 'option1', text: 'Option1'},{value: 'option2', text: 'Select selector'}]} />
-   * @see size="m" https://styleguide.brainly.com/latest/docs/interactive.html?size="m"#select
-   * @see size="l" https://styleguide.brainly.com/latest/docs/interactive.html?size="l"#select
+   * Set to change the Select size. There are three sizes options: `s`, `m` and `l`.
+   * @example <Select size="s" options={[{value: 'option1', text: 'Option1'},{value: 'option2', text: 'Select selector'}]} />
+   * @default "m"
    */
   size?: SelectSizeType;
 
   /**
-   * There are two color variants for form elements, default does not have to be specified
+   * There are two color variants, `default` and `white`. The default does not have to be specified.
    * @example <Select color="white" options={[{value: 'option1', label: 'Option1'},{value: 'option2', label: 'Select selector'}]} />
-   * @see color="default" https://styleguide.brainly.com/latest/docs/interactive.html?color="default"#select
-   * @see color="white" https://styleguide.brainly.com/latest/docs/interactive.html?color="white"#select
    */
   color?: SelectColorType | null | undefined;
 
   /**
-   * Additional class names
+   * Optional callback. Called by clicking on the any part of the Select component.
+   * @example <Select
+   *           onClick={(e) => console.log(e.target)}
+   *           options={[{value: 'option1', label: 'Option1'},{value: 'option2', label: 'Select selector'}]}
+   *          />
    */
-  className?: string;
+  onClick?: (arg0: React.MouseEvent<HTMLDivElement>) => unknown;
+
+  /**
+   * Callback. Called by selecting an option.
+   * @example <Select
+   *           onOptionChange={({value, label, iconName}) => {
+   *              // update component selected options array
+   *           }}
+   *           options={[{value: 'option1', label: 'Option1'},{value: 'option2', label: 'Select selector'}]}
+   *          />
+   */
+  onOptionChange: (SelectOptionType) => unknown;
+
+  /**
+   * Callback. Called only when expanded state is controlled, called when component informs the expanded state should change.
+   * @example <Select
+   *           onToggle={(isOpen) => {
+   *              // set component expanded state to isOpen state
+   *           }}
+   *           options={[{value: 'option1', label: 'Option1'},{value: 'option2', label: 'Select selector'}]}
+   *          />
+   */
+  onToggle: (boolean) => unknown;
 } & Omit<
   React.AllHTMLAttributes<HTMLElement>,
-  'valid' | 'invalid' | 'options' | 'className'
+  | 'className'
+  | 'placeholder'
+  | 'valid'
+  | 'invalid'
+  | 'options'
+  | 'selectedOptions'
+  | 'multiSelect'
+  | 'expanded'
+  | 'defaultExpanded'
+  | 'withIcons'
+  | 'fullWidth'
+  | 'disabled'
+  | 'size'
+  | 'color'
+  | 'onClick'
+  | 'onOptionChange'
+  | 'onToggle'
 >;
 
 const Select = React.forwardRef<HTMLDivElement, SelectPropsType>(
@@ -250,7 +331,7 @@ const Select = React.forwardRef<HTMLDivElement, SelectPropsType>(
     );
 
     return (
-      <div className={selectClass}>
+      <div className={selectClass} onClick={onClick}>
         <div
           ref={mergeRefs(ref, floating.refs.setReference)}
           id={id}
