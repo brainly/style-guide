e3c051ab0ad7bc13eaf2151f445e46761711243d
coderitual <skowronkow@gmail.com>
2023-02-16 12:40:00 +0100

    Extract partcile data to static object

--- src/components/sparks/Sparks.tsx
+++ src/components/sparks/Sparks.tsx
@@ -5,45 +5,11 @@ interface ParticleCSSProperties extends React.CSSProperties {
   '--index'?: number;
 }
 
-const shapes = {
-  heart: (
-    <path
-      fill="currentColor"
-      d="M281.905 416.674c-14.635 13.398-37.094 13.313-51.628-.194l-2.112-1.935C127.403 322.604 61.573 262.406 64.069 187.305a105.899 105.899 0 0 1 44.91-83.038c47.188-31.777 110.766-22.57 147.016 21.292 36.199-43.95 99.85-53.169 147.015-21.292a105.897 105.897 0 0 1 44.91 83.038c2.689 75.101-63.335 135.299-164.095 227.627l-1.92 1.742z"
-      fillRule="nonzero"
-    />
-  ),
-  spark: (
-    <path
-      fill="currentColor"
-      fillRule="evenodd"
-      d="M256 64c11.782075 0 21.333333 9.5512587 21.333333 21.3333333 0 81.6497777 65.528321 147.9945957 146.863834 149.3133267l2.4695.020007C438.448741 234.666667 448 244.217925 448 256c0 11.782075-9.551259 21.333333-21.333333 21.333333-81.649778 0-147.994596 65.528321-149.313327 146.863834l-.020007 2.4695C277.333333 438.448741 267.782075 448 256 448c-11.782075 0-21.333333-9.551259-21.333333-21.333333 0-82.474523-66.858811-149.333334-149.3333337-149.333334l-.3527856-.002858C73.3611886 277.142085 64 267.664254 64 256c0-11.782075 9.5512587-21.333333 21.3333333-21.333333 82.4745227 0 149.3333337-66.858811 149.3333337-149.3333337l.002858-.3527856C234.857915 73.3611886 244.335746 64 256 64z"
-    />
-  ),
-} as const;
-
 interface ParticleProps {
   style?: ParticleCSSProperties;
   size?: number;
   shape?: 'spark' | 'heart';
 }
-
-const Particle = React.forwardRef<HTMLDivElement, ParticleProps>(
-  ({size, shape = 'spark', style}, ref) => {
-    return (
-      <div ref={ref} className="sg-sparks__particle" style={style}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width={size ?? '20'}
-          height={size ?? '20'}
-          viewBox="0 0 512 512"
-        >
-          {shapes[shape]}
-        </svg>
-      </div>
-    );
-  }
-);
 
 interface KeyframeAnimationOptionsEx
   extends Omit<KeyframeAnimationOptions, 'delay' | 'duration'> {
@@ -65,6 +31,12 @@ interface AnimationConfig {
 interface RegisterOptions {
   index?: number;
   animation?: Record<string, Partial<AnimationWithOptions>>;
+}
+
+interface SparksProps {
+  children?: React.ReactNode;
+  shape?: 'spark' | 'heart';
+  variant?: 's' | 'm' | 'l';
 }
 
 function useAnimation(config: AnimationConfig) {
@@ -340,12 +312,117 @@ const shapeColorMap: Record<SparksProps['shape'], string[]> = {
   heart: ['var(--red-20)', 'var(--red-30)', 'var(--red-40)', 'var(--red-50)'],
 };
 
-interface SparksProps {
-  children?: React.ReactNode;
-  shape?: 'spark' | 'heart';
-}
+const shapes = {
+  heart: (
+    <path
+      fill="currentColor"
+      d="M281.905 416.674c-14.635 13.398-37.094 13.313-51.628-.194l-2.112-1.935C127.403 322.604 61.573 262.406 64.069 187.305a105.899 105.899 0 0 1 44.91-83.038c47.188-31.777 110.766-22.57 147.016 21.292 36.199-43.95 99.85-53.169 147.015-21.292a105.897 105.897 0 0 1 44.91 83.038c2.689 75.101-63.335 135.299-164.095 227.627l-1.92 1.742z"
+      fillRule="nonzero"
+    />
+  ),
+  spark: (
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M256 64c11.782075 0 21.333333 9.5512587 21.333333 21.3333333 0 81.6497777 65.528321 147.9945957 146.863834 149.3133267l2.4695.020007C438.448741 234.666667 448 244.217925 448 256c0 11.782075-9.551259 21.333333-21.333333 21.333333-81.649778 0-147.994596 65.528321-149.313327 146.863834l-.020007 2.4695C277.333333 438.448741 267.782075 448 256 448c-11.782075 0-21.333333-9.551259-21.333333-21.333333 0-82.474523-66.858811-149.333334-149.3333337-149.333334l-.3527856-.002858C73.3611886 277.142085 64 267.664254 64 256c0-11.782075 9.5512587-21.333333 21.3333333-21.333333 82.4745227 0 149.3333337-66.858811 149.3333337-149.3333337l.002858-.3527856C234.857915 73.3611886 244.335746 64 256 64z"
+    />
+  ),
+} as const;
 
-const Sparks = ({children, shape = 'heart'}: SparksProps) => {
+const variants = {
+  l: [
+    {
+      style: {
+        gridColumn: '1 / span 1',
+        top: '12px',
+      },
+      colorIndex: 0,
+      size: 16,
+      register: {
+        index: 2,
+        animation: {
+          'heart-loop': {
+            keyframes: [{transform: 'scale(1.25)'}],
+          },
+        },
+      },
+    },
+    {
+      style: {
+        gridColumn: '6 / span 1',
+        top: '4px',
+      },
+      colorIndex: 2,
+      size: 28,
+      register: {index: 0},
+    },
+    {
+      style: {
+        gridColumn: '-2 / span 1',
+        top: '12px',
+      },
+      colorIndex: 3,
+      size: 12,
+      register: {
+        index: 4,
+        animation: {
+          'heart-loop': {
+            keyframes: [{transform: 'scale(1.25)'}],
+          },
+        },
+      },
+    },
+    {
+      style: {
+        '--particle-dir': 1,
+        gridColumn: '2 / span 1',
+        gridRow: '3',
+        alignSelf: 'self-end',
+      },
+      colorIndex: 3,
+      size: 40,
+      register: {index: 3},
+    },
+    {
+      style: {
+        '--particle-dir': 1,
+        gridColumn: '-3 / span 1',
+        gridRow: '3',
+        alignSelf: 'self-end',
+        bottom: '4px',
+      },
+      colorIndex: 1,
+      size: 24,
+      register: {
+        index: 1,
+        animation: {
+          'heart-loop': {
+            keyframes: [{transform: 'scale(1.25)'}],
+          },
+        },
+      },
+    },
+  ],
+} as const;
+
+const Particle = React.forwardRef<HTMLDivElement, ParticleProps>(
+  ({size, shape = 'spark', style}, ref) => {
+    return (
+      <div ref={ref} className="sg-sparks__particle" style={style}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={size ?? '20'}
+          height={size ?? '20'}
+          viewBox="0 0 512 512"
+        >
+          {shapes[shape]}
+        </svg>
+      </div>
+    );
+  }
+);
+
+const Sparks = ({children, shape = 'heart', variant = 'l'}: SparksProps) => {
   const animationConfig = shapeAnimationMap[shape];
   const {register, setPhase} = useAnimation(animationConfig);
 
@@ -366,83 +443,15 @@ const Sparks = ({children, shape = 'heart'}: SparksProps) => {
     >
       {children}
       <div className="sg-sparks__container">
-        <Particle
-          style={{
-            gridColumn: '1 / span 1',
-            color: shapeColor[0],
-            top: '12px',
-          }}
-          size={16}
-          shape={shape}
-          {...register({
-            index: 2,
-            animation: {
-              'heart-loop': {
-                keyframes: [{transform: 'scale(1.25)'}],
-              },
-            },
-          })}
-        />
-        <Particle
-          style={{
-            gridColumn: '6 / span 1',
-            color: shapeColor[2],
-            top: '4px',
-          }}
-          size={28}
-          shape={shape}
-          {...register({index: 0})}
-        />
-        <Particle
-          style={{
-            gridColumn: '-2 / span 1',
-            color: shapeColor[3],
-            top: '12px',
-          }}
-          size={12}
-          shape={shape}
-          {...register({
-            index: 4,
-            animation: {
-              'heart-loop': {
-                keyframes: [{transform: 'scale(1.25)'}],
-              },
-            },
-          })}
-        />
-
-        <Particle
-          style={{
-            '--particle-dir': 1,
-            gridColumn: '2 / span 1',
-            gridRow: '3',
-            alignSelf: 'self-end',
-            color: shapeColor[3],
-          }}
-          size={40}
-          shape={shape}
-          {...register({index: 3})}
-        />
-        <Particle
-          style={{
-            '--particle-dir': 1,
-            gridColumn: '-3 / span 1',
-            gridRow: '3',
-            alignSelf: 'self-end',
-            color: shapeColor[1],
-            bottom: '4px',
-          }}
-          size={24}
-          shape={shape}
-          {...register({
-            index: 1,
-            animation: {
-              'heart-loop': {
-                keyframes: [{transform: 'scale(1.25)'}],
-              },
-            },
-          })}
-        />
+        {variants[variant].map((particle, index) => (
+          <Particle
+            key={index}
+            shape={shape}
+            {...particle}
+            style={{...particle.style, color: shapeColor[particle.colorIndex]}}
+            {...register(particle.register)}
+          />
+        ))}
       </div>
     </div>
   );
