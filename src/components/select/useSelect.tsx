5cd52cc9ea59706077a21408ae72e467c7af1b6c
Dominika Podgórska <dpodgorskad@gmail.com>
2023-02-21 15:41:56 +0100

    Add expand and close height transition

--- src/components/select/useSelect.tsx
+++ src/components/select/useSelect.tsx
@@ -30,6 +30,7 @@ const useSelect = (props: UseSelectPropsType) => {
     expanded || defaultExpanded || false
   );
   const isExpandedControlled = expanded !== undefined;
+  const lastRef = React.useRef<DOMRect>();
 
   // Handle expanded change when controlled
   React.useEffect(() => {
@@ -48,9 +49,60 @@ const useSelect = (props: UseSelectPropsType) => {
     if (!multiSelect) onOpenChange(false);
   };
 
+  const animateEntry = () => {
+    const floatingContainer = document.querySelector(
+      '.sg-select-new__options-floating-container'
+    ) as HTMLDivElement;
+
+    // Desired position
+    lastRef.current = floatingContainer.getBoundingClientRect();
+
+    // Reset the popup to pre-appear position
+    floatingContainer.style.height = `0px`;
+
+    // Wait for the next frame so we
+    // know all the style changes have
+    // taken hold.
+    requestAnimationFrame(function () {
+      // Switch on animations.
+      floatingContainer.classList.add('animate-on-transforms');
+      // Now apply the height change
+      floatingContainer.style.height = `${lastRef.current?.height}px`;
+    });
+  };
+
+  const animateExit = ({callback}) => {
+    const floatingContainer = document.querySelector(
+      '.sg-select-new__options-floating-container'
+    ) as HTMLDivElement;
+
+    floatingContainer.style.height = `0px`;
+
+    // Capture the end with transitionend and call expanded change
+    floatingContainer?.addEventListener('transitionend', () => {
+      callback();
+    });
+  };
+
+  React.useEffect(() => {
+    if (isExpanded) {
+      animateEntry();
+    }
+  }, [isExpanded]);
+
   const onOpenChange = (isOpen: boolean) => {
-    if (isExpandedControlled && onToggle) onToggle(isOpen);
-    else setIsExpanded(isOpen);
+    const handleOpenChange = isOpen => {
+      if (isExpandedControlled && onToggle) onToggle(isOpen);
+      else setIsExpanded(isOpen);
+    };
+
+    // If the component will close
+    // we first have to animate its exit before destroying it
+    if (!isOpen) {
+      animateExit({callback: () => handleOpenChange(isOpen)});
+    } else {
+      handleOpenChange(isOpen);
+    }
   };
 
   return {
