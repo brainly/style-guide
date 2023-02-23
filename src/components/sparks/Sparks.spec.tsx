5e106966cdc99b37155fdb5f8bd17659a566bb08
coderitual <skowronkow@gmail.com>
2023-02-23 15:37:48 +0100

    Mock animation

--- src/components/sparks/Sparks.spec.tsx
+++ src/components/sparks/Sparks.spec.tsx
@@ -3,6 +3,17 @@ import {render} from '@testing-library/react';
 import {testA11y} from '../../axe';
 import Sparks from './Sparks';
 
+// mock all animations WAAPI so tests run correctly in jsdom environment (no window)
+jest.mock('./animation', () => ({
+  useAnimation: jest.fn(() => ({
+    animation: {
+      play: jest.fn(),
+      cancel: jest.fn(),
+    },
+    animationRef: React.createRef(),
+  })),
+}));
+
 describe('<Sparks />', () => {
   it('renders correctly with default props', () => {
     const sparks = render(<Sparks />);
