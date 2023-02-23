dc46117d1ddbb2ad117b55e28b445455db9fc928
coderitual <skowronkow@gmail.com>
2023-02-23 14:31:05 +0100

    Add accessibility rules

---
+++ src/components/sparks/stories/rules.a11y.ts
@@ -0,0 +1,10 @@
+const rules = [
+  {
+    pattern: '<b>Should</b> respect <code>prefers-reduced-motion</code>.',
+    comment: `<code>prefers-reduced-motion</code> system setting is respected.`,
+    status: 'TO DO',
+    tests: 'N/A',
+  },
+];
+
+export default rules;
