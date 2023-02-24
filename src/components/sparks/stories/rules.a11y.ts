3d40b495dd674306953adf1e3c37c0d56d13f70c
coderitual <skowronkow@gmail.com>
2023-02-24 16:42:38 +0100

    Add docs

--- src/components/sparks/stories/rules.a11y.ts
+++ src/components/sparks/stories/rules.a11y.ts
@@ -1,8 +1,8 @@
 const rules = [
   {
     pattern: '<b>Should</b> respect <code>prefers-reduced-motion</code>.',
-    comment: `<code>prefers-reduced-motion</code> system setting is respected.`,
-    status: 'TO DO',
+    comment: `<code>prefers-reduced-motion</code> system setting is respected. Alternate animations are used when the user has requested that the system minimize the amount of animation or motion it uses.`,
+    status: 'DONE',
     tests: 'N/A',
   },
 ];
