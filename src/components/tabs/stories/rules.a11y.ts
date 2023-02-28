57c323b074c473c48288bd5fc6d4a30532b7fc70
katarzynatobis <tobiskatarzyna@gmail.com>
2023-02-28 13:29:45 +0100

    improve Tabs stories structure

---
+++ src/components/tabs/stories/rules.a11y.ts
@@ -0,0 +1,11 @@
+const rules = [
+  {
+    pattern: '<b>Should</b> have an accessible name.',
+    comment:
+      'Can be named by setting a value for <code>title</code> prop (defaults to icon type).',
+    status: 'DONE',
+    tests: 'DONE',
+  },
+];
+
+export default rules;
