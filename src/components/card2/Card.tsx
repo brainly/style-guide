774457136454d42625f754eca50437ddae782526
coderitual <skowronkow@gmail.com>
2023-03-21 15:16:17 +0100

    Add checkbox indicator

--- src/components/card2/Card.tsx
+++ src/components/card2/Card.tsx
@@ -1,5 +1,7 @@
 import Button from './CardButton';
 import Radio from './CardRadio';
-import Checkbox from './CardCheckbox';
+import Checkbox, {
+  CardCheckboxIndicator as CheckboxIndicator,
+} from './CardCheckbox';
 
-export default {Button, Radio, Checkbox};
+export default {Button, Radio, Checkbox, CheckboxIndicator};
--- src/components/card2/CardCheckbox.tsx
+++ src/components/card2/CardCheckbox.tsx
@@ -15,47 +15,91 @@ export interface CardCheckboxPropsType extends CheckboxPropsType {
   style?: React.CSSProperties;
 }
 
+// create context for CardCheckboxIndicator to use
+export const CardCheckboxContext = React.createContext({
+  checked: false,
+  disabled: false,
+});
+
 const CardCheckbox = ({
   className,
   children,
   width,
   height,
   style,
-  ...checkboxProps
+
+  // list all input typed checkbox related props individually one by one
+  checked,
+  defaultChecked = false,
+  disabled,
+  id,
+  indeterminate,
+  invalid = false,
+  required = false,
+  value,
+  name,
+  onChange,
+  ...props
 }: CardCheckboxPropsType) => {
   const [hover, setHover] = React.useState(false);
+  const isControlled = checked !== undefined;
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const [isChecked, setIsChecked] = React.useState(
+    isControlled ? checked : defaultChecked
+  );
+
   const cssVariables = {
     '--card-width': width,
     '--card-height': height,
   };
 
-  const indicator = React.Children.toArray(children).find(
-    child => (child as React.ReactElement).type === CardCheckboxIndicator
-  );
+  const onInputChange = React.useCallback(
+    e => {
+      if (!isControlled) {
+        setIsChecked(val => !val);
+      }
 
-  const defaultIndicator = (
-    <CardCheckboxIndicator
-      {...checkboxProps}
-      className={indicator ? 'visually-hidden' : undefined}
-    />
+      if (onChange) onChange(e);
+    },
+    [onChange, isControlled]
   );
 
   return (
-    <label
-      className={cx('sg-card-radio', className, {
-        'sg-card-radio--hover': hover,
-      })}
-      style={{...style, ...cssVariables}}
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
+    <CardCheckboxContext.Provider
+      value={{
+        checked: isChecked,
+        disabled,
+      }}
     >
-      {indicator || defaultIndicator}
-      {children}
-    </label>
+      <div
+        className={cx('sg-card', className, {
+          'sg-card--hover': hover,
+        })}
+        style={{...style, ...cssVariables}}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        <input
+          ref={inputRef}
+          className="sg-card__input"
+          type="checkbox"
+          checked={isChecked}
+          disabled={disabled}
+          name={name}
+          onChange={onInputChange}
+          required={required}
+          value={value}
+          aria-checked={indeterminate ? 'mixed' : isChecked}
+          aria-invalid={invalid ? true : undefined}
+          {...props}
+        />
+        {children}
+      </div>
+    </CardCheckboxContext.Provider>
   );
 };
 
-export interface CardCheckboxIndicatorPropsType extends CheckboxPropsType {
+export interface CardCheckboxIndicatorPropsType {
   slot?:
     | 'top-left'
     | 'center-left'
@@ -67,22 +111,23 @@ export interface CardCheckboxIndicatorPropsType extends CheckboxPropsType {
   className?: string;
 }
 
-const CardCheckboxIndicator = ({
+export const CardCheckboxIndicator = ({
   slot = 'top-left',
   style,
   className,
-  ...checkboxProps
 }: CardCheckboxIndicatorPropsType) => {
+  const {checked, disabled} = React.useContext(CardCheckboxContext);
+
   return (
     <div
       className={cx(
-        'sg-card-checkbox__indicator',
-        `sg-card-checkbox__indicator--${slot}`,
+        'sg-card__indicator',
+        `sg-card__card__indicator--${slot}`,
         className
       )}
       style={style}
     >
-      <Checkbox {...checkboxProps} />
+      <Checkbox checked={checked} disabled={disabled} />
     </div>
   );
 };
