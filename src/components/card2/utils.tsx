a95c52e17d90211c78a2f1cf25fd0362f73d98d9
coderitual <skowronkow@gmail.com>
2023-04-24 10:29:12 +0200

    Add shade hex color function

---
+++ src/components/card2/utils.tsx
@@ -0,0 +1,17 @@
+export function shadeHexColor(color: string, percent: number) {
+  const f = parseInt(color.slice(1), 16),
+    t = percent < 0 ? 0 : 255,
+    p = percent < 0 ? percent * -1 : percent,
+    R = f >> 16,
+    G = (f >> 8) & 0x00ff,
+    B = f & 0x0000ff;
+
+  return `#${(
+    0x1000000 +
+    (Math.round((t - R) * p) + R) * 0x10000 +
+    (Math.round((t - G) * p) + G) * 0x100 +
+    (Math.round((t - B) * p) + B)
+  )
+    .toString(16)
+    .slice(1)}`;
+}
