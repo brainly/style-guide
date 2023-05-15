49b2ce83864a495317630a9de095925cfc6f478b
katarzynatobis <tobiskatarzyna@gmail.com>
2023-05-15 14:10:34 +0200

    add rules

---
+++ src/components/tooltip/stories/rules.a11y.ts
@@ -0,0 +1,52 @@
+const rules = [
+  {
+    pattern: '<b>Should</b> have no role.',
+    status: 'TO DO',
+    tests: 'TO DO',
+  },
+  {
+    pattern: '<b>Should</b> be hidden from accessibility tree.',
+    status: 'TO DO',
+    tests: 'TO DO',
+  },
+  {
+    pattern: '<b>Should</b> be displayed on trigger hover and focus.',
+    status: 'DONE',
+    tests: 'TO DO',
+  },
+  {
+    pattern: '<b>Should</b> be dismissable by pressing <code>Esc</code>.',
+    status: 'TO DO',
+    tests: 'TO DO',
+  },
+  {
+    pattern: '<b>Should</b> be hoverable.',
+    status: 'TO DO',
+    tests: 'TO DO',
+  },
+  {
+    pattern: '<b>Should</b> be persistent.',
+    status: 'TO DO',
+    tests: 'TO DO',
+  },
+  {
+    pattern: '<b>Should</b> label or describe the trigger.',
+    comment:
+      'Tooltip content is linked to the trigger by <code>aria-labelledby</code> or <code>aria-describedby</code>.',
+    status: 'TO DO',
+    tests: 'TO DO',
+  },
+  {
+    pattern: '<b>Should not</b> be focusable of contain interactive elements.',
+    comment: 'Tooltip can contain only (styled) text and/or images.',
+    status: 'TO DO',
+    tests: 'N/A',
+  },
+  {
+    pattern: '<b>Should</b> be displayed if JS is not available.',
+    status: 'TO DO',
+    tests: 'TO DO',
+  },
+];
+
+export default rules;
