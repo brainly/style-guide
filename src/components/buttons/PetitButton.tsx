d6134af90396659236cf521c1c63d28dd7d2aae4
katarzynatobis <tobiskatarzyna@gmail.com>
2023-05-23 11:18:37 +0200

    add variants

--- src/components/buttons/PetitButton.tsx
+++ src/components/buttons/PetitButton.tsx
@@ -2,13 +2,46 @@ import * as React from 'react';
 import cx from 'classnames';
 import Text from '../text/Text';
 
+export const PETIT_BUTTON_VARIANT = {
+  TRANSPARENT: 'transparent',
+  TRANSPARENT_INVERTED: 'transparent-inverted',
+  TRANSPARENT_LIGHT: 'transparent-light',
+  TRANSPARENT_INDIGO: 'transparent-indigo',
+  SOLID_LIGHT: 'solid-light',
+  SOLID_INDIGO_LIGHT: 'solid-indigo-light',
+} as const;
+
+type PetitButtonVariantType =
+  | 'transparent'
+  | 'transparent-inverted'
+  | 'transparent-light'
+  | 'transparent-indigo'
+  | 'solid-light'
+  | 'solid-indigo-light';
+
 export type PetitButtonPropsType = {
+  /**
+   * Specify variant of the button that you want to use
+   * @example <PetitButton variant="solid-light">
+   *            button
+   *          </PetitButton>
+   *
+   */
+  variant?: PetitButtonVariantType;
   className?: string;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const PetitButton = (props: PetitButtonPropsType) => {
-  const {className, children, ...rest} = props;
-  const buttonClass = cx('sg-button-petit', className);
+const PetitButton = ({
+  className,
+  variant,
+  children,
+  ...rest
+}: PetitButtonPropsType) => {
+  const buttonClass = cx(
+    'sg-button-petit',
+    `sg-button-petit--${variant}`,
+    className
+  );
 
   return (
     <button {...rest} className={buttonClass}>
