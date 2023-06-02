449555ae89203a35b8b8a6add5d868bacec65bf4
Dominika Podgórska <dominika.podgorska@brainly.com>
2023-06-02 12:01:26 +0200

    New Tooltip component (#2732)
    * Tooltip base
    
    * Create useTooltip hook that's responsible for the tooltip logic
    
    * Add logic to the trigger and tooltip
    
    * Style the tooltip
    
    * Do not display tooltip as open
    
    * Add focus support
    
    * Add arrow
    
    * Add story with different types of triggers
    
    * Render the tooltip trigger based on the children type
    
    * Handle placement prop. Add stories
    
    * Add tooltip max width
    
    * Add story with short tooltip
    
    * Handle id
    
    * Add padding on the arrow for top and bottom positioning so it doesn't overflow the rounded edges
    
    * Handle controlled open state
    
    * Story content change
    
    * Limit placement of left and right start and end
    
    * Add tooltip offset from the trigger
    
    * Use TooltipPlacement type
    
    * Add sizes, change element label to string
    
    * Pass tooltip trigger classname
    
    * Add shadow
    
    * Limit tooltip width to 60 character max
    
    * Fix sizes story
    
    * Disable tooltip on the touch screens
    
    * Add max char width to the label
    
    * Add color themes, add drop shadow to the arrow
    
    * Do not close tooltip when hovering it
    
    * Ensure user does not fire hover events over other elements unintentionally while they traverse the polygon
    
    * Fix bug with disappearing box shadow on the floating container
    
    * Fix word break, limit chars width
    
    * Increase story space
    
    * Create new placements story, remove redundant ones
    
    * Add open and close animations
    
    * Add missing timing functions for exit animation
    
    * Change default story
    
    * Add Counter example
    
    * Alter transitions for reduced motion
    
    * Fix trigger display
    
    * Fix mixin name
    
    * tests
    
    * Tooltip a11y (#2745)
    
    * add rules
    
    * expand rules
    
    * add Tooltip accessibility mdx
    
    * add usage examples
    
    * remove tooltip from accessibility tree and allow to label the trigger
    
    * update done rules
    
    * add border in forced colors mode
    
    ---------
    
    Co-authored-by: Dominika Podgórska <dominika.podgorska@brainly.com>
    
    * Add more tests
    
    * Add chromatic stories
    
    * Add usage example
    
    * Add Trigger and Element argtables. Enhance docs
    
    * Ensure forwarded ref is not going to override trigger ref
    
    * Do not set display property on trigger
    
    * Documentation style enhancements
    
    * Add more chromatic stories. Documentation changes
    
    * Add tooltip min height and min width
    
    * Ensure text is centered
    
    * Improvements
    
    * Mock ResizeObserver
    
    * Return width from resize observer mock
    
    * Fix cloning element
    
    * Fix tests
    
    * Mock ResizeObserver for tests
    
    * remove wrong usage example
    
    * Hide tooltip until reference element is in view
    
    * Add more tests covering dismissing with ESC key
    
    * Add background to the p tag example
    
    * Fix arrow offset when there's no space for padding
    
    * Change controlled state management
    
    * Story fix
    
    * Replace with correct param
    
    * Merge chromatic stories, remove unnecessary ones
    
    * Add delay to chromatic snapshots
    
    ---------
    
    Co-authored-by: katarzynatobis <60467496+katarzynatobis@users.noreply.github.com>
    Co-authored-by: katarzynatobis <tobiskatarzyna@gmail.com>

---
+++ src/components/tooltip/Tooltip.tsx
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import TooltipElement from './TooltipElement';
+import TooltipTrigger from './TooltipTrigger';
+import {TooltipContext} from './useTooltipContext';
+import useTooltip, {TooltipPlacement, SizeType, ColorType} from './useTooltip';
+
+export type TooltipPropsType = {
+  /**
+   * Tooltip content which consists of tooltip element and tooltip trigger.
+   * @example <Tooltip>
+        <Tooltip.Element>Here's my tooltip</Tooltip.Element>
+        <Tooltip.Trigger>
+          <Button>Hover me</Button>
+        </Tooltip.Trigger>
+      </Tooltip>
+   */
+  children?: React.ReactNode;
+
+  /**
+   * Tooltip id.
+   */
+  id?: string;
+
+  /**
+   * Tooltip alignment.
+   */
+  placement?: TooltipPlacement;
+
+  /**
+   * Tooltip size.
+   */
+  size?: SizeType;
+
+  /**
+   * Specify color variant of the Tooltip.
+   * @example <Tooltip color="dark">...</Tooltip>
+   */
+  color?: ColorType;
+
+  /**
+   * Set if Tooltip should be displayed by default.
+   */
+  defaultOpen?: boolean;
+
+  /**
+   * Only controlled component. Set Tooltip open state.
+   */
+  open?: boolean;
+
+  asLabel?: boolean;
+
+  /**
+   * Only controlled component. Handle Tooltip open state change.
+   */
+  onOpenChange?: (arg0: boolean) => void;
+} & Omit<
+  React.AllHTMLAttributes<HTMLElement>,
+  'children' | 'className' | 'size' | 'placement'
+>;
+
+const Tooltip = ({
+  children,
+  placement,
+  id,
+  size,
+  color,
+  asLabel,
+  defaultOpen = false,
+  open,
+  onOpenChange,
+}: TooltipPropsType) => {
+  const tooltip = useTooltip({
+    placement,
+    customId: id,
+    size,
+    color,
+    defaultOpen,
+    open,
+    onOpenChange,
+    asLabel,
+  });
+
+  return (
+    <TooltipContext.Provider value={tooltip}>
+      {children}
+    </TooltipContext.Provider>
+  );
+};
+
+Tooltip.Trigger = TooltipTrigger;
+Tooltip.Element = TooltipElement;
+
+export default Tooltip;
