2ec6cd7a9e407870a27423a67b478f97ee3bcc00
coderitual <skowronkow@gmail.com>
2023-06-06 12:44:50 +0200

    Fix unstyled button type

--- src/components/buttons/UnstyledButton.tsx
+++ src/components/buttons/UnstyledButton.tsx
@@ -5,13 +5,14 @@ export type UnstyledButtonPropsType = {
   className?: string;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const UnstyledButton = React.forwardRef<HTMLButtonElement, ButtonPropsType>(
-  (props, ref) => {
-    const {className, ...rest} = props;
-    const buttonClass = cx('sg-button-unstyled', className);
+const UnstyledButton = React.forwardRef<
+  HTMLButtonElement,
+  UnstyledButtonPropsType
+>((props, ref) => {
+  const {className, ...rest} = props;
+  const buttonClass = cx('sg-button-unstyled', className);
 
-    return <button {...rest} ref={ref} className={buttonClass} />;
-  }
-);
+  return <button {...rest} ref={ref} className={buttonClass} />;
+});
 
 export default UnstyledButton;
