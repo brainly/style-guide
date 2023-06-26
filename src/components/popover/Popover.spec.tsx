ccd115c96cf51c1902e7de63ed9222f1b6caaadb
Dominika Podgórska <dpodgorskad@gmail.com>
2023-06-26 12:10:47 +0200

    Tests fixes

--- src/components/popover/Popover.spec.tsx
+++ src/components/popover/Popover.spec.tsx
@@ -18,6 +18,8 @@ global.ResizeObserver = jest.fn().mockImplementation(() => ({
   disconnect: jest.fn(),
 }));
 
+const POPOVER_TEXT = 'Get your badge on!';
+
 export function RenderPopover(props: {
   defaultOpen?: boolean;
   useHover?: boolean;
@@ -28,7 +30,7 @@ export function RenderPopover(props: {
       <Popover.Element padding="24px">
         <Flex direction="column" style={{gap: '10px'}}>
           <Text weight="bold" align="to-left">
-            Get your badge on!
+            {POPOVER_TEXT}
           </Text>
           <Text align="to-left">
             Now you can show off your brain power with flashy badges. Complete
@@ -39,6 +41,7 @@ export function RenderPopover(props: {
       </Popover.Element>
       <Popover.Trigger>
         <Button
+          data-testid="button1"
           icon={<Icon color="adaptive" type="options" aria-hidden />}
           iconOnly
           variant="solid-light"
@@ -52,7 +55,7 @@ describe('<Popover />', () => {
   it('does not display popover by default', () => {
     render(<RenderPopover />);
 
-    expect(screen.queryByText('Copy to clipboard')).not.toBeInTheDocument();
+    expect(screen.queryByText(POPOVER_TEXT)).not.toBeInTheDocument();
   });
 
   it('displays popover when trigger is hovered', async () => {
@@ -63,29 +66,27 @@ describe('<Popover />', () => {
     expect(button).toBeInTheDocument();
 
     userEvent.hover(button);
-    expect(popover.getByText('Copy to clipboard')).toBeInTheDocument();
+    expect(popover.getByText(POPOVER_TEXT)).toBeInTheDocument();
   });
 
   it('does not display popover when opening with hover is disabled and trigger is hovered', async () => {
-    render(<RenderPopover />);
+    render(<RenderPopover useHover={false} />);
     const button = screen.getByTestId('button1');
 
     userEvent.hover(button);
-    expect(screen.queryByText('Copy to clipboard')).not.toBeInTheDocument();
+    expect(screen.queryByText(POPOVER_TEXT)).not.toBeInTheDocument();
   });
 
   it('does not keep popover open when click opening is disabled and trigger is clicked, then unhovered', async () => {
-    const popover = render(<RenderPopover />);
+    const popover = render(<RenderPopover useClick={false} />);
     const button = screen.getByTestId('button1');
 
     userEvent.hover(button);
     userEvent.click(button);
-    expect(screen.queryByText('Copy to clipboard')).toBeInTheDocument();
+    expect(screen.queryByText(POPOVER_TEXT)).toBeInTheDocument();
 
     userEvent.unhover(button);
-    await waitForElementToBeRemoved(() =>
-      popover.queryByText('Copy to clipboard')
-    );
+    await waitForElementToBeRemoved(() => popover.queryByText(POPOVER_TEXT));
   });
 
   it('hides popover when trigger is no longer hovered', async () => {
@@ -94,12 +95,10 @@ describe('<Popover />', () => {
     const button = screen.getByTestId('button1');
 
     userEvent.hover(button);
-    expect(popover.getByText('Copy to clipboard')).toBeInTheDocument();
+    expect(popover.getByText(POPOVER_TEXT)).toBeInTheDocument();
 
     userEvent.unhover(button);
-    await waitForElementToBeRemoved(() =>
-      popover.queryByText('Copy to clipboard')
-    );
+    await waitForElementToBeRemoved(() => popover.queryByText(POPOVER_TEXT));
   });
 
   it('does not hide popover when hover leaves if trigger was clicked', async () => {
@@ -108,10 +107,10 @@ describe('<Popover />', () => {
 
     userEvent.hover(button);
     fireEvent.click(button);
-    expect(popover.getByText('Copy to clipboard')).toBeInTheDocument();
+    expect(popover.getByText(POPOVER_TEXT)).toBeInTheDocument();
 
     userEvent.unhover(button);
-    expect(popover.getByText('Copy to clipboard')).toBeInTheDocument();
+    expect(popover.getByText(POPOVER_TEXT)).toBeInTheDocument();
   });
 
   it('displays popover when trigger is focused', () => {
@@ -119,7 +118,7 @@ describe('<Popover />', () => {
     const button = screen.getByTestId('button1');
 
     fireEvent.focus(button);
-    expect(popover.getByText('Copy to clipboard')).toBeInTheDocument();
+    expect(popover.getByText(POPOVER_TEXT)).toBeInTheDocument();
   });
 
   it('hides popover when trigger loses focus', async () => {
@@ -127,12 +126,10 @@ describe('<Popover />', () => {
     const button = screen.getByTestId('button1');
 
     fireEvent.focus(button);
-    expect(popover.getByText('Copy to clipboard')).toBeInTheDocument();
+    expect(popover.getByText(POPOVER_TEXT)).toBeInTheDocument();
 
     fireEvent.blur(button);
-    await waitForElementToBeRemoved(() =>
-      popover.queryByText('Copy to clipboard')
-    );
+    await waitForElementToBeRemoved(() => popover.queryByText(POPOVER_TEXT));
   });
 
   it('hides popover if hover leaves, even when trigger is focused', async () => {
@@ -140,15 +137,13 @@ describe('<Popover />', () => {
     const button = screen.getByTestId('button1');
 
     fireEvent.focus(button);
-    expect(popover.getByText('Copy to clipboard')).toBeInTheDocument();
+    expect(popover.getByText(POPOVER_TEXT)).toBeInTheDocument();
 
     userEvent.hover(button);
-    expect(popover.getByText('Copy to clipboard')).toBeInTheDocument();
+    expect(popover.getByText(POPOVER_TEXT)).toBeInTheDocument();
 
     userEvent.unhover(button);
-    await waitForElementToBeRemoved(() =>
-      popover.queryByText('Copy to clipboard')
-    );
+    await waitForElementToBeRemoved(() => popover.queryByText(POPOVER_TEXT));
   });
 
   it('hides popover if focus leaves, even when trigger is hovered', async () => {
@@ -157,12 +152,10 @@ describe('<Popover />', () => {
 
     fireEvent.focus(button);
     userEvent.hover(button);
-    expect(popover.getByText('Copy to clipboard')).toBeInTheDocument();
+    expect(popover.getByText(POPOVER_TEXT)).toBeInTheDocument();
 
     fireEvent.blur(button);
-    await waitForElementToBeRemoved(() =>
-      popover.queryByText('Copy to clipboard')
-    );
+    await waitForElementToBeRemoved(() => popover.queryByText(POPOVER_TEXT));
   });
 
   it('hides popover when Esc key is hit, when popover is triggered by focus', async () => {
@@ -170,12 +163,10 @@ describe('<Popover />', () => {
     const button = screen.getByTestId('button1');
 
     fireEvent.focus(button);
-    expect(popover.getByText('Copy to clipboard')).toBeInTheDocument();
+    expect(popover.getByText(POPOVER_TEXT)).toBeInTheDocument();
 
     fireEvent.keyDown(document.activeElement, {key: 'Escape'});
-    await waitForElementToBeRemoved(() =>
-      popover.queryByText('Copy to clipboard')
-    );
+    await waitForElementToBeRemoved(() => popover.queryByText(POPOVER_TEXT));
   });
 
   it('hides popover when Esc key is hit, when popover is triggered by hover', async () => {
@@ -183,12 +174,10 @@ describe('<Popover />', () => {
     const button = screen.getByTestId('button1');
 
     userEvent.hover(button);
-    expect(popover.getByText('Copy to clipboard')).toBeInTheDocument();
+    expect(popover.getByText(POPOVER_TEXT)).toBeInTheDocument();
 
     fireEvent.keyDown(document.activeElement, {key: 'Escape'});
-    await waitForElementToBeRemoved(() =>
-      popover.queryByText('Copy to clipboard')
-    );
+    await waitForElementToBeRemoved(() => popover.queryByText(POPOVER_TEXT));
   });
 
   it('hides popover when Esc key is hit, when popover is both focused and hovered', async () => {
@@ -197,44 +186,40 @@ describe('<Popover />', () => {
 
     fireEvent.focus(button);
     userEvent.hover(button);
-    expect(popover.getByText('Copy to clipboard')).toBeInTheDocument();
+    expect(popover.getByText(POPOVER_TEXT)).toBeInTheDocument();
 
     fireEvent.keyDown(document.activeElement, {key: 'Escape'});
-    await waitForElementToBeRemoved(() =>
-      popover.queryByText('Copy to clipboard')
-    );
+    await waitForElementToBeRemoved(() => popover.queryByText(POPOVER_TEXT));
   });
 
   it('displays popover initally when set as default open', async () => {
-    const popover = render(<RenderPopover />);
+    const popover = render(<RenderPopover defaultOpen />);
 
-    expect(popover.getByText('Copy to clipboard')).toBeInTheDocument();
+    expect(popover.getByText(POPOVER_TEXT)).toBeInTheDocument();
   });
 
   it('can hide default open popover by clicking somewhere in the document', async () => {
     const {queryByText} = render(<RenderPopover />);
 
     userEvent.click(document.body);
-    await waitForElementToBeRemoved(() => queryByText('Copy to clipboard'));
+    await waitForElementToBeRemoved(() => queryByText(POPOVER_TEXT));
   });
 
   it('popover shows up and hides properly when set as default open', async () => {
-    const popover = render(<RenderPopover />);
+    const popover = render(<RenderPopover defaultOpen />);
 
     const button = screen.getByTestId('button1');
 
     expect(button).toBeInTheDocument();
-    expect(popover.getByText('Copy to clipboard')).toBeInTheDocument();
+    expect(popover.getByText(POPOVER_TEXT)).toBeInTheDocument();
 
     userEvent.hover(button);
-    expect(popover.getByText('Copy to clipboard')).toBeInTheDocument();
+    expect(popover.getByText(POPOVER_TEXT)).toBeInTheDocument();
 
     userEvent.unhover(button);
-    await waitForElementToBeRemoved(() =>
-      popover.queryByText('Copy to clipboard')
-    );
+    await waitForElementToBeRemoved(() => popover.queryByText(POPOVER_TEXT));
 
     userEvent.hover(button);
-    expect(popover.getByText('Copy to clipboard')).toBeInTheDocument();
+    expect(popover.getByText(POPOVER_TEXT)).toBeInTheDocument();
   });
 });
