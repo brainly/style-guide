1ec007d7a17c1d6c5d42c38d609360ce9241e8aa
Radoslaw <r.warisch@gmail.com>
2024-03-27 17:55:07 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,20 +1,3 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.use(express.urlencoded({extended: true}));
-
-
-app.post('/echo', (req, res) => {
-  const userInput = req.body.name;
-
-  res.send(`Hello, ${userInput}!`);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
 const http = require('http');
 const url = require('url');
 const querystring = require('querystring');
