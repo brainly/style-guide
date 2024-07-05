768d6977da3d29ffcdaefd0cd91b83dd0b0d7c0c
Karol Rzotki <karol.rzotki@brainly.com>
2024-07-05 15:31:25 +0200

    adjust test

--- src/components/accordion/Accordion.spec.tsx
+++ src/components/accordion/Accordion.spec.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Accordion from './Accordion';
 import AccordionItem from './AccordionItem';
-import {render, fireEvent, waitFor} from '@testing-library/react';
+import {render, fireEvent, waitFor, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import {testA11y} from '../../axe';
 
@@ -215,32 +215,27 @@ describe('<Accordion>', () => {
 
   it('expands and collapses item on Enter/Space keydown when motion is reduced', async () => {
     const accordionId = 'id-1';
-    const accordion = render(
+    render(
       <Accordion reduceMotion>
-        <AccordionItem title={accordionId} id={accordionId}>
+        <AccordionItem title={accordionId} id={accordionId} tabIndex={1}>
           Accordion Item Description
         </AccordionItem>
       </Accordion>
     );
-    const item = accordion.getByRole('button');
+    const item = screen.getByRole('button');
 
     expect(item.getAttribute('aria-expanded')).toEqual('false');
-    expect(accordion.queryByRole('region')).toBeNull();
+    expect(screen.queryByRole('region')).toBeNull();
 
-    // This one does not trigger onFocus method in React 17
-    accordion.getByRole('button').focus();
+    focusElement(item);
 
-    // This one does not set document.activeElement
-    // https://github.com/jsdom/jsdom/issues/2586
-    fireEvent.focus(accordion.getByRole('button'));
-
-    expect(item).toEqual(document.activeElement);
-    userEvent.keyboard('{enter}');
+    fireEvent.keyDown(item, {key: 'Enter', keyCode: '13'});
     expect(item.getAttribute('aria-expanded')).toEqual('true');
-    expect(accordion.getByRole('region')).toBeTruthy();
-    userEvent.keyboard('{space}');
+    expect(screen.getByRole('region')).toBeTruthy();
+
+    fireEvent.keyDown(item, {key: 'Space', keyCode: '32'});
     expect(item.getAttribute('aria-expanded')).toEqual('false');
-    expect(accordion.queryByRole('region')).toBeNull();
+    expect(screen.queryByRole('region')).toBeNull();
   });
 
   it('has an accessible name', () => {
@@ -266,3 +261,14 @@ describe('<Accordion>', () => {
     });
   });
 });
+
+const focusElement = (element: HTMLElement) => {
+  // This does not set the document.activeElement to the item, so expect(item).toHaveFocus() fails
+  fireEvent.focus(element);
+
+  // Those do not trigger onFocus event in React 17, so focusedElementId is always null
+  // userEvent.tab();
+  // item.focus();
+
+  // expect(item).toHaveFocus();
+};
