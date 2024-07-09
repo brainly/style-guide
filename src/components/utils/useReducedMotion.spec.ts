a7bf9c350311397ed38b84446967655dbdc41dff
Karol Rzotki <karol.rzotki@brainly.com>
2024-07-09 16:14:55 +0200

    lint fix

--- src/components/utils/useReducedMotion.spec.ts
+++ src/components/utils/useReducedMotion.spec.ts
@@ -1,7 +1,7 @@
 import useReducedMotion from './useReducedMotion';
-import { renderHook} from '@testing-library/react';
+import {renderHook} from '@testing-library/react';
 import MatchMedia from './__mocks__/mq';
-import { act } from 'react';
+import {act} from 'react';
 
 const MOTION_MQ = {
   reduced: '(prefers-reduced-motion: reduce)',
